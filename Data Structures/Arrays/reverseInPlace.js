4092276dbf9e44f9baedba9b7f79abfdc1988d7d
timothyshores <timothymshores@gmail.com>
2022-11-23 00:43:39 -0700

    Reverse array in place setup and test cases

---
+++ Data Structures/Arrays/reverseInPlace.js
@@ -0,0 +1,18 @@
+// Problem Statement: Write a function that takes an array and reverses its elements in place. Do not use built-in methods and do not create another array
+
+// Expected Runtime: O(N) to iterate through the length N array
+// Space Complexity: O(1) to reverse in-place. Creating a new array is O(N) so try in-place.
+
+// Example
+// print(reverse([1, 2, 3, 4, 5]) == [5, 4, 3, 2, 1]);
+
+const reverse = (array) => {
+    
+};
+
+reverse([]) == [];
+reverse([5]) == [5];
+reverse([5, 10]) == [10, 5];
+reverse([5, 10, 15]) == [15, 10, 5];
+reverse([5, 10, 15, 20]) == [20, 15, 10, 5];
+reverse([1, 2, 3, 4, 5]) == [5, 4, 3, 2, 1];
