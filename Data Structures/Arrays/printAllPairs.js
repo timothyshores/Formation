d4b7f512ef9a487c172b41eeb25f48422bf7c782
timothyshores <timothymshores@gmail.com>
2022-11-22 20:56:47 -0700

    Add solution to print pairs

--- Data Structures/Arrays/printAllPairs.js
+++ Data Structures/Arrays/printAllPairs.js
@@ -1,3 +1,15 @@
+const printAllPairs = (arr) => {
+	if (arr.length < 2) return;
+
+	// Pointer 1 position
+	for (let i = 0; i < arr.length - 1; i++) {
+		// Pointer 2 position
+		for (let j = i + 1; j < arr.length; j++) {
+			console.log(`${arr[i]} ${arr[j]}`);
+		}
+	}
+};
+
 printAllPairs([]);
 // no output
 
@@ -10,12 +22,12 @@ printAllPairs([1, 2, 3]);
 // 2 3
 
 printAllPairs([1, 2, 3, 4]);
-// 1, 2
-// 1, 3
-// 1, 4
-// 2, 3
-// 2, 4
-// 3, 4
+// 1 2
+// 1 3
+// 1 4
+// 2 3
+// 2 4
+// 3 4
 
 printAllPairs([1, 2, 3, 4, 5]);
 // 1 2
