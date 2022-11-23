d3e6f4392b2291e685bed699899232d68f6bbf0e
timothyshores <timothymshores@gmail.com>
2022-11-22 22:04:35 -0700

    Add binary search implementation and sorting  algorithm notes

---
+++ Lecture Notes/Concept Drill/sortingArrays.js
@@ -0,0 +1,14 @@
+// Sorting algorithms
+
+// O(N^2) sorting algorithms
+// bubble sort
+// insertion sort
+// selection sort
+
+// O(NlogN) sorting algorithms
+// heap sort
+// merge sort
+// quick sort
+
+// https://visualgo.net/en/sorting
+
