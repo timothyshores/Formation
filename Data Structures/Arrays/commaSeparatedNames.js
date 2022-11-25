4706401b6639e9cea2cfba3b0bc2624ce8e711f0
timothyshores <timothymshores@gmail.com>
2022-11-25 14:10:25 -0700

    Implement comma separated names function

--- Data Structures/Arrays/commaSeparatedNames.js
+++ Data Structures/Arrays/commaSeparatedNames.js
@@ -3,13 +3,36 @@
 // Expected Runtime: O(N) to iterate through N names in the string
 // Expected space complexity: O(N) to store the N names in the resulting string
 
-const commaSeparate = (arr) => {};
+const combineLastTwoNames = (arr) => {
+	let lastName = arr.pop();
+	let secondToLastName = arr.pop();
+	return `${secondToLastName} and ${lastName}`;
+};
+
+const commaSeparate = (arr) => {
+	let result = "";
+
+	if (arr.length === 0) return result;
+
+	if (arr.length >= 3) {
+		for (let i = 0; i < arr.length - 2; i++) {
+			result += arr[i] + ", ";
+		}
+
+		return result + combineLastTwoNames(arr);
+	}
+
+	if (arr.length === 2) {
+		return combineLastTwoNames(arr);
+	}
+
+	if (arr.length === 1) return arr[0];
+
+	return result;
+};
 
 console.log(commaSeparate([]) == "");
-
 console.log(commaSeparate(["Sophie"]) == "Sophie");
-
-console.log(commaSeparate(["Daniel", "Craig"]) == "Daniel and Craig");
 console.log(commaSeparate(["Daniel", "Craig"]) == "Daniel and Craig");
 console.log(
 	commaSeparate(["Sam", "Grant", "Jenny"]) === "Sam, Grant and Jenny"
@@ -18,7 +41,8 @@ console.log(
 	commaSeparate(["Oliver", "Pixel", "Fido"]) == "Oliver, Pixel and Fido"
 );
 console.log(
-	commaSeparate(["Jono", "Paavo", "Tony", "me"]) == "Jono, Paavo, Tony and me"
+	commaSeparate(["Jono", "Paavo", "Tony", "me", "myself", "I"]) ==
+		"Jono, Paavo, Tony, me, myself and I"
 );
 console.log(commaSeparate(["John", "John", "John"]) == "John, John and John");
 console.log(commaSeparate(["Sean", "John", "Sean"]) == "Sean, John and Sean");
