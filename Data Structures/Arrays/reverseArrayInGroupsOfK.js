380a4234315ab2eb3eadfdd559b84a48a1f23fe1
timothyshores <timothymshores@gmail.com>
2022-11-25 16:37:55 -0700

    Add test cases to reverse array in groups of K

--- Data Structures/Arrays/reverseArrayInGroupsOfK.js
+++ Data Structures/Arrays/reverseArrayInGroupsOfK.js
@@ -45,3 +45,40 @@ const reverse = (arr, k) => {
 
 	return arr;
 };
+
+const array1 = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+const output1 = [3, 2, 1, 6, 5, 4, 9, 8, 7].toString();
+console.log(reverse(array1, 3).toString() === output1); // true
+
+const array2 = [1, 2, 3, 4, 5, 6, 7, 8];
+const output2 = [5, 4, 3, 2, 1, 8, 7, 6].toString();
+console.log(reverse(array2, 5).toString() === output2); // true
+
+const array3 = [1, 2, 3, 4, 5, 6];
+const output3 = [1, 2, 3, 4, 5, 6].toString();
+console.log(reverse(array3, 1).toString() === output3); // true
+
+const array4 = [1, 2, 3, 4, 5, 6, 7, 8];
+const output4 = [8, 7, 6, 5, 4, 3, 2, 1].toString();
+console.log(reverse(array4, 10).toString() === output4); // true
+
+const array5 = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+const output5 = [4, 3, 2, 1, 8, 7, 6, 5, 9].toString();
+console.log(reverse(array5, 4).toString() === output5); // true
+
+const array6 = [1, 2, 3, 4, 5, 6, 7, 8];
+const output6 = [4, 3, 2, 1, 8, 7, 6, 5].toString();
+console.log(reverse(array6, 4).toString() === output6); // true
+
+const array7 = [1, 2, 3, 4, 5, 6, 7, 8];
+const output7 = [5, 4, 3, 2, 1, 8, 7, 6].toString();
+console.log(reverse(array7, 5).toString() === output7); // true
+
+const array8 = [1, 2, 3, 4, 5, 6];
+const output8 = [4, 3, 2, 1, 6, 5].toString();
+console.log(reverse(array8, 4).toString() === output8); // true
+
+console.log(reverse([1], 1) == 1); // true
+console.log(reverse([1], 2) == 1); // true
+console.log(reverse([], 1).length === 0); // true
+console.log(reverse([], 2).length === 0); // true
