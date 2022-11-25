e2765aac269588128cdbc28ab4685fd1d73f128f
timothyshores <timothymshores@gmail.com>
2022-11-25 03:55:12 -0700

    Implement pair that sums to k two pointer solution

--- Data Structures/Arrays/pairThatSumsToKTwoPointerSolution.js
+++ Data Structures/Arrays/pairThatSumsToKTwoPointerSolution.js
@@ -5,7 +5,29 @@
 // Expected Runtime: O(N) - two-pointer approach
 // Expected space complexity: O(1) to store a constant number of variables
 
-const sumToK = (arr, k) => {};
+const sumToK = (arr, k) => {
+	if (arr.length < 2) return false;
+
+	let sum;
+	let lowPointer = 0;
+	let highPointer = arr.length - 1;
+
+	while (lowPointer < highPointer) {
+		sum = arr[lowPointer] + arr[highPointer];
+
+		if (sum === k) {
+			return true;
+		}
+		if (sum < k) {
+			lowPointer++;
+		}
+		if (sum > k) {
+			highPointer--;
+		}
+	}
+
+	return false;
+};
 
 console.log(sumToK([5], 5) === false); // true
 console.log(sumToK([4], 5) === false); // true
