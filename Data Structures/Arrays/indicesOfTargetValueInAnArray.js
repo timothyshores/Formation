2decd997da3961d60aac566b16c16c7b27856af7
timothyshores <timothymshores@gmail.com>
2022-11-26 09:44:00 -0700

    Create Indices of Target Value in Array prompt and examples

---
+++ Data Structures/Arrays/indicesOfTargetValueInAnArray.js
@@ -0,0 +1,20 @@
+// Write a function that receives an array as an input and a target value X,
+// The function will return an array of all indices containing value X.
+
+// Time: O(N) to iterate through the length N array
+// Space: O(N) to store up to N indices in the result
+
+// Examples
+// console.log(indicesOfTarget([30, 30, 30], 30) == [0, 1, 2]);
+// console.log(indicesOfTarget([3, 2, 5, 5, 1], 5) == [2, 3]);
+
+const indicesOfTarget = (input, target) => {};
+
+// TODO: Test the following conditions
+// empty list
+// array with 1 element and target is present
+// array with 1 element and target isn't present
+// array with multiple elements and target is present
+// array with multiple elements and target isn't present
+// array with multiple elements and target is present multiples times
+// array with multiple elements and target is the only value
