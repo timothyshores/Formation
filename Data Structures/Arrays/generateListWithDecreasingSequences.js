7f1c3f0e9d69a88b68f87284216bddd9a889f9f0
timothyshores <timothymshores@gmail.com>
2022-11-26 12:27:04 -0700

    Create generate list with decreasing sequence

---
+++ Data Structures/Arrays/generateListWithDecreasingSequences.js
@@ -0,0 +1,12 @@
+// Prompt
+// Given a target integer X, iterate from X to 1 and return a list of sequences where each  starts with the current iteration and goes down to Each iteration should decrement the array size and values until it reaches 1.
+
+// Example
+// console.log(generateSequence(2)) // [[2,1], [1]]
+// console.log(generateSequence(3)) // [[3,2,1], [2,1], [1]]
+
+// Expected space time complexity
+// Time: O(X2) to iterate X times from 1 to X
+// Space: O(X2) to store up to X elements X times in the matrix
+
+const generateSequence = (target) => {};
