b6ee1697eff87185409494f249488135ad01f30e
timothyshores <timothymshores@gmail.com>
2022-11-26 13:41:24 -0700

    Add test cases and fix bug

--- Data Structures/Arrays/monotonicArray.js
+++ Data Structures/Arrays/monotonicArray.js
@@ -18,7 +18,7 @@ const isMonotonic = (array) => {
 	let increasing = false;
 	let index = 1;
 
-	while (array[0] !== array[index] && index < array.length) {
+	while (index < array.length) {
 		// increasing
 		if (array[0] < array[index]) {
 			increasing = true;
@@ -46,3 +46,50 @@ const isMonotonic = (array) => {
 
 	return true;
 };
+
+// empty array
+console.log(isMonotonic([])); // true
+
+// 1-element array
+console.log(isMonotonic([0])); // true
+console.log(isMonotonic([1])); // true
+
+// strictly increasing array
+console.log(isMonotonic([1, 2])); // true
+console.log(isMonotonic([1, 2, 3])); // true
+console.log(isMonotonic([1, 2, 3, 4])); // true
+console.log(isMonotonic([1, 2, 3, 4, 5])); // true
+
+// strictly decreasing array
+console.log(isMonotonic([2, 1])); // true
+console.log(isMonotonic([3, 2, 1])); // true
+console.log(isMonotonic([4, 3, 2, 1])); // true
+console.log(isMonotonic([5, 4, 3, 2, 1])); // true
+
+// array with adjacent duplicates
+console.log(isMonotonic([1, 1, 2])); // true
+console.log(isMonotonic([1, 2, 2])); // true
+console.log(isMonotonic([2, 2, 1])); // true
+console.log(isMonotonic([2, 1, 1])); // true
+
+// array with separated duplicates
+console.log(isMonotonic([1, 1, 2, 3, 3])); // true
+console.log(isMonotonic([3, 3, 2, 1, 1])); // true
+console.log(isMonotonic([1, 2, 2, 3, 4, 4])); // true
+console.log(isMonotonic([4, 3, 3, 2, 1, 1])); // true
+
+// all elements of the same value
+console.log(isMonotonic([1, 1])); // true
+console.log(isMonotonic([1, 1, 1])); // true
+console.log(isMonotonic([1, 1, 1, 1])); // true
+console.log(isMonotonic([1, 1, 1, 1, 1])); // true
+
+// Examples that return false
+console.log(isMonotonic([2, 1, 3])); // false
+console.log(isMonotonic([4, 2, 8])); // false
+console.log(isMonotonic([8, 2, 4])); // false
+console.log(isMonotonic([2, 3, 1])); // false
+console.log(isMonotonic([8, 2, 4, 2])); // false
+console.log(isMonotonic([1, 2, 3, 1, 2, 3])); // false
+console.log(isMonotonic([3, 2, 1, 3, 2, 1])); // false
+console.log(isMonotonic([2, 2, 2, 1, 4, 5])); // false
