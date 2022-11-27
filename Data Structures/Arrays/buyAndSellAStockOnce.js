eb35c3f9251409f5b9c9c1666ad0e2898791673e
timothyshores <timothymshores@gmail.com>
2022-11-26 22:53:32 -0700

    Create buy and sell stock once and add prompt

---
+++ Data Structures/Arrays/buyAndSellAStockOnce.js
@@ -0,0 +1,20 @@
+// Prompt
+// Given an array of floats that represents the movement in the share price of a company over time, design an algorithm that determines the maximum profit that could have been made by buying and then selling a single share over the time period. The buy must occur before the sell.
+
+// Expected Runtime
+// Time: O(N) to iterate through the length N array once
+// Space: O(1) to use a constant amount of variables for any array length
+
+// Examples
+// Input: prices = [7,1,5,3,6,4]
+
+// Input: prices = [5, 7, 8, 9, 3, 1, 6]
+// Output: 5
+// Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
+// Note: buying on day 2 and selling on day 1 is not allowed because you must buy before you sell.
+
+// Input: prices = [7,6,4,3,1]
+// Output: 0
+// Explanation: In this case, no transactions are done and the max profit = 0.
+
+const maxProfitPotential = (prices) => {};
