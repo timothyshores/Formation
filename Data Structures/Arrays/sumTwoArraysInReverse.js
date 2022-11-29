ae05ddc5856a215de1e8116b7d0f71d64fa30c82
timothyshores <timothymshores@gmail.com>
2022-11-29 12:51:42 -0700

    Add sum two arrays prompt and examples

---
+++ Data Structures/Arrays/sumTwoArraysInReverse.js
@@ -0,0 +1,25 @@
+// Given two arrays of equal length, return an array summing them together with the second array being traversed in reverse.
+
+// Expected Runtime
+// Time: O(N) to iterate through the length N arrays
+// Space: O(N) to store the length N result
+
+const sumInReverse = (array1, array2) => {
+	return;
+};
+
+// Examples
+// array1 = [1, 2, 3]
+// array2 = [4, 6, 10]
+// result = [11, 8, 7]
+// Explanation: array1 is being traversed from left to right and array2 is traversed right to left, so 1 is added to 10 for 11, and so on.
+
+// array1 = [1, 5, 10, 12]
+// array2 = [2, 4, 3, 5]
+// result = [6, 8, 14, 14]
+
+// Test Cases
+// empty array
+// 1-element arrays
+// odd-length arrays
+// even-length arrays
