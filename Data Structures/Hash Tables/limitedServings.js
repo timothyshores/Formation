3cfb1a1ff04318f727e0aef5442e1d2a43dec2c0
timothyshores <timothymshores@gmail.com>
2022-11-29 11:09:54 -0700

    Create limited servings, add prompt and expected run time

---
+++ Data Structures/Hash Tables/limitedServings.js
@@ -0,0 +1,13 @@
+// You're a bartender, and you have to look out for your patrons - you don't want them to drink too much. Assume everyone is having the same drink, and everyone has the same set amount of "allowed servings".
+
+// Given an array of patrons (denoted by their names, eg: Adrian), and an integer value representing "allowed servings", return True if someone attempts to go over the allowed number of servings per person.
+
+// Otherwise, False if no one drinks too much.
+
+// Can you think of any data structures that might help?
+
+// Expected Runtime
+// Time: O(N) where N = # of total interactions (non-unique patrons)
+// Space: O(N) to store up to N patrons
+
+const limitedServings = (patrons, allowedServings) => {};
