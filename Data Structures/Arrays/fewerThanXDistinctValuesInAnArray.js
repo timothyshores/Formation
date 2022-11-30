22fc8303d6cca41cd092fe1a394fde35bf6dc3aa
timothyshores <timothymshores@gmail.com>
2022-11-29 21:06:19 -0700

    Create fewer than X distinct values in an array

---
+++ Data Structures/Arrays/fewerThanXDistinctValuesInAnArray.js
@@ -0,0 +1,19 @@
+// Given an array and a target count X, return true if there are less than X distinct values in the array. For example, given [1, 2, 2, 3, 3] and 4, return true because there are only 3 distinct values 1, 2, and 3.
+
+// Expected Time Complexity: O(N) to iterate through the length N array
+// Expected Space Complexity: O(N) to store up to N elements
+
+// Examples
+// [1, 2, 2, 3, 3], 3 => false (there are exactly 3 distinct values)
+// [1, 2, 2, 3, 4], 3 => false (there are 4 distinct values)
+// [1, 1, 2, 2, 2], 3 => true (there are exactly 3 distinct values)
+
+const fewerThanTargetDistinct = (arr, target) => {};
+
+// Test cases
+// empty array
+// 1-element array
+// array with < X distinct values
+// array with == X distinct values
+// array with > X distinct values
+// array longer than X but with < X distinct values
