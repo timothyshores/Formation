59f9c4deae2e9c4dbaec0dcf1b3b35495c97cdc0
timothyshores <timothymshores@gmail.com>
2022-12-01 14:40:49 -0700

    Add generate list with increasing sequences

---
+++ Data Structures/Arrays/generateListWithIncreasingSequences.js
@@ -0,0 +1,21 @@
+// Prompt
+// Given a target integer X, iterate from 1 to X and return a matrix sequence where each array starts with 1 and goes up to the current iteration. Each iteration should increment the array size and values until it reaches X.
+
+// [
+// [1],
+// [1, 2],
+// [1, 2, 3],
+// [1, 2, 3, 4],
+// [1, 2, 3, 4, 5],
+// ...
+// [1, 2, 3, ..., X]
+// ]
+
+// Expected Time Complexity: O(N^2) to iterate X times from 1 to X
+// Expected Space Complexity: O(N^2) to store up to X elements X times in the matrix
+
+/**
+ * @param {number} target
+ * @return {list[list[int]}
+ */
+const generateSequence = (target) => {};
