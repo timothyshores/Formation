3c9c127b8a248bda6752dacf5e71a24009873c3a
timothyshores <timothymshores@gmail.com>
2022-11-30 21:04:24 -0700

    Create can plant flowers

---
+++ Data Structures/Arrays/canPlantFlowers.js
@@ -0,0 +1,30 @@
+// You have a long flowerbed in which some of the plots are planted, and some are not. However, flowers cannot be planted in adjacent plots.
+// Given an integer array flowerbed containing 0's and 1's, where 0 means empty and 1 means not empty, and an integer newFlowers, return true if all newFlowers can be planted in the flowerbed without violating the no-adjacent-flowers rule.
+
+// Expected Time Complexity: O(N) to iterate through the length N array
+// Expected Space Complexity: O(1) to store some variables
+
+// Example 1
+// Input: flowerbed = [1,0,0,0,1], n = 1
+// Output: true
+
+// Example 2
+// Input: flowerbed = [1,0,0,0,1], n = 2
+// Output: false
+
+// Example 3
+// Input: flowerbed = [0,0,1], n = 1
+// Output: true
+
+const canPlantFlowers = (flowerbed, n: int) => {};
+
+// Test Cases
+// 1-slot flowerbeds
+// 2-slot flowerbeds
+// every other slot filled
+// no slot filled
+// all slots filled
+// mixture of slots filled
+// n > free slots
+// n == free slots
+// n < free slots
