0aedb96a1e3b7bae4ace513617d4f3c7ba8d23b0
timothyshores <timothymshores@gmail.com>
2022-12-01 14:49:33 -0700

    Add first repeated elements in array and update subdirectory name

---
+++ Data Structures/Hash Tables & Sets/firstRepeatedElementInArray.js
@@ -0,0 +1,15 @@
+// Prompt
+// Given an array, return the first element is repeated if you were to traverse the array from left to right.
+
+// function
+//  Runtime
+// Expected Time: O(N) to iterate through the length N list optimally otherwise O(n2) to loop through the length N array for each N index
+// Expected Space: O(N) to track the array elements
+// Examples
+// print(firstRepeatedElement([1, 2, 3, 2, 1, 1]) == 2)
+
+/**
+ * @param {number[]} input
+ * @return {number}
+ */
+const firstRepeatedElement = (input) => {};
