b36e6ce12be8a01f20c3f4f067566fbf87697b45
timothyshores <timothymshores@gmail.com>
2022-12-02 09:19:23 -0700

    Add match fellow by skill level, add prompt, runtime and examples

---
+++ Data Structures/Hash Tables & Sets/matchFellowsBySkillLevel.js
@@ -0,0 +1,12 @@
+// Prompt
+// Given an input dictionary mapping Fellows (as string names) to skill ratings, return true if you're able to pair up Fellows with matching skill ratings with no Fellows leftover.
+
+// Expected Runtime
+// Time: O(N) to iterate through the length N dictionary
+// Space: O(N) to store the skills to compare against
+
+// Examples
+// console.log(canMatchFellows({"oliver": 3, "pixel": 3, "pinky": 5, "tobey": 5}) == True)
+// console.log(canMatchFellows({"oliver": 3, "pixel": 4, "pinky": 5, "tobey": 5}) == False)
+
+const canMatchFellows = (dictionary) => {};
