530aa4335e1d0df088c7069d7cadced388661cf0
timothyshores <timothymshores@gmail.com>
2022-12-01 23:32:28 -0700

    Add test cases to number of elements repeated X times in an array

--- Data Structures/Hash Tables & Sets/numbeElementsRepeatedXTimesInArray.js
+++ Data Structures/Hash Tables & Sets/numbeElementsRepeatedXTimesInArray.js
@@ -25,3 +25,40 @@ const countExactOccurrences = (arr, exactOccurrences) => {
 		.length;
 };
 
+// empty array
+console.log(countExactOccurrences([], 1) === 0); // true
+console.log(countExactOccurrences([], 2) === 0); // true
+
+// 1-element array
+console.log(countExactOccurrences([1], 1) === 1); // true
+console.log(countExactOccurrences([1], 2) === 0); // true
+
+// X element array all with unique values
+console.log(countExactOccurrences([1, 2, 3], 1) === 3); // true
+console.log(countExactOccurrences([1, 2, 3], 2) === 0); // true
+
+// X element array all with the same value
+console.log(countExactOccurrences([1, 1], 1) === 0); // true
+console.log(countExactOccurrences([1, 1], 2) === 1); // true
+console.log(countExactOccurrences([1, 1, 1], 3) === 1); // true
+console.log(countExactOccurrences([1, 1, 1, 1], 4) === 1); // true
+
+// array with exactly one value occurring X times
+console.log(countExactOccurrences([1, 2, 2], 2) === 1); // true
+console.log(countExactOccurrences([1, 2, 3, 3], 2) === 1); // true
+console.log(countExactOccurrences([1, 2, 2, 2], 3) === 1); // true
+
+// array with multiple values occurring X times
+console.log(countExactOccurrences([1, 2, 2, 1], 2) === 2); // true
+console.log(countExactOccurrences([2, 1, 2, 1], 2) === 2); // true
+console.log(countExactOccurrences([2, 2, 1, 1], 2) === 2); // true
+
+// array with all values occurring < X times
+console.log(countExactOccurrences([1, 2], 2) === 0); // true
+console.log(countExactOccurrences([1, 2], 10) === 0); // true
+console.log(countExactOccurrences([1, 2, 1, 2], 3) === 0); // true
+
+// array with value occurring > X times
+console.log(countExactOccurrences([1, 1, 1], 2) === 0); // true
+console.log(countExactOccurrences([1, 1, 1, 1], 3) === 0); // true
+console.log(countExactOccurrences([1, 1, 1, 1, 1], 4) === 0); // true
