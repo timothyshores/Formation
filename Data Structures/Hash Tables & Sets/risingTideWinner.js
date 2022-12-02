ebcb7f99ccf1a4892c69d83967df7a784def3b82
timothyshores <timothymshores@gmail.com>
2022-12-01 23:09:40 -0700

    Create rising tide

---
+++ Data Structures/Hash Tables & Sets/risingTideWinner.js
@@ -0,0 +1,7 @@
+// Prompt
+// Given an array of Fellows representing nominations for a Rising Tide Award at Formation, return the name of the winning Fellow with the most number of nominations
+
+// Expected Time Complexity: O(N) to iterate through the length N array
+// Expected Space Complexity: O(N) to store up to N nominations
+
+const risingTideWinner = (nominations) => {};
