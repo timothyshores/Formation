fb63814108dd8e64fda2dcd9f2397eb053ee8a96
timothyshores <timothymshores@gmail.com>
2022-12-01 23:30:59 -0700

    Create number of element repeated X time in an array

---
+++ Data Structures/Hash Tables & Sets/numbeElementsRepeatedXTimesInArray.js
@@ -0,0 +1,17 @@
+// Prompt
+// Given an array and a target count X, return the number of elements that is repeated exactly X times.
+
+// Expected Runtime
+// Time: O(N) to iterate through the length N array
+// Space: O(N) to store up to N elements
+// Examples
+// [1, 2, 3, 1, 2, 3], 2 => 3
+// [1, 2, 3, 1, 2, 3], 3 => 0
+// [1, 3, 3, 5, 5, 5, 5, 5, 3], 3 => 1
+
+/**
+ * @param {number[]} arr
+ * @param {number} exactOccurrences
+ * @return {number}
+ */
+const countExactOccurrences = (arr, exactOccurrences) => {};
