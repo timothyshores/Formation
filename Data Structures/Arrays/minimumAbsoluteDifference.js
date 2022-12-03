a1aa04ee7a3cfce54a0d14ca600e6f67d3199bcd
timothyshores <timothymshores@gmail.com>
2022-12-03 13:12:40 -0700

    Add minimum absolute difference with prompt and examples

---
+++ Data Structures/Arrays/minimumAbsoluteDifference.js
@@ -0,0 +1,21 @@
+// Prompt
+// Given an array of unique integers, find all pairs of elements with the minimum absolute difference. If there are multiple pairs, return them in ascending order.
+
+//  Runtime
+// Expected Time Complexity: O(N log N) to traverse the length N array and organize it
+// Expected Space Complexity: O(N) to store up to N elements
+
+// Examples
+// Input: arr = [1,3,6,10,15]
+// Output: [[1,3]]
+// Explanation: There is only 1 pair of elements with a minimum absolute difference of 2.
+
+// Input: arr = [3,8,-10,23,19,-4,-14,27]
+// Output: [[-14,-10],[19,23],[23,27]]
+// Explanation: There are 3 pairs of elements with a minimum absolute difference of 4 which are listed in ascending order according to the smaller value in the pair.
+
+// Input: arr = [4,2,1,3]
+// Output: [[1,2],[2,3],[3,4]]
+// Explanation: There are 3 pairs of elements with a minimum absolute difference of 1 which are listed in ascending order according to the smaller value in the pair.
+
+const minAbsDiffPairs = (arr) => {};
