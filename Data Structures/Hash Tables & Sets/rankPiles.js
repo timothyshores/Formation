55221b72a0e21d0c9d0dffd9bb226a6b560aac75
timothyshores <timothymshores@gmail.com>
2022-12-03 14:22:49 -0700

    Add rank piles prompt, examples and expected run time

---
+++ Data Structures/Hash Tables & Sets/rankPiles.js
@@ -0,0 +1,22 @@
+// Prompt
+// Given an array of integers representing piles of rocks (e.g., 3 means 3 rocks), modify the input array to rank the piles from 1 to N, representing the least to the most quantity of rocks.
+// That is, the pile with the lowest count of rocks should be ranked 1, the second lowest should be 2, and so on.
+// You may use built-in functions provided by your programming language.
+
+// Expected Runtime
+// Time: O(N log N) to traverse the length N array and organize it
+// Space: O(N) to store up to N elements for processing
+
+// Example 1
+// Input: [10, 8, 15, 12, 6, 20, 1]
+// Output: [4, 3, 6, 5, 2, 7, 1]
+
+// Example 2
+// Input: [3, 4, 1]
+// Output: [2, 3, 1]
+
+// Example 3
+// Input: [2, 1]
+// Output: [2, 1]
+
+const rankPiles = (piles) => {};
