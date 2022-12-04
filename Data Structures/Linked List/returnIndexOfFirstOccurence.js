ef9331030d799cf1620d5e9a5b0eb0dbf8e32b88
timothyshores <timothymshores@gmail.com>
2022-12-04 09:03:34 -0700

    Add test cases to return first index of occurrence in linked list

--- Data Structures/Linked List/returnIndexOfFirstOccurence.js
+++ Data Structures/Linked List/returnIndexOfFirstOccurence.js
@@ -28,3 +28,35 @@ const firstIndexInLL = (node, target) => {
 
 	return -1;
 };
+
+// empty list
+console.log(firstIndexInLL(null, 1));
+
+// 1 node list with target
+const oneNodeList = new Node(1);
+console.log(firstIndexInLL(oneNodeList, 1));
+
+// 1 node list without target
+console.log(firstIndexInLL(new Node(), 1));
+
+// list with all unique
+const uniquesOneThroughFive = new Node(
+	1,
+	new Node(2, new Node(3, new Node(4, new Node(5))))
+);
+console.log(firstIndexInLL(uniquesOneThroughFive, 1) === 0); // true
+console.log(firstIndexInLL(uniquesOneThroughFive, 2) === 1); // true
+console.log(firstIndexInLL(uniquesOneThroughFive, 3) === 2); // true
+console.log(firstIndexInLL(uniquesOneThroughFive, 5) === 4); // true
+console.log(firstIndexInLL(uniquesOneThroughFive, 6) === -1); // true
+console.log(firstIndexInLL(uniquesOneThroughFive, 9) === -1); // true
+
+// list with duplicates of target
+const duplicate1 = new Node(1, new Node(1)); // true
+console.log(firstIndexInLL(duplicate1, 1) === 0); // true
+
+const duplicate2 = new Node(1, new Node(2, new Node(2))); // true
+console.log(firstIndexInLL(duplicate2, 2) === 1); // true
+
+// list without target
+console.log(firstIndexInLL(duplicate1, 0) === -1); // true
