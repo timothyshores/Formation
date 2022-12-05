8232299130ba9c88ae4d3fba0dccb0342d2b85dd
timothyshores <timothymshores@gmail.com>
2022-12-04 18:46:28 -0700

    Create array to linked list, add prompt, examples and boiler plate code

---
+++ Data Structures/Linked List/arrayToLinked List.js
@@ -0,0 +1,36 @@
+// Prompt
+// Given an array containing numbers, convert this to a singly linked list and return the head of the list.
+
+// Expected Time Complexity: O(n) to iterate through the n length array
+// Expected Space Complexity: O(n) to store n elements into nodes
+
+// Examples
+// console.log(toString(arrayToLL([1,2])) == "1 -> 2")
+// console.log(toString(arrayToLL([1,2,3])) == "1 -> 2 -> 3")
+
+class Node {
+	constructor(val, next = null) {
+		this.val = val;
+		this.next = next;
+	}
+}
+
+function toString(head) {
+	if (!head) return "<empty>";
+
+	let parts = [];
+	while (head) {
+		parts.push(head.val);
+		head = head.next;
+	}
+
+	return parts.join(" -> ");
+}
+
+/**
+ * Converts an array of numbers into a singly linked list
+ *
+ * @param {number[]} array
+ * @return {Node} head node
+ */
+const arrayToLL = (array) => {};
