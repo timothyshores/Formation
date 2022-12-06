480bcc6b3453650aa12f45bbec8fa68d7b8c0d31
timothyshores <timothymshores@gmail.com>
2022-12-06 10:02:11 -0700

    Add print name frequency with prompt, examples and starter code

---
+++ Data Structures/Hash Tables & Sets/printNameFrequency.js
@@ -0,0 +1,15 @@
+// You're given a comma-separated string of names, and you want to print how many times each name appeared. For each person that appears, you should print a string {name} appeared {x} times., in order of appearance.
+// To properly compare results in the test suite, return an array of strings joined by a newline as the result of your method.
+
+// Expected Time Complexity: O(N) to iterate through N names in the string
+// Expected Space Complexity: O(N) to store up to N names
+
+// Examples
+// console.log(printNameFreq("Tony, Jessica, Paavo, Jessica, Tony, Zoe") ==
+// "Tony appeared 2 times.\n\
+// Jessica appeared 2 times.\n\
+// Paavo appeared 1 time.\n\
+// Zoe appeared 1 time.")
+// console.log(printNameFreq("") == "Nobody appeared.")
+
+const printNameFreq = (names) => {};
