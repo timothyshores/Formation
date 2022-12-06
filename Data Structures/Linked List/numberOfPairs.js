d775284e22204a666850133b625c83259b72ede5
timothyshores <timothymshores@gmail.com>
2022-12-06 02:04:58 -0700

    Add number of pairs in a linked list with prompt and starter code

---
+++ Data Structures/Linked List/numberOfPairs.js
@@ -0,0 +1,20 @@
+// Prompt
+// Given a linked list, return the number of values that occurred exactly twice.
+
+// Time: O(N) to iterate through the length N list
+// Space O(N) to count N nodes and their frequencies
+
+// Examples
+// // 1 -> 2 -> 2 -> 3 -> 3 -> 3
+// let head1 = new Node(1, new Node(2, new Node(2, new Node(3, new Node(3, new Node(3))))))
+
+// console.log(numPairs(head1) == 1)
+
+class Node {
+	constructor(value, next = null) {
+		this.value = value;
+		this.next = next;
+	}
+}
+
+const numPairs = (head) => {};
