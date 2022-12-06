b4c22682ace9ebaa594f86e650ef98b5361e0320
timothyshores <timothymshores@gmail.com>
2022-12-06 12:59:16 -0700

    Create find Kth element from end of Linked List two pointer approach

---
+++ Data Structures/Linked List/findKthElementFromEndTwoPointers.js
@@ -0,0 +1,24 @@
+// Q. Given a linked list, return the kth element from the end of the list.
+//    If the k exceeds the length of the list, return -1.
+
+// Examples:
+// Given a linked list: 13 ➞ 1 ➞ 5 ➞ 3 ➞ 7 ➞ 10, k: 1 returns 10
+// Given a linked list: 13 ➞ 1 ➞ 5 ➞ 3 ➞ 7 ➞ 10, k: 7 returns -1
+
+class ListNode {
+	constructor(value = 0, next = null) {
+		this.value = value;
+		this.next = next;
+	}
+}
+
+const kthFromLast = (head, k) => {};
+
+// Test Data
+const LL1 = new ListNode(
+	13,
+	new ListNode(
+		1,
+		new ListNode(5, new ListNode(3, new ListNode(7, new ListNode(10))))
+	)
+);
