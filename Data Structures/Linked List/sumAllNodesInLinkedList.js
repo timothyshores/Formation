4c7bca93b044bd55a10f9924657f18d36b743be3
timothyshores <timothymshores@gmail.com>
2022-12-06 00:14:52 -0700

    Create sum all nodes in linked list with prompt and starter code

---
+++ Data Structures/Linked List/sumAllNodesInLinkedList.js
@@ -0,0 +1,14 @@
+// Given a linked list, sum all elements in the list.
+
+// Examples:
+// Given a linked list: 1 ➞ 4 ➞ 5 // returns 10
+// Given a linked list: 1 // returns 1
+
+class Node {
+	constructor(value = 0, next = null) {
+		this.value = value;
+		this.next = next;
+	}
+}
+
+const sum = (node) => {};
