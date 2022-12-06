ff326ec7050bd36118d908e49f6cadd695642a63
timothyshores <timothymshores@gmail.com>
2022-12-06 00:34:19 -0700

    Create remove duplicates, add prompt, example and starter code

---
+++ Data Structures/Hash Tables & Sets/removeDuplicatesInArray.js
@@ -0,0 +1,12 @@
+// A bug in the Formation session tool accidentally duplicated some Fellows in sessions. Given an array of names representing Fellows in a session, return an array of the distinct Fellows. The array must be in the same order as the input.
+
+// Follow-up question:
+// How would removing the Fellows in place, instead of using a new output array, affect the algorithm's runtime?
+
+// Expected Time Complexity: O(N) to iterate through the length N array
+// Expected Space Complexity: O(N) to store up to N elements
+
+// Example
+// ["oliver", "pixel", "oliver", "pinky"] will return ["oliver", "pixel", "pinky"]
+
+const removeDuplicateFellows = (fellows) => {};
