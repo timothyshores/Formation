e262dc77a21623d012bba8f5ea2210e28326a575
timothyshores <timothymshores@gmail.com>
2022-12-06 02:05:44 -0700

    Add test cases to number of pairs

--- Data Structures/Linked List/numberOfPairs.js
+++ Data Structures/Linked List/numberOfPairs.js
@@ -31,3 +31,47 @@ const numPairs = (head) => {
 
 	return Object.values(counter).filter((value) => value === 2).length;
 };
+
+// empty list
+console.log(numPairs());
+
+// 1 node list
+console.log(numPairs(new Node(1)) === 0); // true
+
+// 2 node list with unique values
+console.log(numPairs(new Node(1, new Node(2))) === 0); // true
+
+// 2 node list with the same value
+console.log(numPairs(new Node(1, new Node(1))) === 1); // true
+
+// list with a pair
+console.log(numPairs(new Node(1, new Node(1, new Node(0)))) === 1); // true
+console.log(numPairs(new Node(0, new Node(1, new Node(1)))) === 1); // true
+console.log(numPairs(new Node(1, new Node(0, new Node(1)))) === 1); // true
+
+// list with multiple pairs
+console.log(numPairs(new Node(0, new Node(0, new Node(1, new Node(1))))) === 2); // true
+
+console.log(
+	numPairs(new Node(-1, new Node(-1, new Node(1, new Node(1))))) === 2
+); // true
+
+console.log(
+	numPairs(new Node(-1, new Node(1, new Node(1, new Node(-1))))) === 2
+); // true
+
+// list with multiple duplicates
+console.log(numPairs(new Node(1, new Node(2, new Node(3)))) === 0); // true
+console.log(numPairs(new Node(1, new Node(1, new Node(1)))) === 0); // true
+
+// list with pairs and multiple duplicates
+console.log(numPairs(new Node(1, new Node(1, new Node(2, new Node(3))))) === 1); // true
+console.log(numPairs(new Node(1, new Node(2, new Node(3, new Node(1))))) === 1); // true
+
+// list with a number that appears 3 times
+console.log(numPairs(new Node(1, new Node(1, new Node(1)))) === 0); // true
+console.log(numPairs(new Node(0, new Node(0, new Node(0)))) === 0); // true
+console.log(numPairs(new Node(-1, new Node(-1, new Node(-1)))) === 0); // true
+
+// list with a number that appears more than twice and the frequency is divisible by two
+console.log(numPairs(new Node(1, new Node(1, new Node(1, new Node(1))))) === 0); // true
