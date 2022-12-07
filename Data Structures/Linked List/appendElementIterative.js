7a0e23e6a9205f602328ec0c1623a1cd86b69228
timothyshores <timothymshores@gmail.com>
2022-12-06 18:21:07 -0700

    Create append element iterative including prompt, examples and starter code

---
+++ Data Structures/Linked List/appendElementIterative.js
@@ -0,0 +1,26 @@
+// Given a linked list, append an element with a target value to the list iteratively.
+
+// Examples:
+// Given a linked list: 1 ➞ 4 ➞ 5, target: 7 // returns 1 ➞ 4 ➞ 5 ➞ 7
+// Given a linked list: 0, target 7 // returns 0 ➞ 7
+
+class Node {
+	constructor(value = 0, next = null) {
+		this.value = value;
+		this.next = next;
+	}
+}
+
+const arrayify = (head) => {
+	let pointer = head;
+	const array = [];
+
+	while (pointer != null) {
+		array.push(pointer.value);
+		pointer = pointer.next;
+	}
+
+	return array;
+};
+
+const append = (head, target) => {};
