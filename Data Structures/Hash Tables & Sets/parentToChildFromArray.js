31babc61988c1e0713b2a2a719d274bf2924a259
timothyshores <timothymshores@gmail.com>
2022-12-08 14:28:35 -0700

    Implement parentToChild function using a hashmap and array

--- Data Structures/Hash Tables & Sets/parentToChildFromArray.js
+++ Data Structures/Hash Tables & Sets/parentToChildFromArray.js
@@ -19,4 +19,32 @@
 // 'Fred is the parent of George'
 // 'Gloria is the parent of Jen'
 
-const parentToChild = (relations) => {};
+const parentToChild = (relations) => {
+	if (relations[0].length === 0) return "No family relations";
+
+	// Want to create an object where the key is the parent and values are the children
+	const parentToChild = {};
+
+	// Need to iterate through each sub array within the relations input
+	for (const [child, parent1, parent2] of relations) {
+		parentToChild[parent1] = parentToChild[parent1]
+			? [...parentToChild[parent1], child]
+			: [child];
+
+		parentToChild[parent2] = parentToChild[parent2]
+			? [...parentToChild[parent2], child]
+			: [child];
+	}
+
+	// Want our output to be Name is the part of child1, child2, child3, ..., childK.
+
+	const parentRelationships = [];
+	// iterate through our parentToChild map
+	for (let [parent, child] of Object.entries(parentToChild)) {
+		// For every parent in the map we want to push the string parentName is the parent of childName
+		parentRelationships.push(`${parent} is the parent of ${child}`);
+	}
+
+	// Join the array with a new line between each parent to child relationship
+	return parentRelationships.join("\n");
+};
