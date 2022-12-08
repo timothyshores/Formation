32781784a36c097061ca50692e559f9b04b577a3
timothyshores <timothymshores@gmail.com>
2022-12-08 14:27:30 -0700

    Add parent to child with prompt, runtime, examples and starter code

---
+++ Data Structures/Hash Tables & Sets/parentToChildFromArray.js
@@ -0,0 +1,22 @@
+// Prompt
+// Given an array of arrays representing relations  child, parent1, and parent2 in each row, print a string representing all children of each parent.
+// For example: Ben is the parent of James, Jen. The formatting doesn't matter, as long as all children are represented in the final output.
+
+// Expected Runtime O(n), where n = # of rows/relations
+// Examples
+// Input: []
+// Output: 'No family relations'
+
+// Input: [
+//   ["James", "Ben", "Lisa"],
+//   ["George", "Taylor", "Fred"],
+//   ["Jen", "Ben", "Gloria"]
+// ]
+// Output:
+// 'Ben is the parent of James, Jen'
+// 'Lisa is the parent of James'
+// 'Taylor is the parent of George'
+// 'Fred is the parent of George'
+// 'Gloria is the parent of Jen'
+
+const parentToChild = (relations) => {};
