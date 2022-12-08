cbc4d4018144caa4f1e774a4653d0e8ef81833b9
timothyshores <timothymshores@gmail.com>
2022-12-08 10:55:21 -0700

    Add test cases to skill with highest overlap

--- Data Structures/Hash Tables & Sets/skillWithHighestOverlap.js
+++ Data Structures/Hash Tables & Sets/skillWithHighestOverlap.js
@@ -26,3 +26,40 @@ const highestSkillOverlap = (fellowSkills) => {
 
 	return mostFrequentSkillRating;
 };
+
+// empty dictionary
+console.log(highestSkillOverlap(new Map()) === null); // true
+
+// one element dictionary
+console.log(highestSkillOverlap({ pinky: 1 }) === 1); // true
+console.log(highestSkillOverlap({ pixel: 2 }) === 2); // true
+console.log(highestSkillOverlap({ oliver: 3 }) === 3); // true
+
+// dictionary with all the same value
+console.log(highestSkillOverlap({ oliver: 3, tobey: 3 }) === 3); // true
+
+// dictionary with one pair and unique values
+console.log(
+	highestSkillOverlap({ oliver: 3, pinky: 1, pixel: 2, tobey: 1 }) === 1
+); // true
+
+// dictionary with unique values
+let oneOfEachSkillRating = { oliver: 3, pinky: 4, pixel: 2, tobey: 1 };
+console.log(
+	highestSkillOverlap(oneOfEachSkillRating) >= 1 &&
+		highestSkillOverlap(oneOfEachSkillRating) <= 4
+); // true
+
+// dictionary with equally repeated pair values
+let twoRatingsForEverySkillLevel = {
+	jess: 1,
+	zoe: 1,
+	paavo: 2,
+	jono: 2,
+	tony: 3,
+	angus: 3,
+};
+console.log(
+	highestSkillOverlap(twoRatingsForEverySkillLevel) >= 1 &&
+		highestSkillOverlap(twoRatingsForEverySkillLevel) <= 3
+); // true
