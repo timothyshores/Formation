d92f66e223d4ed9f61efbc204d20235298adbd71
timothyshores <timothymshores@gmail.com>
2022-12-08 14:29:01 -0700

    Add test cases to parent to child from array

--- Data Structures/Hash Tables & Sets/parentToChildFromArray.js
+++ Data Structures/Hash Tables & Sets/parentToChildFromArray.js
@@ -48,3 +48,56 @@ const parentToChild = (relations) => {
 	// Join the array with a new line between each parent to child relationship
 	return parentRelationships.join("\n");
 };
+
+// empty matrix
+console.log(parentToChild([[]]) === "No family relations"); // true
+
+// child with 2 parents
+const relations1 = [["James", "Ben", "Lisa"]];
+const results1 = parentToChild(relations1);
+
+console.log(
+	results1.includes("Ben is the parent of James") &&
+		results1.includes("Lisa is the parent of James")
+); // true
+
+// parents with multiple children
+const relations2 = [
+	["James", "Ben", "Lisa"],
+	["George", "Taylor", "Fred"],
+	["Jen", "Ben", "Gloria"],
+];
+
+const results2 = parentToChild(relations2);
+
+console.log(
+	results2.includes("Ben is the parent of James,Jen") &&
+		results2.includes("Lisa is the parent of James") &&
+		results2.includes("Taylor is the parent of George") &&
+		results2.includes("Fred is the parent of George") &&
+		results2.includes("Gloria is the parent of Jen")
+); // true
+
+// parent with children with different parents
+
+const relations3 = [
+	["Justine", "Tony", "Jessica"],
+	["Paavo", "Jessica", "Tony"],
+	["Zoe", "Jessica", "Tony"],
+	["Benana", "Ben", "Ana"],
+	["Egg", "Rooster", "Hen"],
+	["Eggart", "Duck", "Hen"],
+	["Mule", "Horse", "Donkey"],
+];
+
+const results3 = parentToChild(relations3);
+
+console.log(results3.includes("Tony is the parent of Justine,Paavo,Zoe")); // true
+console.log(results3.includes("Jessica is the parent of Justine,Paavo,Zoe")); // true
+console.log(results3.includes("Ben is the parent of Benana")); // true
+console.log(results3.includes("Ana is the parent of Benana")); // true
+console.log(results3.includes("Rooster is the parent of Egg")); // true
+console.log(results3.includes("Hen is the parent of Egg,Eggart")); // true
+console.log(results3.includes("Duck is the parent of Eggart")); // true
+console.log(results3.includes("Horse is the parent of Mule")); // true
+console.log(results3.includes("Donkey is the parent of Mule")); // true
