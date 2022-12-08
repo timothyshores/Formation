266c6cd69f2a22f14a1d92e188fa74a98bc730a0
timothyshores <timothymshores@gmail.com>
2022-12-08 10:54:40 -0700

    Add skill with highest overlap

---
+++ Data Structures/Hash Tables & Sets/skillWithHighestOverlap.js
@@ -0,0 +1,10 @@
+// Prompt
+// Given an input dictionary mapping from the name of Fellows to their numerical skill rating, return the skill rating with the highest number of Fellows.
+
+// Expected Time Complexity: O(N) to iterate through N dictionary entries
+// Expected Space Complexity: O(N) to store up to N skill levels
+
+// Example
+// {"oliver": 3, "pixel": 1, "pinky": 3} => 3
+
+const highestSkillOverlap = (fellowSkills) => {};
