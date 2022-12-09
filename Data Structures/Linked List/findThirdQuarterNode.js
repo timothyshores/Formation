1e8c0a0c7b3329f476abd5f154cb0325fc57e26d
timothyshores <timothymshores@gmail.com>
2022-12-08 19:44:39 -0700

    Add test cases to find third quarter node

--- Data Structures/Linked List/findThirdQuarterNode.js
+++ Data Structures/Linked List/findThirdQuarterNode.js
@@ -28,3 +28,47 @@ const findThirdQuarterNode = (head) => {
 
 	return current;
 };
+
+// Empty LL
+console.log(findThirdQuarterNode(null).value === 0); // true
+console.log(findThirdQuarterNode(null).next === null); // true
+
+// Four Node LL
+let fourNodeLL = new Node(1, new Node(2, new Node(3, new Node(4))));
+console.log(findThirdQuarterNode(fourNodeLL).value === 3); // true
+
+// 8 Node LL
+let eightNodeLL = new Node(
+	1,
+	new Node(
+		2,
+		new Node(3, new Node(4, new Node(5, new Node(6, new Node(7, new Node(8))))))
+	)
+);
+console.log(findThirdQuarterNode(eightNodeLL).value === 6); // true
+
+// 12 node LL
+let twelveNodeLL = new Node(
+	1,
+	new Node(
+		2,
+		new Node(
+			3,
+			new Node(
+				4,
+				new Node(
+					5,
+					new Node(
+						6,
+						new Node(
+							7,
+							new Node(8, new Node(9, new Node(10, new Node(11, new Node(12)))))
+						)
+					)
+				)
+			)
+		)
+	)
+);
+
+console.log(findThirdQuarterNode(twelveNodeLL).value === 9); // true
