434457ff2e3c2b1b61df6c57f4aefe8b1c02e9b6
timothyshores <timothymshores@gmail.com>
2022-12-15 00:05:29 -0700

    Create triangle of blocks in recursion directory

---
+++ Algorithms/Recursion/triangleOfBlocks.js
@@ -0,0 +1,13 @@
+// Prompt
+// We have a triangle made of blocks. The topmost row has 1 block, the next row down has 2 blocks, the next row has 3 blocks, and so on. Compute recursively (no loops or multiplication) the total number of blocks in such a triangle with the given number of rows.
+
+// Expected Runtime
+// Time: O(n) where n is the number of rows
+// Space: O(n) where n is the number of rows to create a stack frame for each row
+
+// Examples
+// console.log(triangle(2) === 3); // true
+// console.log(triangle(3) === 6); // true
+// console.log(triangle(5) === 15); // true
+
+const triangle = (rows) => {};
