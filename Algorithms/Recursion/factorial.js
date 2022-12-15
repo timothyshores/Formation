259670f580006beef4b3ca42b3a5652a4d5d9d29
timothyshores <timothymshores@gmail.com>
2022-12-14 23:50:52 -0700

    Create recursion directory and add factorial

---
+++ Algorithms/Recursion/factorial.js
@@ -0,0 +1,12 @@
+// Prompt
+// In mathematics, the factorial of a non-negative integer N, denoted as N!, is the multiplication product of all positive integers <= N. Compute the result recursively (without loops).
+
+// Expected Runtime
+// Time: O(N) to calculate N digits
+// Space: O(N) to store N frames on the function call stack
+
+// Examples
+// console.log(factorial(3) === 6); // true
+// console.log(factorial(5) === 120); // true
+
+const factorial = (n) => {};
