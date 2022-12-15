7677abbd53093641667dc89c5940e3ccaafacd7e
timothyshores <timothymshores@gmail.com>
2022-12-15 11:09:07 -0700

    Add bunny ears twist in recursion directory

---
+++ Algorithms/Recursion/bunnyEarsTwist.js
@@ -0,0 +1,13 @@
+// Prompt
+// We have bunnies standing in a line, numbered 1, 2, ... The odd bunnies (1, 3, ..) have the usual 2 ears. The even bunnies (2, 4, ..) we'll say have 3 ears because they each have a raised foot. Recursively return the number of "ears" in the bunny line 1, 2, ... n (without loops or multiplication).
+
+// Expected Runtime
+// Time: O(n) where n is the number of bunnies
+// Space: O(n) where n is the number of stack frames because one is created for each bunny
+
+// Examples
+// console.log(bunnyEarsTwist(2) === 5); // true
+// console.log(bunnyEarsTwist(3) === 7); // true
+// console.log(bunnyEarsTwist(5) === 12); // true
+
+const bunnyEarsTwist = (bunnies) => {};
