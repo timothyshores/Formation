58926faee9c4bcb6b71341d9eb8f231f11ac8d3d
timothyshores <timothymshores@gmail.com>
2022-12-15 11:27:25 -0700

    Add change pi in recursion directory

---
+++ Algorithms/Recursion/changePi.js
@@ -0,0 +1,13 @@
+// Prompt
+// Given a string, compute recursively (no loops) a new string where all appearances of "pi" have been replaced by "3.14".
+
+// Expected Runtime
+// Time: O(L) where L is the length of the string
+// Space: O(L^2) where L is the length of the string to create a new stack frame for L characters and to create a new L length string from slicing on each frame.
+
+// Examples
+// changePi("xpix") == "x3.14x"
+// changePi("pipi") == "3.143.14"
+// changePi("pip") == "3.14p"
+
+const changePi = (word) => {};
