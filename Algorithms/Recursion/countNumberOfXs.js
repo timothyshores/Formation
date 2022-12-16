e73d7bc7a37942a6110b05815e0b28be3de4fa36
timothyshores <timothymshores@gmail.com>
2022-12-16 11:20:30 -0700

    Implement count X function using recursion and add test cases

--- Algorithms/Recursion/countNumberOfXs.js
+++ Algorithms/Recursion/countNumberOfXs.js
@@ -10,4 +10,44 @@
 // console.log(countX("xhixhix") === 3); // true
 // console.log(countX("hi") === 0); // true
 
-const countX = (word) => {};
+const countX = (word) => {
+	if (word === "") return 0;
+	if (word[0] === "x") return 1 + countX(word.substring(1));
+	return countX(word.substring(1));
+};
+
+// empty string
+console.log(countX("") === 0); // true
+
+// single character strings
+console.log(countX("a") === 0); // true
+console.log(countX("b") === 0); // true
+console.log(countX("c") === 0); // true
+console.log(countX("x") === 1); // true
+console.log(countX("y") === 0); // true
+console.log(countX("z") === 0); // true
+
+// string with capitalized X
+console.log(countX("X") === 0); // true
+
+// string with all x's
+console.log(countX("xx") === 2); // true
+console.log(countX("xxx") === 3); // true
+console.log(countX("xxxx") === 4); // true
+console.log(countX("xxxxx") === 5); // true
+console.log(countX("xxxxxxxxxx") === 10); // true
+
+// string with no x's
+console.log(countX("abcdefghijklmnopqrstuvwyz") === 0); // true
+
+// string with a mixture of x's and other characters
+console.log(countX("xa") === 1); // true
+console.log(countX("ax") === 1); // true
+console.log(countX("xaxb") === 2); // true
+console.log(countX("axbx") === 2); // true
+console.log(countX("xaxbxc") === 3); // true
+console.log(countX("axbxcx") === 3); // true
+console.log(countX("xaxbxcxd") === 4); // true
+console.log(countX("axbxcxdx") === 4); // true
+console.log(countX("xaxbxcxdxe") === 5); // true
+console.log(countX("axbxcxdxex") === 5); // true
