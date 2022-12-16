e53a82d1496e0ad2ac7a9d0fcd1bad4f4336b074
timothyshores <timothymshores@gmail.com>
2022-12-15 19:57:53 -0700

    Create count elements in a linked list in recursion directory

---
+++ Algorithms/Recursion/countElementsInaLinkedList.js
@@ -0,0 +1,14 @@
+// Given an unsorted linked list, count the number of elements (iteratively or recursively).
+
+// Examples:
+// • Given a linked list: 1 ➞ 4 ➞ 5 // returns 3
+// • Given a linked list: 0 // returns 1
+
+class Node {
+	constructor(value = 0, next = null) {
+		this.value = value;
+		this.next = next;
+	}
+}
+
+const count = (head) => {};
