9552b7c0c987974ba5dc941231b9f8b1f3e24c42
timothyshores <timothymshores@gmail.com>
2022-12-15 19:40:44 -0700

    Implement count skipped pairs and add test cases

--- Algorithms/Recursion/countSkippedPairs.js
+++ Algorithms/Recursion/countSkippedPairs.js
@@ -10,4 +10,46 @@
 // countSkippedPairs("axax") == 2
 // countSkippedPairs("aaa") == 1
 
-const countSkippedPairs = (word) => {};
+const countSkippedPairs = (word) => {
+	if (word.length < 3) return 0;
+
+	if (word[0] === word[2]) return 1 + countSkippedPairs(word.substring(1));
+
+	return countSkippedPairs(word.substring(1));
+};
+
+// empty string
+console.log(countSkippedPairs("") === 0); // true
+
+// single character string
+console.log(countSkippedPairs("a") === 0); // true
+console.log(countSkippedPairs("b") === 0); // true
+console.log(countSkippedPairs("c") === 0); // true
+
+// 2-character string
+console.log(countSkippedPairs("aa") === 0); // true
+console.log(countSkippedPairs("bb") === 0); // true
+console.log(countSkippedPairs("cc") === 0); // true
+
+// 3-character strings
+console.log(countSkippedPairs("aba") === 1); // true
+console.log(countSkippedPairs("bab") === 1); // true
+console.log(countSkippedPairs("abc") === 0); // true
+console.log(countSkippedPairs("xyz") === 0); // true
+
+// strings with alternating characters
+console.log(countSkippedPairs("abab") === 2); // true
+console.log(countSkippedPairs("xyxyx") === 3); // true
+console.log(countSkippedPairs("abcabc") === 0); // true
+
+// strings of all the same character
+console.log(countSkippedPairs("aaa") === 1); // true
+console.log(countSkippedPairs("aaaa") === 2); // true
+console.log(countSkippedPairs("aaaaa") === 3); // true
+
+// repeated 2-character strings
+console.log(countSkippedPairs("aba") === 1); // true
+console.log(countSkippedPairs("abab") === 2); // true
+console.log(countSkippedPairs("ababa") === 3); // true
+console.log(countSkippedPairs("ababab") === 4); // true
+console.log(countSkippedPairs("abababa") === 5); // true
