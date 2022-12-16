b6a4473cc5a5568a65455345813f9c18cb2c491b
timothyshores <timothymshores@gmail.com>
2022-12-16 11:47:21 -0700

    Add sum digits under recursion subdirectory

---
+++ Algorithms/Recursion/sumDigits.js
@@ -0,0 +1,13 @@
+// Prompt
+// Given a non-negative int n, return the sum of its digits recursively (no loops). Note that mod (%) by 10 yields the rightmost digit (126 % 10 is 6), while divide (/) by 10 removes the rightmost digit (126 / 10 is 12).
+
+// Expected Runtime
+// Time: O(d) where d is the number of digits
+// Space: O(d) where d is the number of digits to create each stack frame
+
+// Examples
+// console.log(sumDigits(12) === 3); // true
+// console.log(sumDigits(49) === 13); // true
+// console.log(sumDigits(126) === 9); // true
+
+const sumDigits = (n) => {};
