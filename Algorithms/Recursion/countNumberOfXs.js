39a3cf48eee0ae0565e539e0d0a76e5baa8e7fe1
timothyshores <timothymshores@gmail.com>
2022-12-16 11:19:45 -0700

    Add count Xs problem in recursion subdirectory

---
+++ Algorithms/Recursion/countNumberOfXs.js
@@ -0,0 +1,13 @@
+// Prompt
+// Given a string, compute recursively (no loops) the number of lowercase 'x' chars in the string.
+
+// Expected Runtime
+// Time: O(L) where L is the length of the string
+// Space: O(L) where L is the length of the string when using an index or O(L^2) when slicing new strings
+
+// Examples
+// console.log(countX("xxhixx") === 4); // true
+// console.log(countX("xhixhix") === 3); // true
+// console.log(countX("hi") === 0); // true
+
+const countX = (word) => {};
