3bbd9067e3a33fc7d6f9df91d14ecc3c501bedbb
timothyshores <timothymshores@gmail.com>
2022-12-16 11:47:46 -0700

    Implement sum digits using recursion and add test cases

--- Algorithms/Recursion/sumDigits.js
+++ Algorithms/Recursion/sumDigits.js
@@ -10,4 +10,27 @@
 // console.log(sumDigits(49) === 13); // true
 // console.log(sumDigits(126) === 9); // true
 
-const sumDigits = (n) => {};
+const sumDigits = (n) => {
+	if (n < 10) return n;
+	return (n % 10) + sumDigits(Math.floor(n / 10));
+};
+
+// All test cases return true
+console.log(sumDigits(0) === 0);
+console.log(sumDigits(1) === 1);
+console.log(sumDigits(2) === 2);
+console.log(sumDigits(3) === 3);
+console.log(sumDigits(4) === 4);
+console.log(sumDigits(5) === 5);
+console.log(sumDigits(10) === 1);
+console.log(sumDigits(11) === 2);
+console.log(sumDigits(12) === 3);
+console.log(sumDigits(21) === 3);
+console.log(sumDigits(13) === 4);
+console.log(sumDigits(31) === 4);
+console.log(sumDigits(14) === 5);
+console.log(sumDigits(41) === 5);
+console.log(sumDigits(111) === 3);
+console.log(sumDigits(123) === 6);
+console.log(sumDigits(1234) === 10);
+console.log(sumDigits(12345) === 15);
