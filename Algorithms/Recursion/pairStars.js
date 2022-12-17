ac4064f8c7026fc5db6b0c7f9a426340abac1766
timothyshores <timothymshores@gmail.com>
2022-12-16 19:46:51 -0700

    Add pair stairs problem in recursion subdirectory

---
+++ Algorithms/Recursion/pairStars.js
@@ -0,0 +1,13 @@
+// Prompt
+// Given a string, compute recursively a new string where identical chars that are adjacent in the original string are separated from each other by a "*".
+
+// Expected Runtime
+// Time: O(L) where L is the length of the larger string
+// Space: O(L) where L is the length of the larger string to create each stack frame. O(L2) if slicing the string on each recursive step
+
+// Examples
+// console.log(pairStars("hello") === "hel*lo"); // true
+// console.log(pairStars("xxyy") === "x*xy*y"); // true
+// console.log(pairStars("aaaa") === "a*a*a*a"); // true
+
+const pairStars = (word) => {};
