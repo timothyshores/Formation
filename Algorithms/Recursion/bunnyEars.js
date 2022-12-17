9845567aca60a00e11760ac17c29f89a38622b8d
timothyshores <timothymshores@gmail.com>
2022-12-17 00:49:51 -0700

    Create bunny ears in recursion subdirectory

---
+++ Algorithms/Recursion/bunnyEars.js
@@ -0,0 +1,12 @@
+// Prompt
+// We have a number of bunnies, each with two big floppy ears. We want to compute the total number of ears across all the bunnies recursively, without loops or multiplication.
+
+// Expected Runtime
+// Time: O(n) where n is the number of bunnies
+// Space: O(n) where n is the number of stack frames because one is created for each bunny
+
+// Examples
+// console.log(bunnyEars(3) === 6); // true
+// console.log(bunnyEars(5) === 10); // true
+
+const bunnyEars = (bunnies) => {};
