67b46cc5ab25e253857de0f50fb14b56e43c157a
timothyshores <timothymshores@gmail.com>
2022-12-17 06:55:18 -0700

    Add substring count in recursion subdirectory

---
+++ Algorithms/Recursion/substringCount.js
@@ -0,0 +1,13 @@
+// Prompt
+// Given a string and a non-empty substring sub, compute recursively the number of times that sub appears in the string, without the substrings overlapping.
+
+// Expected Runtime
+// TIme: O(L) where L is the length of the larger string
+// Space: O(L^2) where L is the length of the larger string to create each stack frame and to concatenate and slice a new length L string on each frame.
+
+// Examples
+// print(strCount("catcowcat", "cat") == 2)
+// print(strCount("catcowcat", "cow") == 1)
+// print(strCount("catcowcat", "dog") == 0)
+
+const strCount = (word, sub) => {};
