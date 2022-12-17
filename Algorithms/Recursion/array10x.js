faea3a43cbf03e95ef28a83db1aea1e933145396
timothyshores <timothymshores@gmail.com>
2022-12-17 14:36:27 -0700

    Add array 10x in recursion subdirectory

---
+++ Algorithms/Recursion/array10x.js
@@ -0,0 +1,13 @@
+// Prompt
+// Given an array of ints, compute recursively if there's a value immediately followed by that value times 10 somewhere in the array
+
+// Expected Runtime
+// Time: O(n) where n is the length of the array
+// Space: O(n) to store a call stack frame for each array index
+
+// Examples
+// console.log(array10x([3], 0) === false); // true
+// console.log(array10x([3, 30], 0) === true); // true
+// console.log(array10x([1, 2, 20], 0) === true); // true
+
+const array10x = (nums) => {};
