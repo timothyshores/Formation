03eb311507b84a92e2ebc92db0ae6b56d09f4408
timothyshores <timothymshores@gmail.com>
2022-12-17 14:37:49 -0700

    Add test cases to array 10x

--- Algorithms/Recursion/array10x.js
+++ Algorithms/Recursion/array10x.js
@@ -20,3 +20,42 @@ const array10x = (nums) => {
 
 	return array10x(nums);
 };
+
+// All test cases return true
+
+// empty array
+console.log(!array10x([]));
+
+// array with 1 element
+console.log(!array10x([1]));
+console.log(!array10x([0]));
+console.log(!array10x([3]));
+console.log(!array10x([10]));
+console.log(!array10x([-1]));
+
+// array with 2 single digit elements
+console.log(array10x([0, 0]));
+console.log(!array10x([1, 2]));
+console.log(!array10x([-1, -2]));
+
+// array with 2 elements first is 10x the second
+console.log(!array10x([10, 1]));
+console.log(!array10x([20, 2]));
+console.log(!array10x([30, 3]));
+
+// array with 2 elements second is 10x the first
+console.log(array10x([1, 10]));
+console.log(array10x([2, 20]));
+console.log(array10x([3, 30]));
+console.log(array10x([-1, -10]));
+
+// array with multiple elements and desired pair somewhere in the middle
+console.log(array10x([1, 2, 20, 3]));
+console.log(array10x([1, 2, 20, 3, 4]));
+console.log(array10x([1, 2, 3, 30, 4]));
+
+// array with multiple elements but the 10x value is far away
+console.log(!array10x([1, 2, 10]));
+console.log(!array10x([1, 2, 3, 10]));
+console.log(!array10x([1, 2, 3, 4, 10]));
+console.log(!array10x([1, 2, 3, 4, 5, 10]));
