457aae287a355c92d3b9e2dbfe29c623673ef684
timothyshores <timothymshores@gmail.com>
2022-12-17 00:42:17 -0700

    Add test cases to add stars

--- Algorithms/Recursion/addStars.js
+++ Algorithms/Recursion/addStars.js
@@ -15,3 +15,27 @@ const addStars = (word) => {
 	if (word.length === 1) return word;
 	return word[0] + "*" + addStars(word.substring(1));
 };
+
+// empty string
+console.log(addStars("") === ""); // true
+
+// 1 character string
+console.log(addStars("a") === "a"); // true
+console.log(addStars("A") === "A"); // true
+console.log(addStars("z") === "z"); // true
+console.log(addStars("Z") === "Z"); // true
+
+// multi-character string
+console.log(addStars("ab") === "a*b"); // true
+console.log(addStars("ba") === "b*a"); // true
+console.log(addStars("AA") === "A*A"); // true
+console.log(addStars("abc") === "a*b*c"); // true
+console.log(addStars("hello") === "h*e*l*l*o"); // true
+
+// non-alphabetic string
+console.log(addStars("1") === "1"); // true
+console.log(addStars("2") === "2"); // true
+console.log(addStars("3") === "3"); // true
+console.log(addStars("12") === "1*2"); // true
+console.log(addStars("23") === "2*3"); // true
+console.log(addStars("123") === "1*2*3"); // true
