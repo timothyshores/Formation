2116d2bec5f1e7c420a73633c7b4b86417fe4f1a
timothyshores <timothymshores@gmail.com>
2022-12-17 00:50:35 -0700

    Implement bunny ears function and add test cases

--- Algorithms/Recursion/bunnyEars.js
+++ Algorithms/Recursion/bunnyEars.js
@@ -9,4 +9,25 @@
 // console.log(bunnyEars(3) === 6); // true
 // console.log(bunnyEars(5) === 10); // true
 
-const bunnyEars = (bunnies) => {};
+const bunnyEars = (bunnies) => {
+	if (bunnies === 0) return 0;
+	return 2 + bunnyEars(bunnies - 1);
+};
+
+// zero bunnies
+console.log(bunnyEars(0) === 0); // true
+
+// 1 bunny
+console.log(bunnyEars(1) === 2); // true
+
+// even number of bunnies
+console.log(bunnyEars(2) === 4); // true
+console.log(bunnyEars(4) === 8); // true
+console.log(bunnyEars(10) === 20); // true
+console.log(bunnyEars(50) === 100); // true
+
+// odd number of bunnies
+console.log(bunnyEars(3) === 6); // true
+console.log(bunnyEars(5) === 10); // true
+console.log(bunnyEars(69) === 138); // true
+console.log(bunnyEars(101) === 202); // true
