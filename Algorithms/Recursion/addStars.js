9b0632cfebc7a39b6a7be7351c013df25b376fb6
timothyshores <timothymshores@gmail.com>
2022-12-17 00:41:18 -0700

    Create add stars under recursion subdirectory

---
+++ Algorithms/Recursion/addStars.js
@@ -0,0 +1,13 @@
+// Prompt
+// Given a string, insert a star (*) between each character.
+
+// Expected Runtime
+// Time: O(L) where L is the length of the string
+// Space: O(L) where L is the length of the string when using an index or O(L^2) when slicing new strings
+
+// Examples
+// console.log(addStars("hello") === "h*e*l*l*o"); // true
+// console.log(addStars("abc") === "a*b*c"); // true
+// console.log(addStars("ab") === "a*b"); // true
+
+const addStars = (word) => {};
