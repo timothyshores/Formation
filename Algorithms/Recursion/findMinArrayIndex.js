388747aa27f1b90a5f77337225d916e7aed7c7a4
timothyshores <timothymshores@gmail.com>
2022-12-18 14:07:06 -0700

    Add find min array index in recursion subdirectory

---
+++ Algorithms/Recursion/findMinArrayIndex.js
@@ -0,0 +1,13 @@
+// Prompt
+// Given an array, write 2 recursive functions to find the index of the minimum and maximum element in an array. If there's a tie-breaker, return the first occurrence.
+
+// Expected Runtime
+// Time: O(n) where n is the length of the array. O(n2) If using splicing.
+// Space: O(n) to store a call stack frame for each array index. O(n2) If using splicing.
+
+// Examples
+// console.log(findMinIndex([10, 20, 30]) === 0);
+// console.log(findMinIndex([8, 6, 7, 5, 3, 7]) === 4);
+// console.log(findMinIndex([12, 1234, 45, 67, 1]) === 4);
+
+const findMinIndex = (arr) => {};
