5f88c6bd11b65658492b7fffc9d59bcca29cd7af
timothyshores <timothymshores@gmail.com>
2022-12-18 14:07:47 -0700

    Implement find min array index using recrusion

--- Algorithms/Recursion/findMinArrayIndex.js
+++ Algorithms/Recursion/findMinArrayIndex.js
@@ -10,4 +10,36 @@
 // console.log(findMinIndex([8, 6, 7, 5, 3, 7]) === 4);
 // console.log(findMinIndex([12, 1234, 45, 67, 1]) === 4);
 
-const findMinIndex = (arr) => {};
+const findMinIndex = (arr) => {
+	let currentIndex, currentValue;
+
+	const helper = (
+		array,
+		minValue = arr[arr.length - 1],
+		minIndex = arr.length - 1
+	) => {
+		// save the current length of the array
+		currentIndex = array.length - 1;
+
+		// pop off the last element in the array
+		currentValue = array.pop();
+
+		// if the last element in the array is less than the current minimum
+		if (currentValue <= minValue) {
+			// set the new minimum value and index
+			minValue = currentValue;
+			minIndex = currentIndex;
+		}
+
+		// when there is no more elements left in the array
+		if (array.length === 0) {
+			// return the minimum index
+			return minIndex;
+		}
+
+		// else check the remaining elements by calling the function again
+		return helper(array, minValue, minIndex);
+	};
+
+	return helper(arr);
+};
