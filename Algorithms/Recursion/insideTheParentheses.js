0cf3ac5b2829b1d4b6ea88efca7706c4ec204839
timothyshores <timothymshores@gmail.com>
2022-12-18 11:26:08 -0700

    Add inside the parenthesis in recursion subdirectory

---
+++ Algorithms/Recursion/insideTheParentheses.js
@@ -0,0 +1,13 @@
+// Prompt
+// Given a string that contains exactly 1 pair of parentheses, compute recursively a new string made of only the parentheses and their contents, so "xyz(abc)123" yields "(abc)".
+
+// Expected Runtime
+// Time: O(L) where L is the length of the string
+// Space: O(L^2) where L is the length of the string to create each stack frame and to concatenate and slice a new length L string on each frame
+
+// Examples
+// console.log(parenBit("xyz(abc)123") === "(abc)");
+// console.log(parenBit("x(hello)") === "(hello)");
+// console.log(parenBit("(xy)1") === "(xy)");
+
+const parenBit = (word) => {};
