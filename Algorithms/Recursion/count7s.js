2619980a96a2e4e76c250459c1c66d915637ea89
timothyshores <timothymshores@gmail.com>
2022-12-28 16:41:43 -0700

    Add count 7s in recursion subdirectory

---
+++ Algorithms/Recursion/count7s.js
@@ -0,0 +1,32 @@
+/*
+'''
+❓ PROMPT
+Given a non-negative int n, return the count of the occurrences of 7 as a digit, so for example 717 yields 2. (no loops).
+
+Note that mod (%) by 10 yields the rightmost digit (126 % 10 is 6), while integer division by 10 removes the rightmost digit Math.floor(126 / 10) returns 12. 
+
+Example(s)
+count7(7) == 1
+count7(123) == 0
+count7(717) == 2
+
+🔎 EXPLORE
+State your assumptions & discoveries:
+
+Create examples & test cases:
+
+🧠 BRAINSTORM
+What approaches could work? Consider data structures or algorithmic patterns.
+Analyze the space & time complexity.
+Approach 1:
+Time: 
+Space: 
+
+📆 PLAN
+High-level outline of approach #: 
+*/
+
+// 🛠️ IMPLEMENT
+const count7 = (n) => {};
+
+// 🧪 VERIFY - All test cases return true
