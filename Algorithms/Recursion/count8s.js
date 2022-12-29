e5c909a14e9eec450855a4e641fc8bd461b15fc4
timothyshores <timothymshores@gmail.com>
2022-12-28 21:06:54 -0700

    Add test cases to count 8s

--- Algorithms/Recursion/count8s.js
+++ Algorithms/Recursion/count8s.js
@@ -66,3 +66,32 @@ Methodically analyze and debug issue(s).
 */
 
 // All test cases return true
+console.log(count8(0) === 0);
+console.log(count8(1) === 0);
+console.log(count8(8) === 1);
+console.log(count8(11) === 0);
+console.log(count8(78) === 1);
+console.log(count8(88) === 3);
+console.log(count8(111) === 0);
+console.log(count8(123) === 0);
+console.log(count8(818) === 2);
+console.log(count8(888) === 5);
+console.log(count8(188) === 3);
+console.log(count8(8234) === 1);
+console.log(count8(2348) === 1);
+console.log(count8(1080) === 1);
+console.log(count8(9898) === 2);
+console.log(count8(8088) === 4);
+console.log(count8(8818) === 4);
+console.log(count8(8888) === 7);
+console.log(count8(81238) === 2);
+console.log(count8(23884) === 3);
+console.log(count8(88188) === 6);
+console.log(count8(88788) === 6);
+console.log(count8(88888) === 9);
+console.log(count8(881881) === 6);
+console.log(count8(1818188) === 5);
+console.log(count8(8818181) === 5);
+console.log(count8(1881881) === 6);
+console.log(count8(188188188) === 9);
+console.log(count8(881881881) === 9);
