6fbe3f4899880e4923c009b8fc1ef288d5661022
timothyshores <timothymshores@gmail.com>
2023-01-01 19:20:48 -0700

    Add substring copies in recursion subdirectory

---
+++ Algorithms/Recursion/substringCopies.js
@@ -0,0 +1,38 @@
+/*
+
+❓ PROMPT
+Given a string and a non-empty substring **sub**, compute recursively if at least n copies of sub appear in the string somewhere, possibly with overlapping. N will be non-negative.
+
+Example(s)
+strCopies("catcowcat", "cat", 2) == True
+strCopies("catcowcat", "cow", 2) == False
+strCopies("catcowcat", "cow", 1) == True
+
+🔎 EXPLORE
+State your assumptions & discoveries:
+The function will count the number of times that a substring appears within a given string
+If the substring appears n number of times or more return true
+Else return false
+
+Create examples & test cases:
+
+🧠 BRAINSTORM
+What approaches could work? Consider data structures or algorithmic patterns.
+Analyze the space & time complexity.
+Approach 1: 
+Time: O()
+Space: O()
+
+📆 PLAN
+High-level outline of approach 1:
+
+🛠️ IMPLEMENT
+*/
+
+const strCopies = (word, sub, n) => {};
+
+/*
+🧪 VERIFY
+Run your examples & test cases.
+Methodically analyze and debug issue(s).
+*/
