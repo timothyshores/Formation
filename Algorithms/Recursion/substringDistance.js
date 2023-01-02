1a56e47db90f4d5570063e77161abde1ecd96204
timothyshores <timothymshores@gmail.com>
2023-01-01 21:14:35 -0700

    Add substring distance problem under recursion subdirectory

---
+++ Algorithms/Recursion/substringDistance.js
@@ -0,0 +1,34 @@
+/*
+❓ PROMPT
+Given a string and a non-empty substring *sub*, compute recursively the size of the largest substring which starts and ends with the given *sub* string and return its length, including the length of the substrings. When *sub* is multiple characters, they may overlap with each other and share characters.
+
+Example(s)
+strDist("catcowcat", "cat") == 9
+strDist("catcowcat", "cow") == 3
+strDist("cccatcowcatxx", "cat") == 9
+strDist("ooowhwhwooo", "whw") == 5
+
+🔎 EXPLORE
+State your assumptions & discoveries: 
+
+Create examples & test cases:
+
+🧠 BRAINSTORM
+What approaches could work? Consider data structures or algorithmic patterns.
+Analyze the space & time complexity.
+Approach 1: 
+Time: O()
+Space: O()
+
+📆 PLAN
+High-level outline of approach #: 
+
+🛠️ IMPLEMENT  */
+
+const strDist = (word, sub) => {};
+
+/*
+🧪 VERIFY
+Run your examples & test cases.
+Methodically analyze and debug issue(s).
+*/
