5e3261266483ad5a408cf995f324c82d9d0aacb8
timothyshores <timothymshores@gmail.com>
2023-01-02 23:54:54 -0700

    Add get all subsequences under recursion subdirectory

---
+++ Algorithms/Recursion/getAllSubsequences.js
@@ -0,0 +1,33 @@
+/*
+❓ PROMPT
+Define a subsequence of a string s to be a list of characters from s such that the characters appear in the same order in the list and in s.
+For instance, for the string abcd, a, ab, and acd are valid subsequences, whereas db is not, since b comes before d in the original string.
+Given an input string, return all subsequences of the string except the empty string.
+
+Example
+console.log(getAllSubsequences("abc")); // ["a","b","c","ab","ac","bc","abc"]
+
+🔎 EXPLORE
+State your assumptions & discoveries: 
+
+Create examples & test cases:
+
+🧠 BRAINSTORM
+What approaches could work? Consider data structures or algorithmic patterns.
+Analyze the space & time complexity.
+Approach 1: 
+Time: O()
+Space: O()
+
+📆 PLAN
+High-level outline of approach #: 
+
+🛠️ IMPLEMENT  */
+
+const getAllSubsequences = (str) => {};
+
+/*
+🧪 VERIFY
+Run your examples & test cases.
+Methodically analyze and debug issue(s).
+*/
