103c49603962a7739ecbd84c36ca29e4d154d379
timothyshores <timothymshores@gmail.com>
2023-01-08 14:40:36 -0700

    Implement fibonacci sequence recursively and add test cases

--- Algorithms/Recursion/fibonacciSequence.js
+++ Algorithms/Recursion/fibonacciSequence.js
@@ -21,4 +21,21 @@ At what point do you hit your base case?
 
 */
 
-const fib = (i) => {};
+const fib = (n) => {
+	if (n === 0 || n === 1) return n;
+	return fib(n - 1) + fib(n - 2);
+};
+
+// All test cases return true
+console.log(fib(1) === 1);
+console.log(fib(2) === 1);
+console.log(fib(3) === 2);
+console.log(fib(4) === 3);
+console.log(fib(5) === 5);
+console.log(fib(6) === 8);
+console.log(fib(7) === 13);
+console.log(fib(8) === 21);
+console.log(fib(9) === 34);
+console.log(fib(10) === 55);
+console.log(fib(11) === 89);
+console.log(fib(12) === 144);
