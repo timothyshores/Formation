bfed8965c58465e2067b28e1024ae5200705d25c
timothyshores <timothymshores@gmail.com>
2023-01-08 14:40:02 -0700

    Add fibonacci sequence problem under recursion directory

---
+++ Algorithms/Recursion/fibonacciSequence.js
@@ -0,0 +1,24 @@
+/* 
+
+Details
+The "Fibonacci Sequence" is a series of numbers in which each number is the sum of the two preceding numbers.
+
+For example, a simple sequence would look like 0, 1, 1, 2, 3, 5, 8, 13, 21...
+
+In our case, we want to find the n*th number in the sequence (starting at *0).
+
+Given any integer n, compute the value of the *n*th number in the Fibonacci Sequence.
+
+Examples:
+If n = 0, we should return 0
+If n = 2, we should return 1
+If n = 3, we should return 2
+If n = 8, we should return 21
+
+Advice / Tips
+Think about how to calculate each number based on the previous two
+At what point do you hit your base case?
+
+*/
+
+const fib = (i) => {};
