832df713b266280cd38cb3ab8b7941eea938e7cd
timothyshores <timothymshores@gmail.com>
2023-01-11 00:21:22 -0700

    Add test cases to find sum in array

--- Algorithms/Recursion/findSumInArray.js
+++ Algorithms/Recursion/findSumInArray.js
@@ -7,3 +7,21 @@ const recursiveSum = (arr, index = 0) => {
 	return arr[index] + recursiveSum(arr, index + 1);
 };
 
+// All test cases return true
+console.log(recursiveSum([]) === 0);
+console.log(recursiveSum([0]) === 0);
+console.log(recursiveSum([1]) === 1);
+console.log(recursiveSum([-1]) === -1);
+console.log(recursiveSum([0, 1]) === 1);
+console.log(recursiveSum([1, 0]) === 1);
+console.log(recursiveSum([1, 2]) === 3);
+console.log(recursiveSum([2, 1]) === 3);
+console.log(recursiveSum([1, -1]) === 0);
+console.log(recursiveSum([0, 0, 0]) === 0);
+console.log(recursiveSum([1, 2, 3]) === 6);
+console.log(recursiveSum([3, 2, 1]) === 6);
+console.log(recursiveSum([0, 0, 0, 0]) === 0);
+console.log(recursiveSum([-1, -2, -3]) === -6);
+console.log(recursiveSum([1, 2, 3, 4]) === 10);
+console.log(recursiveSum([1, 2, 3, 4, 5]) === 15);
+console.log(recursiveSum([-1, -2, -3, -4]) === -10);
