2554739b2631bcc03d85679cd5708967a6c0312c
timothyshores <timothymshores@gmail.com>
2023-01-11 00:10:43 -0700

    Create append to Linked List under recursion subdirectory

---
+++ Algorithms/Recursion/appendToLinkedList.js
@@ -0,0 +1,11 @@
+/*
+
+Given a linked list, append an element with a target value to the list recursively.
+
+Examples:
+• Given a linked list: 1 ➞ 4 ➞ 5, target: 7 // returns 1 ➞ 4 ➞ 5 ➞ 7
+• Given a linked list: 0, target 7 // returns 0 ➞ 7
+
+*/
+
+const append = (head, target) => {};
