7034a6990223dfef1c11b04ceba25839ddd49330
timothyshores <timothymshores@gmail.com>
2023-01-11 00:14:55 -0700

    Add find max value in linked list under recursion subdirectory

---
+++ Algorithms/Recursion/findMaxNodeInLL.js
@@ -0,0 +1,10 @@
+// Return the max value in a Linked List using recursion and no iterative loops
+
+class Node {
+	constructor(value, next = null) {
+		this.value = value;
+		this.next = next;
+	}
+}
+
+const findMaxNodeValue = (head) => {};
