f0e39ff5c9342b7440c06fae093e70830bd0481a
timothyshores <timothymshores@gmail.com>
2023-01-11 00:13:23 -0700

    Add test cases to get length of linked list recursively

--- Algorithms/Recursion/findLengthOfLinkedList.js
+++ Algorithms/Recursion/findLengthOfLinkedList.js
@@ -12,3 +12,17 @@ const getLengthOfLL = (head) => {
 	return 1 + getLengthOfLL(head.next);
 };
 
+// All test cases return true
+console.log(getLengthOfLL() === 0);
+console.log(getLengthOfLL(new Node(1)) === 1);
+console.log(getLengthOfLL(new Node(100)) === 1);
+console.log(getLengthOfLL(new Node(1, new Node(2))) === 2);
+console.log(getLengthOfLL(new Node(2, new Node(1))) === 2);
+console.log(getLengthOfLL(new Node(1, new Node(2, new Node(3)))) === 3);
+console.log(getLengthOfLL(new Node(3, new Node(2, new Node(1)))) === 3);
+console.log(
+	getLengthOfLL(new Node(1, new Node(2, new Node(3, new Node(4))))) === 4
+);
+console.log(
+	getLengthOfLL(new Node(4, new Node(3, new Node(2, new Node(1))))) === 4
+);
