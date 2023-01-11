c3e158089ad8416fd4e57157a189305a3f289cb7
timothyshores <timothymshores@gmail.com>
2023-01-11 00:21:00 -0700

    Implement find sum of array using recursion only and no iteration

--- Algorithms/Recursion/findSumInArray.js
+++ Algorithms/Recursion/findSumInArray.js
@@ -1,4 +1,9 @@
 // Recursive sum returns the sum of an array using recursion
 // Recursive sum may receive an empty array
 
-const recursiveSum = (arr, index = 0) => {};
+const recursiveSum = (arr, index = 0) => {
+	if (index === arr.length) return 0;
+
+	return arr[index] + recursiveSum(arr, index + 1);
+};
+
