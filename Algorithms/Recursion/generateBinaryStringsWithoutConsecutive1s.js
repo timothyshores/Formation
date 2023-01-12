8c4e4ff7880cd0ebeab7b59201caa731c2d12684
timothyshores <timothymshores@gmail.com>
2023-01-11 23:06:07 -0700

    Add generate binary strings without consecutive 1s under recursion

---
+++ Algorithms/Recursion/generateBinaryStringsWithoutConsecutive1s.js
@@ -0,0 +1,20 @@
+/*
+
+Given an integer *maxLen*, print all binary strings of size *maxLen* that don't have 1s next to each other. That is, no string should contain the substring 11, 111, 1111, 11111, etc. You can assume *maxLen* > 0.
+
+EXAMPLE(S)
+printBinaryWithoutConsecutive1s(maxLen=2)
+00
+01
+10
+
+printBinaryWithoutConsecutive1s(maxLen=3)
+000
+001
+010
+100
+101
+
+*/
+
+const printBinaryWithoutConsecutive1s = (maxLen) => {};
