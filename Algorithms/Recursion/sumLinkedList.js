07f7e89516b0c8ea715d504ffc3422427e251221
timothyshores <timothymshores@gmail.com>
2023-01-13 18:27:34 -0700

    Create and add sum linked list under recursion subdirectory

---
+++ Algorithms/Recursion/sumLinkedList.js
@@ -0,0 +1,16 @@
+/*
+
+Write a function called sumAllValues 
+sumAllValues receives a head node of a Linked List
+sumAllValues returns the sum of all values in a given Linked List
+
+*/
+
+class Node {
+	constructor(value, next = null) {
+		this.value = value;
+		this.next = next;
+	}
+}
+
+const sumAllValues = (head) => {};
