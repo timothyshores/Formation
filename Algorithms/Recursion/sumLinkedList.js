2a70c2d53e55fb2565c2bf2fb8e063a5f3b060a3
timothyshores <timothymshores@gmail.com>
2023-01-13 18:29:19 -0700

    Add test cases to sum all values in a linked list recursively

--- Algorithms/Recursion/sumLinkedList.js
+++ Algorithms/Recursion/sumLinkedList.js
@@ -18,3 +18,26 @@ const sumAllValues = (head) => {
 
 	return head.value + sumAllValues(head.next);
 };
+
+// All test cases return true
+console.log(sumAllValues() === 0);
+console.log(sumAllValues(new Node(1)) === 1);
+console.log(sumAllValues(new Node(0)) === 0);
+console.log(sumAllValues(new Node(2)) === 2);
+console.log(sumAllValues(new Node(-1)) === -1);
+console.log(sumAllValues(new Node(100)) === 100);
+console.log(sumAllValues(new Node(0, new Node(1))) === 1);
+console.log(sumAllValues(new Node(1, new Node(0))) === 1);
+console.log(sumAllValues(new Node(1, new Node(1))) === 2);
+console.log(sumAllValues(new Node(1, new Node(2))) === 3);
+console.log(sumAllValues(new Node(2, new Node(1))) === 3);
+console.log(sumAllValues(new Node(-1, new Node(1))) === 0);
+console.log(sumAllValues(new Node(-1, new Node(-1))) === -2);
+console.log(sumAllValues(new Node(500, new Node(500))) === 1000);
+console.log(sumAllValues(new Node(1, new Node(2, new Node(3)))) === 6);
+console.log(
+	sumAllValues(new Node(1, new Node(2, new Node(3, new Node(4))))) === 10
+);
+console.log(
+	sumAllValues(new Node(-1, new Node(-2, new Node(-3, new Node(-4))))) === -10
+);
