50295733eee76a987f439e410a99b2d6a93d016c
timothyshores <timothymshores@gmail.com>
2023-01-14 23:47:39 -0700

    Add bunny ears with a twist engineering method

---
+++ Algorithms/Recursion/bunnyEarsTwistEngineeringMethod.js
@@ -0,0 +1,33 @@
+/*
+❓ PROMPT
+We have bunnies standing in a line, numbered 1, 2, ... The odd bunnies (1, 3, ..) have the usual 2 ears. The even bunnies (2, 4, ..) we'll say have 3 ears because they each have a raised foot. Recursively return the number of "ears" in the bunny line 1, 2, ... n (without loops or multiplication).
+
+Example(s)
+bunnyEarsTwist(2) == 5
+bunnyEarsTwist(3) == 7
+bunnyEarsTwist(5) == 12
+
+🔎 EXPLORE
+State your assumptions & discoveries:
+
+Create examples & test cases:
+
+🧠 BRAINSTORM
+What approaches could work? Consider data structures or algorithmic patterns.
+Analyze the space & time complexity.
+Approach 1: 
+Time: O()
+Space: O()
+
+📆 PLAN
+High-level outline of approach #: 
+
+🛠️ IMPLEMENT */
+
+const bunnyEarsTwist = (bunnies) => {};
+
+/*
+🧪 VERIFY
+Run your examples & test cases.
+Methodically analyze and debug issue(s).
+*/
