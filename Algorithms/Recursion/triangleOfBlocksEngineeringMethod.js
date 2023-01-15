14926afa2d11a0f3f2f521cb54cf9d4aa506ddb7
timothyshores <timothymshores@gmail.com>
2023-01-15 13:17:16 -0700

    Add triangle of blocks with engineering method

---
+++ Algorithms/Recursion/triangleOfBlocksEngineeringMethod.js
@@ -0,0 +1,33 @@
+/*
+
+❓ PROMPT
+We have a triangle made of blocks. The topmost row has 1 block, the next row down has 2 blocks, the next row has 3 blocks, and so on. Compute recursively (no loops or multiplication) the total number of blocks in such a triangle with the given number of rows.
+
+Example(s)
+triangle(2) == 3
+triangle(3) == 6
+triangle(5) == 15
+
+🔎 EXPLORE
+State your assumptions & discoveries:
+
+Create examples & test cases:
+
+🧠 BRAINSTORM
+What approaches could work? Consider data structures or algorithmic patterns.
+Analyze the space & time complexity.
+Approach 1: 
+Time: O()
+Space: O()
+
+📆 PLAN
+High-level outline of approach #: 
+
+🛠️ IMPLEMENT */
+const triangle = (rows) => {};
+
+/*
+🧪 VERIFY
+Run your examples & test cases.
+Methodically analyze and debug issue(s).
+*/
