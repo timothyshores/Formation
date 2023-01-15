cebe082d11de954c166dbf4d157a415195eba9e7
timothyshores <timothymshores@gmail.com>
2023-01-14 22:57:14 -0700

    Add inside the parentheses with engineering method

---
+++ Algorithms/Recursion/insideTheParenthesesEngineeringMethod.js
@@ -0,0 +1,33 @@
+/*
+
+❓ PROMPT
+Given a string that contains exactly 1 pair of parentheses, compute recursively a new string made of only the parentheses and their contents, so "xyz(abc)123" yields "(abc)".
+
+Example(s)
+parenBit("xyz(abc)123") == "(abc)"
+parenBit("x(hello)") == "(hello)"
+parenBit("(xy)1") == "(xy)"
+
+🔎 EXPLORE
+State your assumptions & discoveries:
+
+Create examples & test cases:
+
+🧠 BRAINSTORM
+What approaches could work? Consider data structures or algorithmic patterns.
+Analyze the space & time complexity.
+Approach 1: 
+Time: O()
+Space: O()
+
+📆 PLAN
+High-level outline of approach #: 
+🛠️ IMPLEMENT */
+
+const parenBit = (word) => {};
+
+/*
+🧪 VERIFY
+Run your examples & test cases.
+Methodically analyze and debug issue(s).
+*/
