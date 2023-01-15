2d91ced4c6ab56eef314fe2e756e87adeefe21b4
timothyshores <timothymshores@gmail.com>
2023-01-14 18:16:42 -0700

    Add substring count with engineering method

---
+++ Algorithms/Recursion/substringCountEngineeringMethod.js
@@ -0,0 +1,33 @@
+/*
+
+❓ PROMPT
+Given a string and a non-empty substring **sub**, compute recursively the number of times that sub appears in the string, without the substrings overlapping.
+
+Example(s)
+strCount("catcowcat", "cat") == 2
+strCount("catcowcat", "cow") == 1
+strCount("catcowcat", "dog") == 0
+
+🔎 EXPLORE
+State your assumptions & discoveries:
+
+Create examples & test cases:
+
+🧠 BRAINSTORM
+What approaches could work? Consider data structures or algorithmic patterns.
+Analyze the space & time complexity.
+Approach 1: 
+Time: O()
+Space: O()
+
+📆 PLAN
+High-level outline of approach #: 
+
+🛠️ IMPLEMENT */
+const strCount = (word, sub) => {};
+
+/*
+🧪 VERIFY
+Run your examples & test cases.
+Methodically analyze and debug issue(s).
+*/
