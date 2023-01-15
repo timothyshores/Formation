d84587269de2d133ba76a1c3de0503e8420778d0
timothyshores <timothymshores@gmail.com>
2023-01-14 21:26:14 -0700

    Add bunny ears with engineering method

---
+++ Algorithms/Recursion/bunnyEarsEngineeringMethod.js
@@ -0,0 +1,32 @@
+/*
+❓ PROMPT
+We have a number of bunnies, each with two big floppy ears. We want to compute the total number of ears across all the bunnies recursively, without loops or multiplication.
+
+Example(s)
+bunnyEars(3) == 6
+bunnyEars(5) == 10
+
+🔎 EXPLORE
+State your assumptions & discoveries:
+
+Create examples & test cases:
+
+🧠 BRAINSTORM
+What approaches could work? Consider data structures or algorithmic patterns.
+Analyze the space & time complexity.
+Approach 1: 
+Time: O()
+Space: O()
+
+📆 PLAN
+High-level outline of approach #: 
+
+🛠️ IMPLEMENT */
+
+const bunnyEars = (bunnies) => {};
+
+/*
+🧪 VERIFY
+Run your examples & test cases.
+Methodically analyze and debug issue(s).
+*/
