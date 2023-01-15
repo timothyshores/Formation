ad2c302546aa1f32b73455598d47d09708ced251
timothyshores <timothymshores@gmail.com>
2023-01-14 22:57:51 -0700

    Add explore notes for inside the parentheses

--- Algorithms/Recursion/insideTheParenthesesEngineeringMethod.js
+++ Algorithms/Recursion/insideTheParenthesesEngineeringMethod.js
@@ -10,8 +10,26 @@ parenBit("(xy)1") == "(xy)"
 
 🔎 EXPLORE
 State your assumptions & discoveries:
+· We want to keep everything inside of the parenthesis including the opening and close parenthesis
 
 Create examples & test cases:
+
+console.log(parenBit("()") === "()");
+console.log(parenBit("()1") === "()");
+console.log(parenBit("1()") === "()");
+console.log(parenBit("(x)") === "(x)");
+console.log(parenBit("1()1") === "()");
+console.log(parenBit("(xy)1") === "(xy)");
+console.log(parenBit("(abc)") === "(abc)");
+console.log(parenBit("(abc)x") === "(abc)");
+console.log(parenBit("ab(ab)ab") === "(ab)");
+console.log(parenBit("ab(ab)ab") === "(ab)");
+console.log(parenBit("(abc)xyz") === "(abc)");
+console.log(parenBit("x(hello)") === "(hello)");
+console.log(parenBit("xyz(abc)123") === "(abc)");
+console.log(parenBit("res (ipsa) loquitor") === "(ipsa)");
+console.log(parenBit("not really (possible)") === "(possible)");
+console.log(parenBit("hello(not really)there") === "(not really)");
 
 🧠 BRAINSTORM
 What approaches could work? Consider data structures or algorithmic patterns.
