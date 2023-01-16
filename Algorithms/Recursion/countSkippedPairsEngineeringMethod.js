7562d166cd0ad88237db44d795227c9b2ed44f81
timothyshores <timothymshores@gmail.com>
2023-01-15 18:35:49 -0700

    Add count skipped pairs with engineering method

---
+++ Algorithms/Recursion/countSkippedPairsEngineeringMethod.js
@@ -0,0 +1,35 @@
+/*
+
+❓ PROMPT
+We'll say that a "skipped pair" in a string is two instances of a char separated by a char. So "AxA" the A's make a pair. Pair's can overlap, so "AxAxA" contains 3 skipped pairs -- 2 for A and 1 for x. Recursively compute the number of skipped pairs in the given string.
+
+Example(s)
+countSkippedPairs("axa") == 1
+countSkippedPairs("axax") == 2
+countSkippedPairs("aaa") == 1
+
+🔎 EXPLORE
+State your assumptions & discoveries:
+
+Create examples & test cases:
+
+🧠 BRAINSTORM
+What approaches could work? Consider data structures or algorithmic patterns.
+Analyze the space & time complexity.
+Approach 1: 
+Time: O()
+Space: O()
+
+📆 PLAN
+High-level outline of approach #: 
+
+🛠️ IMPLEMENT
+*/
+
+const countSkippedPairs = (word) => {};
+
+/*
+🧪 VERIFY
+Run your examples & test cases.
+Methodically analyze and debug issue(s).
+*/
