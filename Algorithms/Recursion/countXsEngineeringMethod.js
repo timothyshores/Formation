c59848ca3107749c4699b30fa7502c21755fef07
timothyshores <timothymshores@gmail.com>
2023-01-22 20:17:19 -0700

    Create count Xs engineering method in recursion subdirectory

---
+++ Algorithms/Recursion/countXsEngineeringMethod.js
@@ -0,0 +1,33 @@
+/*
+
+❓ PROMPT
+Given a string, compute recursively (no loops) the number of lowercase 'x' chars in the string.
+
+Example(s)
+countX("xxhixx") == 4
+countX("xhixhix") == 3
+countX("hi") == 0
+
+🔎 EXPLORE
+State your assumptions & discoveries:
+
+Create examples & test cases:
+
+🧠 BRAINSTORM
+What approaches could work? Consider data structures or algorithmic patterns.
+Analyze the space & time complexity.
+Approach 1: 
+Time: O()
+Space: O()
+
+📆 PLAN
+High-level outline of approach #: 
+
+🛠️ IMPLEMENT */
+const countX = (word) => {};
+
+/*
+🧪 VERIFY
+Run your examples & test cases.
+Methodically analyze and debug issue(s).
+*/
