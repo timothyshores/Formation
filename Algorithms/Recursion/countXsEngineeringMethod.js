3ccb4252a7f5013e664988e204fe0aa3db99fa9d
timothyshores <timothymshores@gmail.com>
2023-01-22 20:18:50 -0700

    Add explore notes and create example test cases for count Xs

--- Algorithms/Recursion/countXsEngineeringMethod.js
+++ Algorithms/Recursion/countXsEngineeringMethod.js
@@ -10,8 +10,34 @@ countX("hi") == 0
 
 🔎 EXPLORE
 State your assumptions & discoveries:
+· Return the number of lowwercase 'x's contained within a string
 
 Create examples & test cases:
+
+console.log(countX("") === 0);
+console.log(countX("a") === 0);
+console.log(countX("b") === 0);
+console.log(countX("c") === 0);
+console.log(countX("x") === 1);
+console.log(countX("y") === 0);
+console.log(countX("z") === 0);
+console.log(countX("X") === 0);
+console.log(countX("xa") === 1);
+console.log(countX("ax") === 1);
+console.log(countX("xx") === 2);
+console.log(countX("xxx") === 3);
+console.log(countX("xaxb") === 2);
+console.log(countX("axbx") === 2);
+console.log(countX("xxxx") === 4);
+console.log(countX("xxxxx") === 5);
+console.log(countX("xaxbxc") === 3);
+console.log(countX("axbxcx") === 3);
+console.log(countX("xaxbxcxd") === 4);
+console.log(countX("axbxcxdx") === 4);
+console.log(countX("xaxbxcxdxe") === 5);
+console.log(countX("axbxcxdxex") === 5);
+console.log(countX("xxxxxxxxxx") === 10);
+console.log(countX("abcdefghijklmnopqrstuvwyz") === 0);
 
 🧠 BRAINSTORM
 What approaches could work? Consider data structures or algorithmic patterns.
