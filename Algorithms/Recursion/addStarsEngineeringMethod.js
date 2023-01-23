2f27b2e40a8a9efd40a60ef48dfe08d07eb55597
timothyshores <timothymshores@gmail.com>
2023-01-22 17:18:48 -0700

    Implement add stars and verify with test cases

--- Algorithms/Recursion/addStarsEngineeringMethod.js
+++ Algorithms/Recursion/addStarsEngineeringMethod.js
@@ -9,6 +9,7 @@ addStars("ab") == "a*b"
 
 🔎 EXPLORE
 State your assumptions & discoveries:
+· Add a star in between every character excluding the empty string and single character strings
 · Input can be an empty string
 · There be capital letters, special characters, and numbers
 
@@ -33,7 +34,7 @@ console.log(addStars("hello") === "h*e*l*l*o");
 🧠 BRAINSTORM
 What approaches could work? Consider data structures or algorithmic patterns.
 Analyze the space & time complexity.
-Approach 1: Use recursion and a helper method to add a & between every character
+Approach 1: Use recursion and a helper method to add a * between every character
 Time: O(N)
 Space: O(N)
 
@@ -47,10 +48,40 @@ High-level outline of approach #: 1
 · Return withStars
 
 🛠️ IMPLEMENT */
-const addStars = (word) => {};
+const addStars = (word) => {
+	if (word === "") return "";
+	let withStars = "";
+
+	const helper = (index = 0) => {
+		withStars += index < word.length - 1 ? word[index] + "*" : word[index];
+		if (index == word.length - 1) return;
+		helper(index + 1);
+	};
+
+	helper();
+	return withStars;
+};
 
 /* 
 🧪 VERIFY
 Run your examples & test cases.
 Methodically analyze and debug issue(s).
 */
+
+// All test cases return true
+console.log(addStars("") === "");
+console.log(addStars("1") === "1");
+console.log(addStars("2") === "2");
+console.log(addStars("3") === "3");
+console.log(addStars("a") === "a");
+console.log(addStars("A") === "A");
+console.log(addStars("z") === "z");
+console.log(addStars("Z") === "Z");
+console.log(addStars("12") === "1*2");
+console.log(addStars("23") === "2*3");
+console.log(addStars("ab") === "a*b");
+console.log(addStars("ba") === "b*a");
+console.log(addStars("AA") === "A*A");
+console.log(addStars("123") === "1*2*3");
+console.log(addStars("abc") === "a*b*c");
+console.log(addStars("hello") === "h*e*l*l*o");
