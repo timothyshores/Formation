3380ef177defca8f838472acedb1dbe9604142ed
timothyshores <timothymshores@gmail.com>
2023-01-22 17:06:05 -0700

    Create add stars with engineering method and add under recursion folder

---
+++ Algorithms/Recursion/addStarsEngineeringMethod.js
@@ -0,0 +1,32 @@
+/*
+❓ PROMPT
+Given a string, insert a star (*) between each character.
+
+Example(s)
+addStars("hello") == "h*e*l*l*o"
+addStars("abc") == "a*b*c"
+addStars("ab") == "a*b"
+
+🔎 EXPLORE
+State your assumptions & discoveries:
+
+Create examples & test cases:
+
+🧠 BRAINSTORM
+What approaches could work? Consider data structures or algorithmic patterns.
+Analyze the space & time complexity.
+Approach 1: 
+Time: O()
+Space: O()
+
+📆 PLAN
+High-level outline of approach #: 
+
+🛠️ IMPLEMENT */
+const addStars = (word) => {};
+
+/* 
+🧪 VERIFY
+Run your examples & test cases.
+Methodically analyze and debug issue(s).
+*/
