fef21b843361b2c4be0efc2d837574881c68abd1
timothyshores <timothymshores@gmail.com>
2023-01-22 17:06:28 -0700

    Add explore notes and create example test cases

--- Algorithms/Recursion/addStarsEngineeringMethod.js
+++ Algorithms/Recursion/addStarsEngineeringMethod.js
@@ -9,8 +9,26 @@ addStars("ab") == "a*b"
 
 🔎 EXPLORE
 State your assumptions & discoveries:
+· Input can be an empty string
+· There be capital letters, special characters, and numbers
 
 Create examples & test cases:
+console.log(addStars("") === "");
+console.log(addStars("1") === "1");
+console.log(addStars("2") === "2");
+console.log(addStars("3") === "3");
+console.log(addStars("a") === "a");
+console.log(addStars("A") === "A");
+console.log(addStars("z") === "z");
+console.log(addStars("Z") === "Z");
+console.log(addStars("12") === "1*2");
+console.log(addStars("23") === "2*3");
+console.log(addStars("ab") === "a*b");
+console.log(addStars("ba") === "b*a");
+console.log(addStars("AA") === "A*A");
+console.log(addStars("123") === "1*2*3");
+console.log(addStars("abc") === "a*b*c");
+console.log(addStars("hello") === "h*e*l*l*o");
 
 🧠 BRAINSTORM
 What approaches could work? Consider data structures or algorithmic patterns.
