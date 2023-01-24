c66d4b4a85047bb1f8052b96d52d7bfac6262c6b
timothyshores <timothymshores@gmail.com>
2023-01-24 11:01:08 -0700

    Add linked list frequency counter under recursion subdirectory

---
+++ Algorithms/Recursion/linkedListFrequencyCounter.js
@@ -0,0 +1,10 @@
+// Prompt Given a linked list and an integer, return how many times the integer exists in the list.
+
+class Node {
+	constructor(value, next = null) {
+		this.value = value;
+		this.next = next;
+	}
+}
+
+const linkedListFrequencyCounter = (list, int) => {};
