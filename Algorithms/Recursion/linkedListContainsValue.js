fdc553350542ecbfc2c70127743f230525f3ba02
timothyshores <timothymshores@gmail.com>
2023-01-24 11:05:06 -0700

    Add linked list contains value under recursion subdirectory

---
+++ Algorithms/Recursion/linkedListContainsValue.js
@@ -0,0 +1,10 @@
+// Prompt: Given a linked list and an integer, find whether the integer exists in the list. Return a boolean.
+
+class Node {
+	constructor(value, next = null) {
+		this.value = value;
+		this.next = next;
+	}
+}
+
+const linkedListContainsValue = (node, int) => {};
