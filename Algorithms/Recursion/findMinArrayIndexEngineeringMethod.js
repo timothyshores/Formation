6f56b4d2205428f7d5a8209099ca869d6c2f24f0
timothyshores <timothymshores@gmail.com>
2023-01-24 11:10:04 -0700

    Add find min array index with engineering method

---
+++ Algorithms/Recursion/findMinArrayIndexEngineeringMethod.js
@@ -0,0 +1,32 @@
+/*
+
+❓ PROMPT
+Given an array, write a recursive function to find the index of the minimum element in an array. If there's a tie-breaker, return the first occurrence.
+
+Example(s)
+findMinIndex([12, 1234, 45, 67, 1]) == 4
+findMinIndex([10, 20, 30]) == 0
+findMinIndex([8, 6, 7, 5, 3, 7]) == 4
+
+🔎 EXPLORE
+State your assumptions & discoveries:
+
+Create examples & test cases:
+
+🧠 BRAINSTORM
+What approaches could work? Consider data structures or algorithmic patterns.
+Analyze the space & time complexity.
+Approach 1: 
+Time: O()
+Space: O()
+
+📆 PLAN
+High-level outline of approach #: 
+🛠️ IMPLEMENT */
+const findMinIndex = (arr) => {};
+
+/*
+🧪 VERIFY
+Run your examples & test cases.
+Methodically analyze and debug issue(s).
+*/
