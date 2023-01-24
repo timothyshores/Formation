09c80b7bf0cd7b9694168f06288c5d1164b03244
timothyshores <timothymshores@gmail.com>
2023-01-24 11:23:34 -0700

    Add engineering method template

---
+++ Lecture Notes/engineringMethodTemplate.js
@@ -0,0 +1,33 @@
+/*
+❓ PROMPT
+
+Example(s)
+
+🔎 EXPLORE
+State your assumptions & discoveries:
+· 
+
+Create examples & test cases:
+
+🧠 BRAINSTORM
+What approaches could work? Consider data structures or algorithmic patterns.
+Analyze the space & time complexity.
+
+Approach 1: 
+Time: 
+Space: 
+
+📆 PLAN
+High-level outline of approach #: 1
+· 
+
+🛠️ IMPLEMENT */
+const functionName = () => {};
+
+/* 
+🧪 VERIFY
+Run your examples & test cases.
+Methodically analyze and debug issue(s).
+*/
+
+// All test cases return true
