fb78d8774e48f455a7ea8f29d2fa5f0c5f221839
timothyshores <timothymshores@gmail.com>
2023-01-24 11:15:12 -0700

    Add change pi with engineering method under recursion subdirectory

---
+++ Algorithms/Recursion/changePiEngineeringMethod.js
@@ -0,0 +1,33 @@
+/*
+❓ PROMPT
+Given a string, compute recursively (no loops) a new string where all appearances of "pi" have been replaced by "3.14".
+
+Example(s)
+changePi("xpix") == "x3.14x"
+changePi("pipi") == "3.143.14"
+changePi("pip") == "3.14p"
+
+🔎 EXPLORE
+State your assumptions & discoveries:
+
+Create examples & test cases:
+
+🧠 BRAINSTORM
+What approaches could work? Consider data structures or algorithmic patterns.
+Analyze the space & time complexity.
+Approach 1: 
+Time: ON)
+Space: O()
+
+📆 PLAN
+High-level outline of approach #: 
+
+🛠️ IMPLEMENT */
+
+const changePi = (word) => {};
+
+/*
+🧪 VERIFY
+Run your examples & test cases.
+Methodically analyze and debug issue(s).
+*/
