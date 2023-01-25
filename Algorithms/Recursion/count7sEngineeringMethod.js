e062332d07ada1d585bd61d05316a4606cc726aa
timothyshores <timothymshores@gmail.com>
2023-01-25 01:04:06 -0700

    Add count 7s with engineering method under recursion directory

---
+++ Algorithms/Recursion/count7sEngineeringMethod.js
@@ -0,0 +1,37 @@
+/*
+❓ PROMPT
+Given a non-negative int n, return the count of the occurrences of 7 as a digit, so for example 717 yields 2. (no loops).
+
+Note that mod (%) by 10 yields the rightmost digit (126 % 10 is 6), while integer division by 10 removes the rightmost digit (126 / 10 is 12). Be aware that some languages require some special handling to do integer division while others do not.
+
+*Python integer division*: x // y
+*Javascript integer division*: Math.floor(x / y)
+
+Example(s)
+count7(7) == 1
+count7(123) == 0
+count7(717) == 2
+
+🔎 EXPLORE
+State your assumptions & discoveries:
+
+Create examples & test cases:
+
+🧠 BRAINSTORM
+What approaches could work? Consider data structures or algorithmic patterns.
+Analyze the space & time complexity.
+Approach 1:
+Time: 
+Space: 
+
+📆 PLAN
+High-level outline of approach #: 
+
+🛠️ IMPLEMENT */
+const count7 = (n) => {};
+
+/*
+🧪 VERIFY
+Run your examples & test cases.
+Methodically analyze and debug issue(s).
+*/
