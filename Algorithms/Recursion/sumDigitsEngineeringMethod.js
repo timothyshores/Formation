e4f8f97a1437a392171045e97e5ca2907b052d88
timothyshores <timothymshores@gmail.com>
2023-01-25 01:15:26 -0700

    Add sum digits with engineering method under recursion subdirectory

---
+++ Algorithms/Recursion/sumDigitsEngineeringMethod.js
@@ -0,0 +1,35 @@
+/*
+
+❓ PROMPT
+Given a non-negative int n, return the sum of its digits recursively (no loops). Note that mod (%) by 10 yields the rightmost digit (126 % 10 is 6), while divide (/) by 10 removes the rightmost digit (126 / 10 is 12).
+
+Example(s)
+sumDigits(12) == 3
+sumDigits(49) == 13
+sumDigits(126) == 9
+
+🔎 EXPLORE
+State your assumptions & discoveries:
+
+Create examples & test cases:
+
+🧠 BRAINSTORM
+What approaches could work? Consider data structures or algorithmic patterns.
+Analyze the space & time complexity.
+Approach 1: 
+Time: 
+Space: 
+
+📆 PLAN
+High-level outline of approach #: 
+
+🛠️ IMPLEMENT */
+const sumDigits = (n) => {};
+
+/*
+🧪 VERIFY
+Run your examples & test cases.
+Methodically analyze and debug issue(s).
+*/
+
+// All test cases return true
