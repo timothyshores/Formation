aaebcc8183c6a0aac5dd9faa4c7886814c1af618
timothyshores <timothymshores@gmail.com>
2023-01-26 15:12:29 -0700

    Implement password generator and add notes

--- Algorithms/Recursion/passwordGenerator.js
+++ Algorithms/Recursion/passwordGenerator.js
@@ -49,13 +49,49 @@ Space: O(N)
 
 📆 PLAN
 High-level outline of approach #: 1
+· Use a for loop to recursively call the function until our strings get to max length
 
 🛠️ IMPLEMENT */
 
-const generatePasswords = (validChars, minLen, maxLen) => {};
+const generatePasswords = (validChars, minLen, maxLen) => {
+	let passwords = [];
+
+    const generator = (password = "", passwordLength = 0) => {
+		if (passwordLength > maxLen) return;
+
+		if (passwordLength >= minLen && passwordLength <= maxLen) passwords.push(password);
+
+		for (const currentChar of validChars) {
+			generator(password + currentChar, passwordLength + 1);
+		}
+	};
+
+	generator();
+	return passwords;
+};
 
 /*
 🧪 VERIFY
 Run your examples & test cases.
 Methodically analyze and debug issue(s).
 */
+
+console.log(generatePasswords(["a"], 0, 0)); // [""]
+console.log(generatePasswords(["a", "b"], 0, 0)); // [""]
+console.log(generatePasswords(["a"], 0, 1)); // ["", "a"]
+console.log(generatePasswords(["a"], 1, 2)); // ["a", "aa"]
+console.log(generatePasswords(["a"], 2, 3)); // ["aa", "aaa"]
+console.log(generatePasswords(["a"], 0, 2)); // ["", "a", "aa"]
+console.log(generatePasswords(["a", "b"], 1, 1)); // ["a", "b"]
+console.log(generatePasswords(["a"], 1, 3)); // ["a", "aa", "aaa"]
+console.log(generatePasswords(["a", "b", "c", "d"], 0, 0)); // ['']
+console.log(generatePasswords(["a"], 2, 4)); // ["aa", "aaa", "aaaa"]
+console.log(generatePasswords(["a"], 0, 3)); // ["", "a", "aa", "aaa"]
+console.log(generatePasswords(["a", "b"], 2, 2)); // ["aa", "ab", "ba", "bb"]
+console.log(generatePasswords(["a", "b", "c", "d"], 3, 4).length === 320); // true
+console.log(generatePasswords(["a", "b", "c", "d"], 3, 5).length === 1344); // true
+console.log(generatePasswords(["a", "b", "c", "d"], 1, 1)); // ['a', 'b', 'c', 'd']
+console.log(generatePasswords(["a", "b", "c", "d"], 0, 1)); // ['', 'a', 'b', 'c', 'd']
+console.log(generatePasswords(["a", "b"], 3, 3)); // ["aaa", "aab", "aba", "abb", "baa", "bab", "bba", "bbb"]
+console.log(generatePasswords(["a", "b", "c"], 2, 3)); // ["aa","aaa","aab","aac","ab","aba","abb","abc","ac","aca","acb","acc","ba","baa","bab","bac","bb","bba","bbb","bbc","bc","bca","bcb","bcc","ca","caa","cab","cac","cb","cba","cbb","cbc","cc","cca","ccb","ccc"'
+console.log(generatePasswords(["a", "b", "c", "d"], 2, 3)); // ["aa","aaa","aab","aac","aad","ab","aba","abb","abc","abd","ac","aca","acb","acc","acd","ad","ada","adb","adc","add","ba","baa","bab","bac","bad","bb","bba","bbb","bbc","bbd","bc","bca","bcb","bcc","bcd","bd","bda","bdb","bdc","bdd","ca","caa","cab","cac","cad","cb","cba","cbb","cbc","cbd","cc","cca","ccb","ccc","ccd","cd","cda","cdb","cdc","cdd","da","daa","dab","dac","dad","db","dba","dbb","dbc","dbd","dc","dca","dcb","dcc","dcd","dd","dda","ddb","ddc","ddd"]
