aebc670e8821a3fdcce76db334e1b9d40b4f7948
timothyshores <timothymshores@gmail.com>
2023-01-26 15:11:20 -0700

    Add password generator under recursion subdirectory

---
+++ Algorithms/Recursion/passwordGenerator.js
@@ -0,0 +1,45 @@
+/*
+
+❓ PROMPT
+Given a set of characters, a minimum length, and a maximum length, generate all strings that can be created using characters from the set between those lengths inclusively.
+
+This algorithm requires a large time and space complexity to enumerate all the possibilities. You should be able to get this to either time out or run out of memory even on rather small lengths.
+
+Example(s)
+generatePasswords(["a"], 2, 4) === ["aa","aaa","aaaa"]
+
+generatePasswords(["a", "b", "c"], 2, 3) === [
+"aa","aaa","aab","aac",
+"ab","aba","abb","abc",
+"ac","aca","acb","acc",
+"ba","baa","bab","bac",
+"bb","bba","bbb","bbc",
+"bc","bca","bcb","bcc",
+"ca","caa","cab","cac",
+"cb","cba","cbb","cbc",
+"cc","cca","ccb","ccc"]
+
+🔎 EXPLORE
+State your assumptions & discoveries:
+
+Create examples & test cases:
+
+🧠 BRAINSTORM
+What approaches could work? Consider data structures or algorithmic patterns.
+Analyze the space & time complexity.
+Approach 1:
+Time: 
+Space: 
+
+📆 PLAN
+High-level outline of approach #: 1
+
+🛠️ IMPLEMENT */
+
+const generatePasswords = (validChars, minLen, maxLen) => {};
+
+/*
+🧪 VERIFY
+Run your examples & test cases.
+Methodically analyze and debug issue(s).
+*/
