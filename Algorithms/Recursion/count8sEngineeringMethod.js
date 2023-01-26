fa82cc0d4e8b6845adad9dc4db59ee9c6c03a169
timothyshores <timothymshores@gmail.com>
2023-01-26 06:39:28 -0700

    Add count 8s with engineering method

---
+++ Algorithms/Recursion/count8sEngineeringMethod.js
@@ -0,0 +1,36 @@
+/*
+
+❓ PROMPT
+Given a non-negative int n, compute recursively (no loops) the number of occurrences of 8 as a digit, except that an 8 with another 8 immediately to its left counts double, so 8818 yields 4 (because 2+1+0+1).
+
+Note that mod (%) by 10 yields the rightmost digit (126 % 10 is 6), while dividing (/) by 10 removes the rightmost digit (126 / 10 is 12).
+
+Example(s)
+count8(8) == 1
+count8(818) == 2
+count8(8818) == 4  (because 2+1+0+1)
+
+🔎 EXPLORE
+State your assumptions & discoveries:
+
+Create examples & test cases:
+
+🧠 BRAINSTORM
+What approaches could work? Consider data structures or algorithmic patterns.
+Analyze the space & time complexity.
+Approach 1: 
+Time: 
+Space: 
+
+📆 PLAN
+High-level outline of approach #: 
+
+🛠️ IMPLEMENT */
+
+const count8 = (n) => {};
+
+/*
+🧪 VERIFY
+Run your examples & test cases.
+Methodically analyze and debug issue(s).
+*/
