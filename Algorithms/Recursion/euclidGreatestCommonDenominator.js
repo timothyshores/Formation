308bbf7bd699a286fba260d5bc9b28df3d2983d5
timothyshores <timothymshores@gmail.com>
2023-01-26 22:25:22 -0700

    Add explore and brainstorm notes and example tests for Euclid GCD

--- Algorithms/Recursion/euclidGreatestCommonDenominator.js
+++ Algorithms/Recursion/euclidGreatestCommonDenominator.js
@@ -20,15 +20,27 @@ euclidGCD(3, 1) == 1
 
 🔎 EXPLORE
 State your assumptions & discoveries:
+· Want to find the greatest common denominator given to integers
 
 Create examples & test cases:
+
+euclidGCD(1, 1) === 1
+euclidGCD(50, 1) === 1
+euclidGCD(50, 2) === 2
+euclidGCD(50, 4) === 2
+euclidGCD(31, 19) === 1
+euclidGCD(10, 10) === 10
+euclidGCD(23, 23) === 23
+euclidGCD(25, 50) === 25
+euclidGCD(50, 25) === 25
+euclidGCD(100, 50) === 50
 
 🧠 BRAINSTORM
 What approaches could work? Consider data structures or algorithmic patterns.
 Analyze the space & time complexity.
-Approach 1: 
-Time: 
-Space: 
+Approach 1: Use recursion 
+Time: O(log n) 
+Space: O(log n)
 
 📆 PLAN
 High-level outline of approach #: 
