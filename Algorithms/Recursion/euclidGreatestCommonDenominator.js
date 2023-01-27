e6d4b7334b2dc9a22fc1daa380aa428edbc375d6
timothyshores <timothymshores@gmail.com>
2023-01-26 22:23:27 -0700

    Add Euclid greatest common denominators to recursion subdirectory

---
+++ Algorithms/Recursion/euclidGreatestCommonDenominator.js
@@ -0,0 +1,43 @@
+/*
+
+❓ PROMPT
+Euclid's algorithm for finding the greatest common divisor (GCD) of two numbers *a* and *b* is:
+
+euclidGCD(a, 0) := a
+euclidGCD(a, b) := gcd(b, a mod b) 
+
+Write a function that implements Euclid's algorithm and finds the GCD of two numbers. 
+
+Check out this video https://www.youtube.com/watch?v=Jwf6ncRmhPg
+for more information on this very old algorithm.
+
+Wikipedia https://en.wikipedia.org/wiki/Euclidean_algorithm also has a formal write-up.
+
+Example(s)
+euclidGCD(9, 27) == 9
+euclidGCD(27, 9) == 9
+euclidGCD(3, 1) == 1
+
+🔎 EXPLORE
+State your assumptions & discoveries:
+
+Create examples & test cases:
+
+🧠 BRAINSTORM
+What approaches could work? Consider data structures or algorithmic patterns.
+Analyze the space & time complexity.
+Approach 1: 
+Time: 
+Space: 
+
+📆 PLAN
+High-level outline of approach #: 
+
+🛠️ IMPLEMENT */
+const euclidGCD = (a, b) => {};
+
+/*
+🧪 VERIFY
+Run your examples & test cases.
+Methodically analyze and debug issue(s).
+*/
