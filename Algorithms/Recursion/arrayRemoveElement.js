00443379fd96c9e32a9862567e7f88153408940c
timothyshores <timothymshores@gmail.com>
2023-02-18 22:36:43 -0700

    Add array remove element and explore notes under recursion subdirectory

---
+++ Algorithms/Recursion/arrayRemoveElement.js
@@ -0,0 +1,21 @@
+/*
+
+❓ PROMPT 
+Given an array are a target value write a function that returns the array without the specified target value
+
+Example(s)
+console.log(removeElement([1], 1)); // []
+
+🔎 EXPLORE
+State your assumptions & discoveries:
+· Want to return an array without the specific target value
+· Similar to the built in .filter() method for arrays arr.filter(el => el !== targetValue);
+
+🛠️ IMPLEMENT */
+const removeElement = (arr, el) => {};
+
+/* 
+🧪 VERIFY
+Run your examples & test cases.
+Methodically analyze and debug issue(s).
+*/
