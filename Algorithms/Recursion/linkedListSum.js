9076afe58718098f267d41179b301c5ba33b22aa
timothyshores <timothymshores@gmail.com>
2023-02-18 17:56:46 -0700

    Add linked list sum under recursion subdirectory along with starter code

---
+++ Algorithms/Recursion/linkedListSum.js
@@ -0,0 +1,12 @@
+// PROMPT: Given the head of a linked list of nodes containing integers.
+// Write a function called sumList the returns the sum of all nodes in the Linked List using recursion
+// The function should return 0 is the value of the head node is null
+
+class Node {
+	constructor(value, next = null) {
+		this.value = value;
+		this.next = next;
+	}
+}
+
+const sumList = (head) => {};
