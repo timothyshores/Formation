455746ab7bbc320ea73654c765574fbfacb7cfdd
timothyshores <timothymshores@gmail.com>
2023-02-18 17:20:34 -0700

    Add array frequency counter in recursion subdirectory

---
+++ Algorithms/Recursion/arrayFrequencyCounter.js
@@ -0,0 +1,12 @@
+/*
+Given an integer array and an integer, return how many times the integer exists in the array.
+
+- return integer (count)
+- if given empty array, return 0
+
+Find mean of an integer array
+Replace all negative values with a 0
+Reverse the values in an array
+ */
+
+const countInteger = (array, target, index = 0) => {};
