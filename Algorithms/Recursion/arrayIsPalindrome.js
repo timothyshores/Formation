e709d6d3c49e11331aa14c8bbd54e8355f30aaa1
timothyshores <timothymshores@gmail.com>
2023-02-18 23:02:16 -0700

    Implement array is palindrome using recursion and verify with tests

--- Algorithms/Recursion/arrayIsPalindrome.js
+++ Algorithms/Recursion/arrayIsPalindrome.js
@@ -5,4 +5,20 @@
 // Continue moving points in until we finish comparing the final middle elements
 // If values are unequal return false
 
-const isPalindrome = (arr) => {};
+const isPalindrome = (arr) => {
+	if (arr.length < 2) return true;
+	if (arr.shift() !== arr.pop()) return false;
+	return isPalindrome(arr);
+};
+
+// All test cases return true
+console.log(isPalindrome([]));
+console.log(isPalindrome([1]));
+console.log(isPalindrome([1, 1]));
+console.log(!isPalindrome([1, 2]));
+console.log(isPalindrome([1, 1, 1]));
+console.log(isPalindrome([1, 2, 1]));
+console.log(isPalindrome([2, 1, 2]));
+console.log(!isPalindrome([1, 2, 3]));
+console.log(isPalindrome([1, 2, 3, 2, 1]));
+console.log(isPalindrome([3, 2, 1, 2, 3]));
