5e59f6359849447395da19e139b50e192aa313b1
timothyshores <timothymshores@gmail.com>
2023-02-18 22:15:45 -0700

    Add find min and max in array under recursion subdirectory

---
+++ Algorithms/Recursion/findMinAndMaxInAnArray.js
@@ -0,0 +1,39 @@
+/*
+'''
+❓ PROMPT
+Given an array, write 2 recursive functions to find the minimum and maximum element in an array. You may use the *min(a,b)* and *max(a,b)* functions to shorthand picking the minimum and maximum between 2 values.
+
+Example(s)
+findMin([12, 1234, 45, 67, 1]) == 1
+findMin([10, 20, 30]) == 10
+findMin([8, 6, 7, 5, 3, 7]) == 3
+
+findMax([12, 1234, 45, 67, 1]) == 1234
+findMax([10, 20, 30]) == 30
+findMax([8, 6, 7, 5, 3, 7]) == 8
+
+🔎 EXPLORE
+State your assumptions & discoveries:
+
+Create examples & test cases:
+
+🧠 BRAINSTORM
+What approaches could work? Consider data structures or algorithmic patterns.
+Analyze the space & time complexity.
+Approach 1: 
+Time: O()
+Space: O()
+
+📆 PLAN
+High-level outline of approach #: 
+
+🛠️ IMPLEMENT */
+const findMin = (arr) => {};
+
+const findMax = (arr) => {};
+
+/*
+🧪 VERIFY
+Run your examples & test cases.
+Methodically analyze and debug issue(s).
+*/
