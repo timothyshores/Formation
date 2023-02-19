9306c2cbddbe55c3eb99295416c2d02cdeddd0d5
timothyshores <timothymshores@gmail.com>
2023-02-18 23:01:41 -0700

    Add array is palindrome under recursion subdirectory

---
+++ Algorithms/Recursion/arrayIsPalindrome.js
@@ -0,0 +1,8 @@
+// Prompt: Check if an array of integers is a palindrome
+
+// Compare first and last elements
+// If they are the same then move the pointers inward one index
+// Continue moving points in until we finish comparing the final middle elements
+// If values are unequal return false
+
+const isPalindrome = (arr) => {};
