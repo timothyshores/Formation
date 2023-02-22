69512c2519f3ef41d2065c0402fd4e0d7bb122a5
timothyshores <timothymshores@gmail.com>
2023-02-21 18:23:40 -0700

    Add notes for approaches 1 & 2 in brainstorm section of find left peak

--- Data Structures/Arrays/findLeftPeaks.js
+++ Data Structures/Arrays/findLeftPeaks.js
@@ -29,6 +29,21 @@ console.log(find_left_peaks([3, 2, 1])); // [3, 2, 1]
 What approaches could work? Consider data structures or algorithmic patterns.
 Analyze the space & time complexity.
 
+Approach 1: 
+
+Brute force approach at every element we check every element to it's right
+If the current element is greater than all other elements to it's right then push it into the left peaks results array
+Check each element one by one from the left most element at index 0 to the right most elements at index arr.length - 1
+Return the final left peaks results array
+
+Time: O(N^2) for the first element we need to check n - 1 elements to it's right then n - 2, n - 3, ...., 3, 2, 1, 0 elements
+Space: O(N) only store the final left peaks results array in memory
+
+Approach 2: Create another array that matches the input array arr and store the current maximum of all elements to it's right and compare to input array arr
+
+Time: O(N) Technically 2N because we need to iterate through the input array in reverse but linear time is far better than quadratic time in brute force solution
+Space: O(N) Need N space for the precomputed maximums from the right to the left in addition to the final left peaks array
+
 📆 PLAN
 High-level outline of approach #: 2
 
