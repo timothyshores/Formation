da2a09a8aed44efd69832bd55a2db6411104bce9
timothyshores <timothymshores@gmail.com>
2023-02-21 18:22:04 -0700

    Add find left peak with engineering method under array subdirectory

---
+++ Data Structures/Arrays/findLeftPeaks.js
@@ -0,0 +1,31 @@
+/*
+
+❓ PROMPT
+Given an array of integers, return a sub-array of 'Left Peaks'. A Left Peak is defined as an element that is greater or equal in value to all elements to its right.
+
+Example(s)
+find_left_peaks([1, 2, 5, 3, 1, 2]) => [5, 3, 2]
+find_left_peaks([3, 2, 1]) => [3, 2, 1]
+
+🔎 EXPLORE
+State your assumptions & discoveries:
+
+Create examples & test cases:
+
+🧠 BRAINSTORM
+What approaches could work? Consider data structures or algorithmic patterns.
+Analyze the space & time complexity.
+
+📆 PLAN
+High-level outline of approach #: 2
+
+🛠️ IMPLEMENT */
+const find_left_peaks = (arr) => {};
+
+/*
+
+🧪 VERIFY
+Run your examples & test cases.
+Methodically analyze and debug issue(s).
+
+*/
