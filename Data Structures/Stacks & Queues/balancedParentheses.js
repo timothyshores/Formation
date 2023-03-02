9a3d9ac1f96bebbeb81a0f9f69b45b629c29cdde
timothyshores <timothymshores@gmail.com>
2023-03-01 23:54:10 -0700

    Create balanced parentheses under stacks and queues subdirectory

---
+++ Data Structures/Stacks & Queues/balancedParentheses.js
@@ -0,0 +1,60 @@
+/*
+
+PROMPT: 
+
+Write a function called isBalanced that receives a string parameter str
+The string parameter consistences only on opening parenthesis '(' and closing ')' characters
+A balanced parenthesis is the combination '()'
+Return true if the string only contains balanced parentheses of  
+
+Examples Test Cases Return True
+
+console.log(isBalanced(""));
+console.log(isBalanced("()"));
+console.log(isBalanced("(())"));
+console.log(isBalanced("()()"));
+console.log(isBalanced("(()())"));
+console.log(isBalanced("((()))"));
+console.log(isBalanced("()()()"));
+console.log(isBalanced("(()())"));
+console.log(isBalanced("(())(())"));
+console.log(isBalanced("()(())()"));
+console.log(isBalanced("()((()))"));
+console.log(isBalanced("()(())(())"));
+console.log(isBalanced("((()))((()))"));
+console.log(isBalanced("(())(())(())"));
+console.log(isBalanced("()()()()()(((((())))))"));
+console.log(isBalanced("()()()()()(((((())))))"));
+
+Examples Test Cases Return False
+
+console.log(isBalanced("("));
+console.log(isBalanced(")"));
+console.log(isBalanced(")("));
+console.log(isBalanced("())"));
+console.log(isBalanced("(()"));
+console.log(isBalanced("(("));
+console.log(isBalanced("))"));
+console.log(isBalanced("))("));
+console.log(isBalanced("((("));
+console.log(isBalanced(")))"));
+console.log(isBalanced("))(("));
+console.log(isBalanced("(((("));
+console.log(isBalanced("(((()"));
+console.log(isBalanced("(((())"));
+console.log(isBalanced("()))))"));
+console.log(isBalanced("(()))))"));
+console.log(isBalanced("(()(()("));
+console.log(isBalanced("((())))))"));
+console.log(isBalanced("(((((((((((((("));
+console.log(isBalanced("))))))))))))"));
+
+Approach 1: 
+
+Time complexity: 
+Space complexity:
+
+
+*/
+
+const isBalanced = (str) => {};
