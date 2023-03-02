6c71eb13a2fff2632df88f4a45051aba53dbe6ef
timothyshores <timothymshores@gmail.com>
2023-03-01 23:59:19 -0700

    Add approach 1 notes and space time complexity to balanced parentheses

--- Data Structures/Stacks & Queues/balancedParentheses.js
+++ Data Structures/Stacks & Queues/balancedParentheses.js
@@ -49,11 +49,16 @@ console.log(isBalanced("((())))))"));
 console.log(isBalanced("(((((((((((((("));
 console.log(isBalanced("))))))))))))"));
 
-Approach 1: 
+Approach 1: Use a stack
 
-Time complexity: 
-Space complexity:
+If string length is 0 or odd return false
+Initialize an empty stack using built in JS array
+Iterate through the input string str
+If we find an opening bracket append a closing bracket to the stack
+If we encounter a closing bracket pop from the stack and check if it's a closing bracket
 
+Time complexity: O(N) iterates through the string
+Space complexity: O(N) store up to str.length - 1 characters in the stack
 
 */
 
