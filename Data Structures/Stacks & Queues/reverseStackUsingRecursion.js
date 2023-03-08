b91427e8cf4e79e6f39a78446e1961deece55a02
timothyshores <timothymshores@gmail.com>
2023-03-08 15:29:30 -0700

    Implement reverse stack using recursion

--- Data Structures/Stacks & Queues/reverseStackUsingRecursion.js
+++ Data Structures/Stacks & Queues/reverseStackUsingRecursion.js
@@ -1,3 +1,17 @@
 // Prompt: Reverse a stack using recursion
 
-const reverseStack = (stack) => {};
+const reverseStack = (stack) => {
+	if (stack.length < 2) return stack;
+
+	const result = [];
+
+	const helper = () => {
+		if (stack.length) {
+			result.push(stack.pop());
+			helper();
+		}
+	};
+
+	helper();
+	return result;
+};
