a81f27acb11295a5550b76ad39282bec5f66766a
timothyshores <timothymshores@gmail.com>
2023-03-08 15:30:26 -0700

    Verify reverse stack using recursion with test cases

--- Data Structures/Stacks & Queues/reverseStackUsingRecursion.js
+++ Data Structures/Stacks & Queues/reverseStackUsingRecursion.js
@@ -15,3 +15,14 @@ const reverseStack = (stack) => {
 	helper();
 	return result;
 };
+
+// Test Cases
+console.log(reverseStack([])); // []
+console.log(reverseStack([1])); // [1]
+console.log(reverseStack([1, 2])); // [2, 1]
+console.log(reverseStack([2, 1])); // [1, 2]
+console.log(reverseStack([1, 2, 3])); // [3, 2, 1]
+console.log(reverseStack([3, 2, 1])); // [1, 2, 3]
+console.log(reverseStack([1, 2, 3, 4])); // [4, 3, 2, 1]
+console.log(reverseStack([4, 3, 2, 1])); // [1, 2, 3, 4]
+console.log(reverseStack([1, 2, 3, 4, 5])); // [5, 4, 3, 2, 1]
