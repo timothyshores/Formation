75649917b36ba4e88f63e61bb7584d9a3052a8c3
timothyshores <timothymshores@gmail.com>
2023-03-11 13:17:32 -0700

    Create iterative variation for sum elements in a binary tree

---
+++ Data Structures/Binary Trees/sumElementsIterative.js
@@ -0,0 +1,49 @@
+/*
+
+❓ PROMPT
+
+Given a binary tree, sum all elements in the tree.
+
+Example:
+• Given a binary tree:
+                 1
+                / \
+               7   3
+              / \
+             4   5
+// returns 20
+
+
+🔎 EXPLORE
+What are some other insightful & revealing test cases?
+
+🧠 BRAINSTORM
+What approaches could work?
+Algorithm 1: 
+Time: O()
+Space: O() 
+
+📆 PLAN
+Outline of algorithm #: 
+
+🛠️ IMPLEMENT
+Write your algorithm.
+
+*/
+
+class TreeNode {
+	constructor(value = 0, leftChild = null, rightChild = null) {
+		this.value = value;
+		this.left = leftChild;
+		this.right = rightChild;
+	}
+}
+
+const sumBT = (root) => {};
+
+/*
+
+🧪 VERIFY
+Run tests. Methodically debug & analyze issues.
+
+*/
