d05900940b48cdfc95fae88cd989ece6a5dd3780
timothyshores <timothymshores@gmail.com>
2023-03-10 23:26:33 -0700

    Add count elements recursively in binary trees subdirectory

---
+++ Data Structures/Binary Trees/countElementsRecursive.js
@@ -0,0 +1,48 @@
+/*
+
+❓ PROMPT
+
+Given a binary tree, count the number of elements in the tree.
+
+Example:
+• Given a binary tree:
+                 1
+                / \
+               7   3
+              / \
+             4   5
+// returns 5
+
+🔎 EXPLORE
+What are some other insightful & revealing test cases?
+
+🧠 BRAINSTORM
+What approaches could work?
+Algorithm 1: 
+Time: O()
+Space: O()
+
+📆 PLAN
+Outline of algorithm #: 
+
+🛠️ IMPLEMENT
+Write your algorithm.
+
+*/
+
+class TreeNode {
+	constructor(value = 0, leftChild = null, rightChild = null) {
+		this.value = value;
+		this.left = leftChild;
+		this.right = rightChild;
+	}
+}
+
+const countTree = (root) => {};
+
+/* 
+
+🧪 VERIFY
+Run tests. Methodically debug & analyze issues 
+
+*/
