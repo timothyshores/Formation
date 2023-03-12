a13ab4dd10281ab9bd1451dd2615d1615e8195d4
timothyshores <timothymshores@gmail.com>
2023-03-12 14:44:19 -0600

    Create atoi problem under recursion subdirectory

---
+++ Algorithms/Recursion/atoi.js
@@ -0,0 +1,34 @@
+/*
+
+❓ PROMPT
+Given a base-10 integer as a string, parse the integer and return it as an int. This problem can be done iteratively and recursively, and it's worth doing both! 
+
+Example(s)
+atoi("123") == 123
+atoi("4") == 4
+atoi("007") == 7
+atoi("-1") == -1
+
+🔎 EXPLORE
+State your assumptions & discoveries: 
+
+🧠 BRAINSTORM
+What approaches could work? Consider data structures or algorithmic patterns.
+Analyze the space & time complexity.
+Approach 1: 
+Time: O() 
+Space: O() 
+
+📆 PLAN
+High-level outline of approach #: 
+
+🛠️ IMPLEMENT */
+const atoi = (intAsString) => {};
+
+/*
+
+🧪 VERIFY
+Run your examples & test cases.
+Methodically analyze and debug issue(s).
+
+*/
