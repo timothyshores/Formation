a3acf402943120af843ed0d7951a98e06ecda7c0
timothyshores <timothymshores@gmail.com>
2023-03-12 16:02:40 -0600

    Add explore, brainstorm and plan notes to atoi recursive implementation

--- Algorithms/Recursion/atoiRecursive.js
+++ Algorithms/Recursion/atoiRecursive.js
@@ -11,14 +11,36 @@ atoi("-1") == -1
 
 🔎 EXPLORE
 State your assumptions & discoveries: 
+· Want to convert a string integer into an integer using recursion
+· Integers either start with a non-zero or a negative number
+· The only time an integer is allowed to start with a zero is if the integer is 0
 
 🧠 BRAINSTORM
 What approaches could work? Consider data structures or algorithmic patterns.
 Analyze the space & time complexity.
-Approach 1: 
+Approach 1: Recursion
+
+Time: O(N) where n is the length of the input string 
+Space: O(N) where n is the length of the input string.
 
 📆 PLAN
+High-level outline of approach #: 1
 
+If the length of the input string is 0 return NaN
+
+Initialize a variable sign to 1 to keep track if the input string is a positive or negative integer
+
+If the first character of the input string is '-', 
+    Set sign to -1
+    Remove the '-' character from the input string.
+
+We extract the first character of the input string using charCodeAt and subtract 48 to get its integer value, which we store in a variable digit.
+
+We then extract the rest of the input string and store it in a variable restOfString.
+
+If the length of restOfString is 0, we return sign * digit, which is the final result.
+
+Otherwise, we recursively call atoi on restOfString to obtain the integer value of the rest of the string, and then add digit multiplied by 10 raised to the power of the length of restOfString to shift the existing digits of the result to the left and add the new digit on the right. We multiply this sum by sign to account for the sign of the original input string, and return it as the final result.
 
 🛠️ IMPLEMENT */
 const atoi = (intAsString) => {};
