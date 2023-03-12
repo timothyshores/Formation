0ffab6c19faefc06c9b8f5641cce8d42f8888d2a
timothyshores <timothymshores@gmail.com>
2023-03-12 08:45:21 -0600

    Add iterative version of search binary search tree problem

---
+++ Data Structures/Binary Trees/binarySearchTreeIterative.js
@@ -0,0 +1,46 @@
+/* 
+
+PROMPT: Creating the following tree and searching through it
+
+                  21
+       14                   28
+  11        18         25       32
+5   12   15    19    23   27   30   37
+
+*/
+
+class ListNode {
+	constructor(val, left = null, right = null) {
+		this.val = val;
+		this.left = left;
+		this.right = right;
+	}
+}
+
+const leftLeftTree = new ListNode(11, new ListNode(5), new ListNode(12));
+const leftRightTree = new ListNode(18, new ListNode(15), new ListNode(19));
+const rightLeftTree = new ListNode(25, new ListNode(23), new ListNode(27));
+const rightRightTree = new ListNode(32, new ListNode(30), new ListNode(37));
+const leftSubTree = new ListNode(14, leftLeftTree, leftRightTree);
+const rightSubTree = new ListNode(28, rightLeftTree, rightRightTree);
+const root = new ListNode(21, leftSubTree, rightSubTree);
+
+/*
+
+EXPLORE
+
+BRAINSTORM
+
+Plan
+
+Implement
+
+*/
+
+const iterativeSearchInBST = (root, target) => {};
+
+/*
+
+Verify all test cases return true
+
+*/
