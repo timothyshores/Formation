229d8d18ae1ef23e4d525828bdd912ecdf36f028
timothyshores <timothymshores@gmail.com>
2023-03-14 21:12:37 -0600

    Verify depth first values implementation with test cases

--- Data Structures/Binary Trees/depthFirstValues.js
+++ Data Structures/Binary Trees/depthFirstValues.js
@@ -59,7 +59,7 @@ const depthFirstValues = (root) => {
 		// Pop off the top element from the store and store in currentNode variable
 		const currentNode = stack.pop();
 		// Add the currentNode to a set of visited nodes
-		visited.push(currentNode?.val);
+		visited.push(currentNode.val);
 		// If the currentNode has a right child, add it to the stack
 		if (currentNode.right) stack.push(currentNode.right);
 		// If the currentNode has a left child, add it to the stack
@@ -74,3 +74,55 @@ const depthFirstValues = (root) => {
 Verify 
 
 */
+
+// Test 1
+const a1 = new Node("a");
+const b1 = new Node("b");
+const c1 = new Node("c");
+const d1 = new Node("d");
+const e1 = new Node("e");
+const f1 = new Node("f");
+
+a1.left = b1;
+a1.right = c1;
+b1.left = d1;
+b1.right = e1;
+c1.right = f1;
+
+//      a
+//    /   \
+//   b     c
+//  / \     \
+// d   e     f
+
+console.log(depthFirstValues(a1)); // ['a', 'b', 'd', 'e', 'c', 'f']
+
+// Test 2
+const a2 = new Node("a");
+const b2 = new Node("b");
+const c2 = new Node("c");
+const d2 = new Node("d");
+const e2 = new Node("e");
+
+a2.right = b2;
+b2.left = c2;
+c2.right = d2;
+d2.right = e2;
+
+//      a
+//       \
+//        b
+//       /
+//      c
+//       \
+//        d
+//         \
+//          e
+
+console.log(depthFirstValues(a2)); // ['a', 'b', 'c', 'd', 'e']
+
+// Test 3
+console.log(depthFirstValues(new Node("a"))); // ['a']
+
+// Test 4
+console.log(depthFirstValues(null)); // []
