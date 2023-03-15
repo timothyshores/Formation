6dcb4507a35f4d2c56af404847c2f789959abce7
timothyshores <timothymshores@gmail.com>
2023-03-14 21:06:53 -0600

    Add depth first values prompt with engineering method and starter code

---
+++ Data Structures/Binary Trees/depthFirstValues.js
@@ -0,0 +1,31 @@
+/*
+
+PROMPT
+
+Write a function, depthFirstValues, that takes in the root of a binary tree.The function should return an array containing all values of the tree in depth - first order.
+
+EXPLORE
+
+BRAINSTORM
+
+PLAN
+
+IMPLEMENT
+
+*/
+
+class Node {
+	constructor(val) {
+		this.val = val;
+		this.left = null;
+		this.right = null;
+	}
+}
+
+const depthFirstValues = (root) => {};
+
+/*
+
+Verify 
+
+*/
