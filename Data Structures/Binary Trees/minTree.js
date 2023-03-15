02c6de9be1b7fb0cfa99719df2111d1ccf430c96
timothyshores <timothymshores@gmail.com>
2023-03-15 13:14:43 -0600

    Add brainstorm notes to minimum tree value

--- Data Structures/Binary Trees/minTree.js
+++ Data Structures/Binary Trees/minTree.js
@@ -4,6 +4,20 @@ Write a function, treeMinValue, that takes in the root of a binary tree that con
 
 You may assume that the input tree is non-empty.
 
+Brainstorm:
+
+Approach 1: Iterative breadth first search
+Time complexity: O(N^2) because unshift of O(N)
+Space complexity: O(N)
+
+Approach 2: Recursive depth first search
+Time complexity: O(N)
+Space complexity: O(N)
+
+Approach 3: Iterative depth first search
+Time complexity: O(N)
+Space complexity: O(N)
+
 */
 
 class Node {
