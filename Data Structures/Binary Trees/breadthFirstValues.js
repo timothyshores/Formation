e541ba365ee7c2fb403279e594a2b04bc3abb2ed
timothyshores <timothymshores@gmail.com>
2023-03-14 22:59:37 -0600

    Add breadth first values in binary trees subdirectory

---
+++ Data Structures/Binary Trees/breadthFirstValues.js
@@ -0,0 +1,17 @@
+/*
+
+PROMPT
+
+Write a function, breadthFirstValues, that takes in the root of a binary tree. The function should return an array containing all values of the tree in breadth-first order.
+
+*/
+
+class Node {
+	constructor(val) {
+		this.val = val;
+		this.left = null;
+		this.right = null;
+	}
+}
+
+const breadthFirstValues = (root) => {};
