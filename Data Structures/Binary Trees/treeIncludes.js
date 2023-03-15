cfe907081c26c48c85931a23a0bfbeada46863eb
timothyshores <timothymshores@gmail.com>
2023-03-15 08:02:56 -0600

    Add tree includes problem in binary tree subdirectory

---
+++ Data Structures/Binary Trees/treeIncludes.js
@@ -0,0 +1,17 @@
+/*
+
+PROMPT
+
+Write a function, treeIncludes, that takes in the root of a binary tree and a target value. The function should return a boolean indicating whether or not the value is contained in the tree.
+
+*/
+
+class Node {
+	constructor(val) {
+		this.val = val;
+		this.left = null;
+		this.right = null;
+	}
+}
+
+const treeIncludes = (root, target) => {};
