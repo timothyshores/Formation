5ac1ddfe42bfa22cfb414ed7e0c8d2a8447324ae
timothyshores <timothymshores@gmail.com>
2023-03-15 13:13:59 -0600

    Add minimum tree value under recursion subdirectory

---
+++ Data Structures/Binary Trees/minTree.js
@@ -0,0 +1,17 @@
+/*
+
+Write a function, treeMinValue, that takes in the root of a binary tree that contains number values.The function should return the minimum value within the tree.
+
+You may assume that the input tree is non-empty.
+
+*/
+
+class Node {
+	constructor(val) {
+		this.val = val;
+		this.left = null;
+		this.right = null;
+	}
+}
+
+const treeMinValue = (root) => {};
