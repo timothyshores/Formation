d72c2127913696804a8331acbac103ea496d0649
timothyshores <timothymshores@gmail.com>
2023-03-14 21:07:19 -0600

    Add explore and brainstorm notes to depth first values

--- Data Structures/Binary Trees/depthFirstValues.js
+++ Data Structures/Binary Trees/depthFirstValues.js
@@ -6,7 +6,15 @@ Write a function, depthFirstValues, that takes in the root of a binary tree.The 
 
 EXPLORE
 
+- Need to handle edge cases like null value passed into the function and single root node being passed into function
+- If null value is passed into function return an empty array
+- If a single root node is passed in return the value of the root node in an array
+
 BRAINSTORM
+
+Approach 1: Iterative DFS
+Time complexity: O(N) to visit N number of nodes in the binary tree
+Space complexity: O(H) where H is the maximum height in the binary tree
 
 PLAN
 
