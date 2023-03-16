d8efd223283196c32ccd2c86baf6bbe1432b7ae2
timothyshores <timothymshores@gmail.com>
2023-03-15 22:41:26 -0600

    Implement max path sum using recursive DFS

--- Data Structures/Binary Trees/maxRootToLeafPathSum.js
+++ Data Structures/Binary Trees/maxRootToLeafPathSum.js
@@ -16,4 +16,8 @@ class Node {
 	}
 }
 
-const maxPathSum = (root) => {};
+const maxPathSum = (root) => {
+	if (!root) return -Infinity;
+	if (!root.left && !root.right) return root.val;
+	return root.val + Math.max(maxPathSum(root.left), maxPathSum(root.right));
+};
