7e8a0fff7946c46d89571aadc1f75c43006bf1f5
timothyshores <timothymshores@gmail.com>
2023-04-27 15:16:51 -0600

    Add find element using iterative BFS prompt and engineering method

---
+++ Data Structures/Binary Trees/findElementBFS.js
@@ -0,0 +1,53 @@
+/*
+▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁
+✏️ Description
+▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔
+Q. Given a binary tree and a target element's value, determine if the tree contains the target using breadth first search (BFS).
+
+Examples:
+• Given a binary tree:
+                 3
+                / \
+              29   4
+              /     \
+             2       2
+                    /
+                   9
+• For target: 1 // returns False
+• For target: 2 // returns True
+
+/*
+'''
+🔎 EXPLORE
+What are some other insightful & revealing test cases?
+
+🧠 BRAINSTORM
+What approaches could work?
+Algorithm 1: Iterative BFS
+Worst case time: 
+Best case time: 
+Space:
+
+📆 PLAN
+Outline of algorithm #: 
+
+🛠️ IMPLEMENT
+
+*/
+
+class TreeNode {
+	constructor(value = 0, leftChild = null, rightChild = null) {
+		this.value = value;
+		this.left = leftChild;
+		this.right = rightChild;
+	}
+}
+
+const BFSTree = (root, target) => {};
+
+/*
+
+🧪 VERIFY
+Run tests. Methodically debug & analyze issues.
+
+*/
