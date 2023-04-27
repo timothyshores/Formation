d1c0f3d1b73d2c054f258a6a462259c5ea770381
timothyshores <timothymshores@gmail.com>
2023-04-27 15:27:21 -0600

    Add prompt for find element in BST using iterative approach

---
+++ Data Structures/Binary Trees/Binary Search Tree/findElementIterative.js
@@ -0,0 +1,29 @@
+/*
+▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁
+✏️ Description
+▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔
+Q. Given a binary search tree and a target integer, check if the tree contains a target.
+
+Examples:
+• Given a binary search tree:
+                  8
+                /   \
+               3     10
+              / \      \
+             1   6      14
+                       /
+                     13
+• For target: 4 // returns False
+• For target: 14 // returns True
+
+*/
+
+class TreeNode {
+	constructor(value = 0, leftChild = null, rightChild = null) {
+		this.value = value;
+		this.left = leftChild;
+		this.right = rightChild;
+	}
+}
+
+const searchBST = (root, target) => {};
