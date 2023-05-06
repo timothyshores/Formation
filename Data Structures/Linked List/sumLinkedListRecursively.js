3b4732357be687078f89a812dbf9a127b7b3cb56
timothyshores <timothymshores@gmail.com>
2023-05-06 17:44:06 -0600

    Add sum Linked List recursively prompt in Linked List subdirectory

---
+++ Data Structures/Linked List/sumLinkedListRecursively.js
@@ -0,0 +1,19 @@
+/*
+
+PROMPT 
+Given a linked list, sum all elements in the list.
+
+Examples:
+• Given a linked list: 1 ➞ 4 ➞ 5 // returns 10
+• Given a linked list: 1 // returns 1
+
+*/
+
+class ListNode {
+	constructor(value = 0, next = null) {
+		this.value = value;
+		this.next = next;
+	}
+}
+
+const sumLinkedListRecursively = (node) => {};
