7d76b4769a6859f87cf00b215cf40f28a1653b0f
timothyshores <timothymshores@gmail.com>
2023-07-06 06:26:11 -0600

    Final solution for splice practice

--- JavaScript/splicePractice.js
+++ JavaScript/splicePractice.js
@@ -6,8 +6,9 @@ const original = [
 	{ name: "Ethan" },
 	{ name: "Olivia" },
 ];
+
 const copy = [...original];
-const temp = null;
+let temp = null;
 
 /**
  * This is a quick exercise is in splicing arrays. You should only use the following
@@ -22,4 +23,29 @@ const temp = null;
  * 4. Make sure that the original looks the same as above in the logs
  */
 
-console.log(original, copy);
+console.log(
+	"original\n",
+	original,
+	"\n",
+	"\n1. Create a copy of original and store it in copy.\n",
+	copy
+);
+console.log(
+	"\n2. Using ONLY one or more ARRAY SPLICE operations, and the single temp variable above:"
+);
+
+console.log("a. move 'Ethan' to the front of the copy");
+copy.splice(0, 0, { name: "Ethan" });
+
+console.log("b. then, move 'Oliver' to the end of the copy");
+temp = copy.splice(1, 1);
+copy.push(...temp);
+
+console.log(
+	"\n3. Confirm in the console.log below that your operations worked\n",
+	copy
+);
+console.log(
+	"\nMake sure that the original looks the same as above in the logs\n",
+	original
+);
