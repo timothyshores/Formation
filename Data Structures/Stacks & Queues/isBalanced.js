13dc57c499ea4b032460f34a83b73d73ac429e18
timothyshores <timothymshores@gmail.com>
2023-07-10 17:37:01 -0600

    Add notes for Approach 1 use a stack

--- Data Structures/Stacks & Queues/isBalanced.js
+++ Data Structures/Stacks & Queues/isBalanced.js
@@ -21,7 +21,16 @@ Edge case/Assumption/Observation:
 1. Only ( and ) expected in the input string 
 2. Empty string is balanced
 
-Approach 1: 
+Approach 1: Use a stack
+
+If string length is 0 or odd return false
+Initialize an empty stack using built in JS array
+Iterate through the input string str
+If we find an opening bracket append a closing bracket to the stack
+If we encounter a closing bracket pop from the stack and check if it's a closing bracket
+
+Time complexity: O(N) iterates through the string
+Space complexity: O(N) store up to str.length - 1 characters in the stack
 
 */
 
@@ -39,3 +48,4 @@ Follow up :
 - isAlmostBalanced - string contains only ( and ) but may contain at most one mistake.
 
 */
+
