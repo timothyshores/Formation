5b281321d162087f193404c4d0493ac4f8a23a5b
timothyshores <timothymshores@gmail.com>
2023-07-10 17:36:14 -0600

    Add isBalanced prompt

---
+++ Data Structures/Stacks & Queues/isBalanced.js
@@ -0,0 +1,41 @@
+/*
+
+Question : 
+
+- isBalanced - input to this function is a string containing only ( and ) characters balanced?
+
+Input : "(((((((((((((("
+Output : False
+
+Input: "()"
+Output: True
+
+Input: "()()()()()(((((())))))"
+Output : True
+
+Input: "))))))))))))"
+Output : False
+
+Edge case/Assumption/Observation: 
+- 
+1. Only ( and ) expected in the input string 
+2. Empty string is balanced
+
+Approach 1: 
+
+*/
+
+/* 
+
+What if we add all 3 types of brackets (), {} and []
+
+Examples
+
+*/
+
+/*
+
+Follow up : 
+- isAlmostBalanced - string contains only ( and ) but may contain at most one mistake.
+
+*/
