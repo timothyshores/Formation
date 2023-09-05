9f13f432093c6135374fd4705719c9e5afdab45d
timothyshores <timothymshores@gmail.com>
2023-09-05 17:00:03 -0600

    Add prompt for Underline Flipped Number Signs

---
+++ Code Reviews/underlineFlippedNumberSigns.js
@@ -0,0 +1,18 @@
+/* 
+
+You're holding a silent auction with 650 bidders, assigning each bidder a numbered sign between 1-650. A person raises their sign when they want to make an offer.
+
+However, they sometimes hold the sign upside-down, and the auctioneer
+mistakes their number for a different number. For example, bidder #6 
+raises their sign upside-down and could be mistaken for bidder #9.
+
+Write a function that underlines all numbers from 1-650 that can be
+misinterpreted for another number within that range when rotated 180 degrees.
+
+EXAMPLES
+The number 6 should be underlined because it can be misinterpreted for the number 9 and vice versa.
+
+function underlineMistakenNumbers(upperBound) {
+def underlineMistakenNumbers(upperBound: int) -> None:
+
+*/
