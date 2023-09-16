d9ac368604f1bc8798a3b74ca99aa6e40bc9a8fc
Timothy Shores <timothymshores@gmail.com>
2023-09-16 14:31:52 -0600

    Add Dutch National Flag problem prompt

---
+++ Algo Marathon/Arrays/dutchNationalFlag.js
@@ -0,0 +1,18 @@
+/*
+
+Dutch National Flag
+
+The Dutch National Flag consists of 3 bands of colors which we will represent as 3 numbers in an array. Given an array of 0s, 1s and 2s, sort them in-place.
+
+Source: https://www.geeksforgeeks.org/sort-an-array-of-0s-1s-and-2s/
+
+EXAMPLE(S)
+Input: [1,1,1,0,0,2,2,0,1,0,2,2]
+Output: [0,0,0,0,1,1,1,2,2,2,2]
+
+FUNCTION SIGNATURE
+def sortColors(nums: list[int]) -> list[int]:
+
+*/
+
+const sortColors = (nums) => {};
