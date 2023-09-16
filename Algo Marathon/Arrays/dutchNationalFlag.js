2b5c7d8a2c35b9b95597a87790227a24cffcdfbe
Timothy Shores <timothymshores@gmail.com>
2023-09-16 14:39:58 -0600

    Add engineering method notes for Dutch national flag problem

--- Algo Marathon/Arrays/dutchNationalFlag.js
+++ Algo Marathon/Arrays/dutchNationalFlag.js
@@ -13,6 +13,29 @@ Output: [0,0,0,0,1,1,1,2,2,2,2]
 FUNCTION SIGNATURE
 def sortColors(nums: list[int]) -> list[int]:
 
+Engineering Method Notes
+
+Approaches 
+1. Brute force nested for 
+
+3 pointer approach - O(N) time, O(1) space
+0 - start at index 0, for every 0 that's added increment pointer index
+1 - Is current pointer
+2 - start at input.length - 1 index, for every 2 that's added decrement pointer index
+
+initialize 3 pointers
+left start at index 0
+right starts at nums.length - 1
+current starts at 0
+
+Look at first element
+If it's 1 do nothing
+If it's 0 do an in place swap with current index and the left pointer index
+- Increment the left pointer index
+If it's 2 do an in place swap with current index and the right pointer index
+- Decrement the right pointer index
+Once we're at the end then nums will be sorted
+
 */
 
 const sortColors = (nums) => {};
