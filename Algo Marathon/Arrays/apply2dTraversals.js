fc4baa547865049d85bd341f547f6153ae4809c5
timothyshores <timothymshores@gmail.com>
2023-09-18 08:21:42 -0600

    Add prompt for find average column and row minimums for a matrix

---
+++ Algo Marathon/Arrays/apply2dTraversals.js
@@ -0,0 +1,48 @@
+/*
+
+❓ PROMPT
+In this task, we're going to apply basic 2-dimensional matrix traversals to solve some simple problems. While working on these, look out for other patterns you may have seen previously. Each of these takes the row- and column-major traversals and composites them with simpler ideas you have almost certainly encountered in one-dimensional problems.
+
+This task is two similar problems in one:
+- First, write a function that returns the average of the smallest values in each _column_.
+- Write a new version of this function that returns the average of the smallest value in each _row_.
+
+Remember that since we represent a matrix as nested arrays (an array of arrays), many problems on a matrix can be broken down into two array patterns. This makes them easier to reason about and code. 
+
+Example(s)
+matrix = [
+    [32, 23, 3],
+    [23,  7, 5]
+]
+averageColumnMinimum(matrix) == 11 (because average(23, 7, 3) = 11)
+averageRowMinimum(matrix) == 4 (because average(5, 3) = 4)
+
+🔎 EXPLORE
+List your assumptions & discoveries:
+
+🧠 BRAINSTORM
+What approaches could work?
+Algorithm 1: 
+
+📆 PLAN
+Outline of algorithm #:
+
+🛠️ IMPLEMENT 
+
+*/
+
+const averageColumnMinimum = (matrix) => {
+
+};
+
+const averageRowMinimum = (matrix) => {
+
+};
+
+/*
+
+🧪 VERIFY
+Run tests. Methodically debug & analyze issues.
+
+*/
+
