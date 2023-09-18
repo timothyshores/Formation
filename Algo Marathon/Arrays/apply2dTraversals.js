49b58ada0054029846ae8bd452727b0120bf979b
timothyshores <timothymshores@gmail.com>
2023-09-18 08:22:22 -0600

    Add explore and brainstorm notes for average row and col minimums

--- Algo Marathon/Arrays/apply2dTraversals.js
+++ Algo Marathon/Arrays/apply2dTraversals.js
@@ -20,9 +20,22 @@ averageRowMinimum(matrix) == 4 (because average(5, 3) = 4)
 🔎 EXPLORE
 List your assumptions & discoveries:
 
+averageColumnMinimum
+- Column major traversal, find the minimum in each column, sum all minimums, divide by number of columns
+
+averageRowMinimum
+- Row major traversal, find the minimum in each row, sum all minimums, divide by number of rows
+
+Edge cases - Input matrix could be [[]]. If input matrix is [[]] return 0.
+
+Insightful & revealing test cases:
+
+
 🧠 BRAINSTORM
 What approaches could work?
-Algorithm 1: 
+Algorithm 1: Use for loop to iterate
+Time: O(N) - must iterate through all N elements in the matrix. Could also be m * n where m and n are number of elements in each row and column.
+Space: O(1) - Need to track the sum of the minimum elements in each row or column and the total number of rows or columns to calculate the ACM or ARM. 
 
 📆 PLAN
 Outline of algorithm #:
