1eb0e005eca1a531aa5f4458c0d08d59690864bc
Timothy Shores <timothymshores@gmail.com>
2023-09-24 18:20:30 -0600

    Add zig zag matrix traversal prompt

---
+++ Algo Marathon/Arrays/zigzagMatrixTraversal.js
@@ -0,0 +1,43 @@
+/*
+
+❓ PROMPT
+Given a two dimensional array, output a new one dimensional array with the elements of the input in zig zag order. This means that the first row will be in the output from first to last, but the second row will be the reverse, last to first, then the third is back to normal order again, each row the opposite order of the ones before and after.
+
+Example(s)
+const matrix = [
+    [1, 2, 3],
+    [4, 5, 6]
+]
+linearizeZigZag(matrix) == [1,2,3,6,5,4]
+
+Additional details:
+- Rows within the matrix will always be the same length
+- Matrix could be either square or rectangular
+
+🔎 EXPLORE
+List your assumptions & discoveries:
+
+Insightful & revealing test cases:
+
+
+🧠 BRAINSTORM
+What approaches could work?
+
+📆 PLAN
+Outline of algorithm #: 1
+
+🛠️ IMPLEMENT
+function linearizeZigZag(matrix) {
+def linearizeZigZag(matrix: list[list[int]]) -> list[int]:
+
+*/
+
+/*
+
+🧪 VERIFY
+Run tests. Methodically debug & analyze issues.
+
+*/
+
+// Test Data
+const testData = [];
