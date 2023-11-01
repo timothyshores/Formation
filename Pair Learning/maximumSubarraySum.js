cee28e571a370b248ec0e0b51d052d4fd8ef499a
timothyshores <timothymshores@gmail.com>
2023-11-01 17:40:45 -0600

    Add maximum subarray sum prompt

---
+++ Pair Learning/maximumSubarraySum.js
@@ -0,0 +1,13 @@
+/*
+
+Kadane’s Algorithm
+
+Given an array of integers, return maximum subarray sum. A subarray is any contiguous set of numbers. If the input array has at least one element, then the shortest subarray should include at least one element.
+
+EXAMPLE(S)
+[2, 2, -3, 6, -10, 4] => 7 because of the subarray [2, 2, -3, 6]
+
+FUNCTION SIGNATURE
+func maxSubarraySum(input: [Int]) -> Int
+
+*/
