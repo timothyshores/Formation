fa62ec6abb1d78e4c0430c3cbdf72b634cb0fe1b
timothyshores <timothymshores@gmail.com>
2023-11-05 10:55:19 -0700

    Add bros recipe prompt

---
+++ Practical Problems/brosRecipe.js
@@ -0,0 +1,50 @@
+/*
+
+❓ PROMPT
+You found a note from your brother when you got home from school:
+
+"Doofus, go to the store to pick up some ingredients for my recipe. Don't buy anything we already have at home like last time. Make your grocery list, you lazy bum. Use your own money to pay for it, you owe me for only pranking you 5 times this week. Try to get it done before you're old, loser."
+
+Your brother's a jerk and gave you the recipe in the hardest possible way to decipher: one long line of all the quantities followed by all the ingredients. Meanwhile, the ingredients at home are in tabular format, ugh!
+
+Example(s)
+Recipe: "12 3 3 2 egGs baCon sAusaGe souRdoughBread"
+At home: {"SauSage": 6, "EGgs": 4, "BACoN": 3, "banAnA"; 1}
+Grocery list: {"eggs": 8, "sourdoughbread": 1}
+
+You need 12 eggs but you only have 4 at home, so you need 8 more, dweeb.
+You need 3 bacon and you have 3 at home, so you don't need anymore, dweeb.
+You need 3 sausage and you have 6 at home, so you don't need anymore, dweeb.
+You need 1 sourdoughbread but you have none at home, so you need 1 more, dweeb.
+The banana isn't a part of the recipe, so it doesn't matter, dweeb.
+
+Your brother swears he could do this so much faster than you, but he's got more important things to do like go grind the half-pipe with some gnarly shredz.
+
+🔎 EXPLORE
+List your assumptions & discoveries:
+
+
+Insightful & revealing test cases:
+
+
+🧠 BRAINSTORM
+What approaches could work?
+Algorithm 1:
+Time: O()
+Space: O()
+
+📆 PLAN
+Outline of algorithm #: 
+
+🛠️ IMPLEMENT
+function getGroceryList(brosRecipe, ingredientsAtHome) {
+def get_grocery_list(bros_recipe: str, ingredients_at_home:dict) -> dict:
+
+*/
+
+/*
+
+🧪 VERIFY
+Run tests. Methodically debug & analyze issues.
+
+*/
