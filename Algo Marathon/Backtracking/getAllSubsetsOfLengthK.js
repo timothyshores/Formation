eaae4655b5e0aeed5ac21a7161d0813239980062
timothyshores <timothymshores@gmail.com>
2023-11-29 10:58:56 -0700

    Add prompt for get all subsets of length K

---
+++ Algo Marathon/Backtracking/getAllSubsetsOfLengthK.js
@@ -0,0 +1,24 @@
+/*
+
+Subsets of Length K
+
+You are given an array of distinct elements and an integer k. Your task is to implement a function getAllSubsetsOfLengthK that returns all possible subsets of the given array with a length of k.
+
+Examples
+getAllSubsetsOfLengthK([1, 2, 3], 0) => [[]]
+getAllSubsetsOfLengthK([1, 2, 3], 1) => [[1], [2], [3]]
+getAllSubsetsOfLengthK([1, 2, 3], 2) => [[1, 2], [1, 3], [2, 3]]
+getAllSubsetsOfLengthK([1, 2, 3], 3) => [[1, 2, 3]]
+getAllSubsetsOfLengthK(["A", "B", "C"], 0) => [[]]
+getAllSubsetsOfLengthK(["A", "B", "C"], 1) => [['A'], ['B'], ['C']]
+getAllSubsetsOfLengthK(["A", "B", "C"], 2) => [['A', 'B'], ['A', 'C'], ['B', 'C']]
+getAllSubsetsOfLengthK(["A", "B", "C"], 3) => [['A', 'B', 'C']]
+
+Constraints:
+- The input array arr contains distinct elements.
+- The length of the input array is at most 10^5.
+- 0 <= k <= length of the input array.
+
+*/
+
+const getAllSubsetsOfLengthK = (arr, k) => {};
