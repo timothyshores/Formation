f035016e2b9d2cf8d0b13cfe16f265e0b14c93d3
timothyshores <timothymshores@gmail.com>
2023-11-28 19:54:58 -0700

    Add atoli prompt

---
+++ Algo Marathon/Backtracking/atoi.js
@@ -0,0 +1,30 @@
+/*
+
+❓ PROMPT
+Given a base-10 integer as a string, parse the integer and return it as an int. This problem can be done iteratively and recursively, and it's worth doing both! 
+
+Example(s)
+atoi("123") == 123
+atoi("4") == 4
+atoi("007") == 7
+atoi("-1") == -1
+
+🔎 EXPLORE
+List your assumptions & discoveries:
+
+Insightful & revealing test cases:
+
+🧠 BRAINSTORM
+What approaches could work?
+
+📆 PLAN
+Outline of algorithm #: 
+
+🛠️ IMPLEMENT
+function atoi(intAsString) {
+def atoi(intAsString: str) -> int:
+
+🧪 VERIFY
+Run tests. Methodically debug & analyze issues.
+
+*/
