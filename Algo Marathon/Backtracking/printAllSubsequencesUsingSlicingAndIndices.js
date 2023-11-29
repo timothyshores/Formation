76f5a2ef4d551c58dc409bcc86f412eb9e1d4a40
timothyshores <timothymshores@gmail.com>
2023-11-29 11:13:56 -0700

    Add prompt for print all subsequences of a string

---
+++ Algo Marathon/Backtracking/printAllSubsequencesUsingSlicingAndIndices.js
@@ -0,0 +1,26 @@
+/*
+
+Given a word, print all subsequences of the word.
+
+All the subsequences of the word "ABCD" are:
+
+"ABCD"
+"ABC"
+"ABD"
+"AB"
+"ACD"
+"AC"
+"AD"
+"A"
+"BCD"
+"BC"
+"BD"
+"B"
+"CD"
+"C"
+"D"
+"" 
+
+Implement this 2 ways. First use string slicing creating a copy of the substring then using indices
+
+*/
