56d8e038e6333a52d92e10d16959b0c6f8eaa949
timothyshores <timothymshores@gmail.com>
2023-11-28 19:55:22 -0700

    Add explore and brainstorm notes to atoi

--- Algo Marathon/Backtracking/atoi.js
+++ Algo Marathon/Backtracking/atoi.js
@@ -11,11 +11,23 @@ atoi("-1") == -1
 
 🔎 EXPLORE
 List your assumptions & discoveries:
+- Convert the string integer to an integer
 
 Insightful & revealing test cases:
+atoi("0") == 0
+atoi("1") == 1
+atoi("10") == 10
+atoi("10001") == 10001
 
 🧠 BRAINSTORM
 What approaches could work?
+Algorithm 1: Recursion using a multiplier. Start at the end and move left. Multiple the multiplier by 10 every time you decrement the index.
+Time: O(N)
+Space: O(1)
+
+Additional Notes
+- The string can contain leading zeroes
+- The string can start with a - to indicate a negative number
 
 📆 PLAN
 Outline of algorithm #: 
