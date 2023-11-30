f9f61b60617bd1126ebfb8d6102be1d28da93633
timothyshores <timothymshores@gmail.com>
2023-11-29 22:49:51 -0700

    Verify get all subsequences of a string with test cases

--- Algo Marathon/Backtracking/getAllSubSequencesOfAString.js
+++ Algo Marathon/Backtracking/getAllSubSequencesOfAString.js
@@ -12,7 +12,7 @@ getAllSubsequences("abc") ==["a", "b", "c", "ab", "ac", "bc", "abc"]
 
 🔎 EXPLORE
 List your assumptions & discoveries:
-- Return all subsequences of a string in an arry
+- Return all subsequences of a string in an array
 
 Insightful & revealing test cases:
 
@@ -58,3 +58,26 @@ const getAllSubsequences = (word) => {
 	getSubsequence();
 	return subsequences;
 };
+
+// All test cases return true
+console.log(JSON.stringify(getAllSubsequences("")) === "[]");
+console.log(JSON.stringify(getAllSubsequences("a")) === '["a"]');
+console.log(
+	JSON.stringify(getAllSubsequences("ab").sort()) === '["a","ab","b"]'
+);
+console.log(
+	JSON.stringify(getAllSubsequences("abc").sort()) ===
+		'["a","ab","abc","ac","b","bc","c"]'
+);
+console.log(
+	JSON.stringify(getAllSubsequences("abcd").sort()) ===
+		'["a","ab","abc","abcd","abd","ac","acd","ad","b","bc","bcd","bd","c","cd","d"]'
+);
+console.log(
+	JSON.stringify(getAllSubsequences("1A").sort()) === '["1","1A","A"]'
+);
+console.log(
+	JSON.stringify(getAllSubsequences("1A2b").sort()) ===
+		'["1","12","12b","1A","1A2","1A2b","1Ab","1b","2","2b","A","A2","A2b","Ab","b"]'
+);
+console.log(getAllSubsequences("jesitony").length === 255);
