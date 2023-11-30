bec687fe1d2c1724f88a77540e9e4199e457cb4b
timothyshores <timothymshores@gmail.com>
2023-11-29 22:48:57 -0700

    Add get all subsequences of a string prompt

---
+++ Algo Marathon/Backtracking/getAllSubSequencesOfAString.js
@@ -0,0 +1,32 @@
+/*
+
+❓ PROMPT
+Define a subsequence of a string "s" to be a list of characters from "s" such that the characters appear in the same order in the list and in "s".
+
+For instance, for the string "abcd", "a", "ab", and "acd" are valid subsequences, whereas "db" is not, since "b" comes before "d" in the original string.
+
+Given an input string, return all subsequences except the empty string.
+
+Example(s)
+getAllSubsequences("abc") ==["a", "b", "c", "ab", "ac", "bc", "abc"]
+
+🔎 EXPLORE
+List your assumptions & discoveries:
+
+Insightful & revealing test cases:
+
+🧠 BRAINSTORM
+What approaches could work?
+
+
+📆 PLAN
+Outline of algorithm #:
+
+🛠️ IMPLEMENT
+function getAllSubsequences(word) {
+def getAllSubsequences(word: str) -> list[str]:
+
+🧪 VERIFY
+Run tests. Methodically debug & analyze issues.
+
+*/
