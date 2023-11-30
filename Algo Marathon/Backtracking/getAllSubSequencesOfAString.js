1407cc32134a4c7ea1b354b41beee3ee2c017635
timothyshores <timothymshores@gmail.com>
2023-11-29 22:49:13 -0700

    Add explore brainstorm and plan notes

--- Algo Marathon/Backtracking/getAllSubSequencesOfAString.js
+++ Algo Marathon/Backtracking/getAllSubSequencesOfAString.js
@@ -12,15 +12,28 @@ getAllSubsequences("abc") ==["a", "b", "c", "ab", "ac", "bc", "abc"]
 
 🔎 EXPLORE
 List your assumptions & discoveries:
+- Return all subsequences of a string in an arry
 
 Insightful & revealing test cases:
 
 🧠 BRAINSTORM
 What approaches could work?
-
+Algorithm 1: Recursive backtracking
+Time: O(2^N) where N is the length of the string
+Space: O(2^N) - 1 number of subsequences and O(N) for the stack in memory
 
 📆 PLAN
-Outline of algorithm #:
+Outline of algorithm #: 1
+
+- Initialize an empty array called subsequences
+- Create a helper function called getSubsequence that receives an index as a parameter and the current subsequence
+    - Base case
+        - When index is equal to the length of the string return
+    - Recursive cases
+        - Recursive case 1: Include the current character at index and call the helper function with index + 1
+        - Recursive case 2: Exclude the current character at index and call the helper function with index + 1
+- Call helper function getSubsequence with index 0
+- Return the subsequences array
 
 🛠️ IMPLEMENT
 function getAllSubsequences(word) {
