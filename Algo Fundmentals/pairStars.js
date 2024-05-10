b9477140c629a5e876a2b8b13a50d3ddbc967538
Timothy Shores <timothymshores@gmail.com>
2024-05-10 15:05:35 -0600

    Add pair stars prompt

---
+++ Algo Fundmentals/pairStars.js
@@ -0,0 +1,33 @@
+/*
+
+❓ PROMPT
+Given a string, recursively compute a new string where identical chars that are adjacent in the original string are separated from each other by a "*".
+
+Example(s)
+pairStars("hello") == "hel*lo"
+pairStars("xxyy") == "x*xy*y"
+pairStars("aaaa") == "a*a*a*a"
+
+Input string can be empty
+Input string will NOT be null
+Input string can contain uppercase, lowercase, numbers and symbols
+
+
+🔎 EXPLORE
+List your assumptions & discoveries:
+
+Insightful & revealing test cases:
+
+🧠 BRAINSTORM
+What approaches could work?
+
+
+📆 PLAN
+Outline of algorithm #:
+
+🛠️ IMPLEMENT
+
+🧪 VERIFY
+Run tests. Methodically debug & analyze issues.
+
+*/
