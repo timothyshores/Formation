63feff77e28f0dd41e752bf94f646042f8be6c77
Timothy Shores <timothymshores@gmail.com>
2024-05-11 15:17:28 -0600

    Add matrix row and column sum check prompt

---
+++ Algo Fundmentals/matrixRowAndColumnSumCheck.js
@@ -0,0 +1,48 @@
+/*
+
+❓ PROMPT
+Given two arrays, `rowSums` and `columnSums`, and a rectangular matrix. 
+Check to see if the two arrays are correct with respect to the matrix. 
+Return true if the ith row sums to the value in `rowSums[i]` for every i and if the jth column sums to `columnSums[j]` for every j. 
+Return false if any of these are incorrect.
+
+Example(s)
+matrix = [
+  [1, 2],
+  [3, 4]
+]
+
+rowSums = [3, 7]
+columnSums = [4, 6]
+
+will return true.
+
+matrix = [
+  [1, 2],
+  [3, 4]
+]
+rowSums = [3, 7]
+columnSums = [4, 7]
+
+will return false.
+ 
+
+🔎 EXPLORE
+List your assumptions & discoveries:
+ 
+
+🧠 BRAINSTORM
+What approaches could work?
+ 
+📆 PLAN
+Outline of algorithm #:
+*/
+
+const checkSums = (matrix, rowSums, columnSums) => {};
+
+/*
+
+🧪 VERIFY
+Run tests. Methodically debug & analyze issues.
+
+*/
