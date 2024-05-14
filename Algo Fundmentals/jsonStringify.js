291cdf1834e8d7ee83cef068a9dcfd62c67cd718
Timothy Shores <timothymshores@gmail.com>
2024-05-14 14:03:17 -0600

    Add Stringify JSON prompt

---
+++ Algo Fundmentals/jsonStringify.js
@@ -0,0 +1,30 @@
+/*
+
+❓ PROMPT
+You will be implementing a function called stringify which will take in a Javascript Object and return the JSON representation of the object as a string.
+This function is actually built into Javascript as `JSON.stringify(object)` but you have to write yours from scratch!
+
+You may want to take a moment to review the rules for [JSON syntax](https://www.w3schools.com/js/js_json_syntax.asp).
+
+Example(s)
+
+Examples:
+- Input: {"x": 5, "y": "Oliver"}
+  Output: '{"x": 5, "y": "Oliver"}'
+
+- Input: [1, "hello", "null", {"x": 5, "y": "Oliver"}]
+  Output: '[1, "hello", "null", {"x": 5, "y": "Oliver"}]'
+
+A brief note about the input and output. They look very similar, but the input is an object and the output is a string enclosed in single ticks. This means that the double quotes inside that string are characters in the string and are important.
+ 
+🔎 EXPLORE
+List your assumptions & discoveries:
+
+
+🧠 BRAINSTORM
+What approaches could work?
+
+📆 PLAN
+Outline of algorithm #: 
+*/
+
