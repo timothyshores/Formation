725fa9ae9a1c233bb9e8c022d257506cde378978
Timothy Shores <timothymshores@gmail.com>
2024-05-14 21:42:48 -0600

    Add factorial prompt

---
+++ Algo Fundmentals/factorial.js
@@ -0,0 +1,27 @@
+/*
+
+❓ PROMPT
+In mathematics, the factorial of a non-negative integer N, denoted as N!, is the multiplication product of all positive integers <= N. 
+Compute the result recursively (without loops).
+
+Example(s)
+factorial(3) == 6
+factorial(5) == 120
+
+Note
+- N will always be a postitive non-negative integer e.g 1, 2, 3, ..., etc.
+ 
+🔎 EXPLORE
+List your assumptions & discoveries:
+
+Insightful & revealing test cases:
+
+
+🧠 BRAINSTORM
+What approaches could work?
+📆 PLAN
+Outline of algorithm #: 
+
+🛠️ IMPLEMENT
+
+*/
