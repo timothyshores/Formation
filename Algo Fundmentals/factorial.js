49f3b37051b69182d2b5d25f4be41abf5e1b10dd
Timothy Shores <timothymshores@gmail.com>
2024-05-14 21:43:34 -0600

    Implement factorial using recursion with ternary operator and add test cases

--- Algo Fundmentals/factorial.js
+++ Algo Fundmentals/factorial.js
@@ -42,3 +42,13 @@ Recursive case: n > 1
 🛠️ IMPLEMENT
 
 */
+
+const factorial = (n) => (n === 1 ? 1 : n * factorial(n - 1));
+
+// All test cases return true
+console.log(factorial(1) == 1);
+console.log(factorial(2) == 2);
+console.log(factorial(3) == 6);
+console.log(factorial(4) == 24);
+console.log(factorial(5) == 120);
+console.log(factorial(6) == 720);
