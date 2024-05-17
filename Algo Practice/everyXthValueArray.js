7bb7f3804cae4232f55d5b7dc73c5d5d6def04e4
Timothy Shores <timothymshores@gmail.com>
2024-05-16 23:25:21 -0600

    Add explore brainstorm and plan notes

--- Algo Practice/everyXthValueArray.js
+++ Algo Practice/everyXthValueArray.js
@@ -7,10 +7,50 @@ Example(s)
 everyXth([1,2,3,4,5,6], 2) == [2,4,6]
 everyXth([1,2,3,4,5,6], 3) == [3,6]
 
+🔎 EXPLORE
+List your assumptions & discoveries:
+- Start at index -1 and add X to get the first element in the results array
+  everyXth(arr, 2) = (arr[1], arr[3], arr[5], ...)
+  everyXth(arr, 3) = (arr[2], arr[5], arr[8], ...)
+
+
+Insightful & revealing test cases:
+
+everyXth([1,2,3], 3) == [3]
+everyXth([1,2,3], 4) == [0]
+
 🧠 BRAINSTORM
 What approaches could work?
+Algorithm 1: Initialize empty array, for loop where i += x, push to array and return
+Time: O(N) technically N / X to iterate through the array 
+Space: O(N) technically N / X elements in the output array
+ 
+Algorithm 2: Same as algorithm 1 but recursively iterate through array
+
+Time: O(N) technically N / X to iterate through the array 
+Space: O(N) technically N / X elements in the output array
+
 📆 PLAN
-Outline of algorithm #: 
+Outline of algorithm #: 1
+
+- Intialize empty array called results
+- For loop starting at X - 1; x < inputArr.length; i += X
+  - Push the current element into the array
+- Return the array
+
+Outline of algorithm #: 2
+ 
+- Initalize an empty array called results
+- Initialze a helper function that recieves an index as an input
+  - Base case: 
+    - index > inputArr.length return
+  - Recursive case:
+    - index < inputArr.length
+    - push the current element into the results array
+    - call the helper function with current index + X
+- Call the helper function start at index X - 1
+- Return the array
+
 
 🛠️ IMPLEMENT
 
