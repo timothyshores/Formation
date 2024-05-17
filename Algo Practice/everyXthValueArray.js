a6afd5ef2758ac88476ad4101576369304c87595
Timothy Shores <timothymshores@gmail.com>
2024-05-16 23:25:06 -0600

    Add prompt for every Xth value in array

---
+++ Algo Practice/everyXthValueArray.js
@@ -0,0 +1,19 @@
+/*
+
+❓ PROMPT
+Given an array and a number *X*, return an array containing every *X*th number in the input array.
+
+Example(s)
+everyXth([1,2,3,4,5,6], 2) == [2,4,6]
+everyXth([1,2,3,4,5,6], 3) == [3,6]
+
+🧠 BRAINSTORM
+What approaches could work?
+📆 PLAN
+Outline of algorithm #: 
+
+🛠️ IMPLEMENT
+
+*/
+
+// All test cases return true
