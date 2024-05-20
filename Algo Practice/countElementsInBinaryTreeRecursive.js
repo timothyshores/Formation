d3727d1ca35318712b59899e73ec7d5f2fb1131b
Timothy Shores <timothymshores@gmail.com>
2024-05-20 13:28:31 -0600

    Add TreeNode class implement solution and add test cases for elements in binary tree

--- Algo Practice/countElementsInBinaryTreeRecursive.js
+++ Algo Practice/countElementsInBinaryTreeRecursive.js
@@ -39,3 +39,33 @@ Else there a root node
 
 */
 
+class TreeNode {
+  constructor(value = 0, leftChild = null, rightChild = null) {
+    this.value = value;
+    this.left = leftChild;
+    this.right = rightChild;
+  }
+}
+
+let countTree = (root) => {
+  // Base case
+  if (!root) return 0;
+
+  // Recursive case
+  return 1 + countTree(root.left) + countTree(root.right);
+};
+
+// All Test Cases return true
+console.log(countTree(null) === 0);
+console.log(countTree(new TreeNode(1, new TreeNode(2), new TreeNode(3))) === 3);
+console.log(
+  countTree(
+    new TreeNode(
+      2,
+      new TreeNode(29, new TreeNode(26)),
+      new TreeNode(4, null, new TreeNode(2, new TreeNode(9)))
+    )
+  ) === 6
+);
+console.log(countTree(new TreeNode()) === 1);
+
