d0eca8c2df417806934e8345b63c30dfc489018d
Timothy Shores <timothymshores@gmail.com>
2024-05-20 13:27:48 -0600

    Add count elements in binary tree prompt

---
+++ Algo Practice/countElementsInBinaryTreeRecursive.js
@@ -0,0 +1,16 @@
+/*
+
+Given a binary tree, count the number of elements in the tree.
+
+Example:
+• Given a binary tree:
+                 1
+                / \
+               7   3
+              / \
+             4   5
+
+countElements(root) -> 5
+
+*/
+
