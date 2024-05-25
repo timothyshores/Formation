265582975a7b61dfd642e7eb72cf609ef50422fc
Timothy Shores <timothymshores@gmail.com>
2024-05-25 03:59:21 -0600

    Add engineering method notes for Buy and Sell a Stock Once

--- Algo Practice/buyAndSellStockOnce.js
+++ Algo Practice/buyAndSellStockOnce.js
@@ -21,4 +21,40 @@ Note
 - If stock price always go down return 0
 - Max profit must always be >= 0
  
+
+🔎 EXPLORE
+List your assumptions & discoveries:
+- Want to return the maximum price from buying and then selling 1 stock
+- Stock must be purchased before it can be sold
+- Never buy and then sell it for less money later
+- If prices.length < 1 return 0
+- If prices are always going down then return 0
+
+Insightful & revealing test cases:
+
+ maxProfitPotential([]) // 0
+ maxProfitPotential([17]) // 0
+ maxProfitPotential([7,1,5,3,6,4]) // 5
+ maxProfitPotential([7,6,4,3,1]) // 0
+ 
+
+🧠 BRAINSTORM
+What approaches could work?
+Algorithm 1: Loop through the prices array, track variables and return max price
+Time: O(N) where N is prices.length
+Space: O(1) to store variables
+
+📆 PLAN
+Outline of algorithm #: 1
+ 
+Initialize maxProfit and set to 0
+Initialize a minPrice and set to Infinity
+
+Iterate through the prices array
+- Calculate the potential profit if you sold the current stock and stock into a variable
+- If currentProfit > maxProfit then set maxProfit = currentProfit
+- If currentStockPrice < minPrice then set minPrice = currentStockPrice
+
+Return maxProfit
+
 */
