957dafede3d766b4315d18e47bae482e6b96c316
Timothy Shores <timothymshores@gmail.com>
2024-06-03 20:35:34 -0600

    Add prompt for convert hexadecimal to integer

---
+++ Algo Practice/convertHexadecimaltoInteger.js
@@ -0,0 +1,38 @@
+/*
+'''
+❓ PROMPT
+Given a hexadecimal string as input, write a function that returns its integer representation.
+The hexadecimal character representation will be given in uppercase format.
+
+Example(s)
+Ex 1:
+    Given an input: 2A
+    iteration 1: (2)
+    0 * 16 = 0
+    0 + 2 = 2
+    
+    iteration 2: (A) 
+    2 * 16 = 32
+    32 + 10 = 42
+    
+    final result = 42.
+
+Ex 2:
+    Given an input 3FF
+    iteration 1: (3)
+    0 * 16 = 0
+    0 + 3 = 3
+    
+    iteration 2: (F)
+    3 * 16 = 48
+    48 + 15 = 63
+    
+    iteration 3: (F)
+    64 * 16 = 1008
+    1008 + 15 = 1023
+    
+    final result = 1023.
+
+'''
+*/
+
