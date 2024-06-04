bbb34dfcf02264a4e03923b7b0204c7469ff6239
Timothy Shores <timothymshores@gmail.com>
2024-06-03 20:52:02 -0600

    Add prompt for find element in sorted LL

---
+++ Algo Practice/findElementInSortedLinkedList.js
@@ -0,0 +1,9 @@
+/*
+
+Given a sorted linked list of unique integers, check if the list contains an element with a target value.
+
+Examples:
+• Given a linked list: 2 ➞ 3 ➞ 5, target: 2 // returns True
+• Given a linked list: 2 ➞ 3 ➞ 5, target: 4  // returns False
+
+*/
