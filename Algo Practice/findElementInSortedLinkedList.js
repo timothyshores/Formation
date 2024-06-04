93f665fb25e5d40c58d8d6cfa14abe6a012267a2
Timothy Shores <timothymshores@gmail.com>
2024-06-03 20:52:31 -0600

    Implement find element in sorted LL iterative and recursive

--- Algo Practice/findElementInSortedLinkedList.js
+++ Algo Practice/findElementInSortedLinkedList.js
@@ -7,3 +7,28 @@ Examples:
 • Given a linked list: 2 ➞ 3 ➞ 5, target: 4  // returns False
 
 */
+
+class ListNode {
+  constructor(value = 0, next = null) {
+    this.value = value;
+    this.next = next;
+  }
+}
+
+const iterativeSearch = (head, target) => {
+  if (!head) return false;
+  let node = head;
+
+  while (node) {
+    if (node.value === target) return true;
+    if (node.value > target) return false;
+    node = node.next;
+  }
+
+  return false;
+};
+
+const recursiveSearch = (head, target) => {
+  if (!head || head.value > target) return false;
+  return head.value === target || recursiveSearch(head.next, target);
+};
