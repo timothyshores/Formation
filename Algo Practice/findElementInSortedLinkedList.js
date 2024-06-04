9e4265fe8ef1dfbcfaed81cb794331dcc871b981
Timothy Shores <timothymshores@gmail.com>
2024-06-03 20:53:03 -0600

    Add tests for find element in sorted LL

--- Algo Practice/findElementInSortedLinkedList.js
+++ Algo Practice/findElementInSortedLinkedList.js
@@ -32,3 +32,45 @@ const recursiveSearch = (head, target) => {
   if (!head || head.value > target) return false;
   return head.value === target || recursiveSearch(head.next, target);
 };
+
+// Helper function to create a linked list from an array of values
+const createLinkedList = (values) => {
+  let dummy = new ListNode(0);
+  let current = dummy;
+  for (let value of values) {
+    current.next = new ListNode(value);
+    current = current.next;
+  }
+  return dummy.next;
+};
+
+// Test Data
+const LL1 = createLinkedList([1, 2, 3, 5, 6, 7, 10]);
+const LL2 = createLinkedList([2, 3, 5]);
+const LL3 = createLinkedList([10]);
+const LL4 = createLinkedList([-10, -3, 0, 5, 9]);
+
+console.log("Iterative implementation");
+console.log(iterativeSearch(null, 1) === false);
+console.log(iterativeSearch(LL1, 2) === true);
+console.log(iterativeSearch(LL1, 4) === false);
+console.log(iterativeSearch(LL1, -1) === false);
+console.log(iterativeSearch(LL1, 10) === true);
+console.log(iterativeSearch(LL1, 11) === false);
+console.log(iterativeSearch(LL2, 3) === true);
+console.log(iterativeSearch(LL3, 10) === true);
+console.log(iterativeSearch(LL3, -10) === false);
+console.log(iterativeSearch(LL4, 0) === true);
+console.log();
+
+console.log("Recursive implementation");
+console.log(recursiveSearch(null, 1) === false);
+console.log(recursiveSearch(LL1, 2) === true);
+console.log(recursiveSearch(LL1, 4) === false);
+console.log(recursiveSearch(LL1, -1) === false);
+console.log(recursiveSearch(LL1, 10) === true);
+console.log(recursiveSearch(LL1, 11) === false);
+console.log(recursiveSearch(LL2, 3) === true);
+console.log(recursiveSearch(LL3, 10) === true);
+console.log(recursiveSearch(LL3, -10) === false);
+console.log(recursiveSearch(LL4, 0) === true);
