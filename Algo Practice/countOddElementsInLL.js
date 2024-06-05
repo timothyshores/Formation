cbfd27c4022f0179201eb0b7e277a754c6fb6cf8
Timothy Shores <timothymshores@gmail.com>
2024-06-05 01:42:32 -0600

    Implement count odd elements in a LL and verify with tests

--- Algo Practice/countOddElementsInLL.js
+++ Algo Practice/countOddElementsInLL.js
@@ -50,3 +50,50 @@ Call countOdd(head.next)
 After tranversing through all nodes in the LL return the count
 
 */
+
+class Node {
+  constructor(value, next) {
+    this.value = value;
+    this.next = next;
+  }
+}
+
+const countOdd = (head) => {
+  if (!head) return 0;
+  return (head.value % 2 === 0 ? 0 : 1) + countOdd(head.next);
+};
+
+// Empty LL
+console.log(countOdd(null) == 0);
+
+// 0
+let head = new Node(0);
+console.log(countOdd(head) == 0);
+
+// 5
+head = new Node(5);
+console.log(countOdd(head) == 1);
+
+// 6
+head = new Node(6);
+console.log(countOdd(head) == 0);
+
+// 1 -> 1 -> 1 -> 1
+head = new Node(1, new Node(1, new Node(1, new Node(1))));
+console.log(countOdd(head) == 4);
+
+// 1 -> 2 -> 3 -> 4
+head = new Node(1, new Node(2, new Node(3, new Node(4))));
+console.log(countOdd(head) == 2);
+
+// 2 -> 2 -> 2 -> 2
+head = new Node(2, new Node(2, new Node(2, new Node(2))));
+console.log(countOdd(head) == 0);
+
+// 1 -> 2 -> 3 -> 4 -> 5
+head = new Node(1, new Node(2, new Node(3, new Node(4, new Node(5)))));
+console.log(countOdd(head) == 3);
+
+// 6 -> 2 -> 100 -> 4 -> 8
+head = new Node(6, new Node(2, new Node(100, new Node(4, new Node(8)))));
+console.log(countOdd(head) == 0);
