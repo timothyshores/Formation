67250c234f729d9942126c8728c84c7cf6256bf4
Timothy Shores <timothymshores@gmail.com>
2024-06-05 01:30:45 -0600

    Add engineering method notes for bunny ears twist

--- Algo Practice/bunnyEarsTwist.js
+++ Algo Practice/bunnyEarsTwist.js
@@ -8,4 +8,35 @@ bunnyEarsTwist(2) == 5
 bunnyEarsTwist(3) == 7
 bunnyEarsTwist(5) == 12
 
+🔎 EXPLORE
+List your assumptions & discoveries:
+- Odd bunnies have 2 ears
+- Even bunnies have 3 ears
+
+Insightful & revealing test cases:
+bunnyEarsTwist(0) == 0
+bunnyEarsTwist(1) == 2
+bunnyEarsTwist(2) == 5
+bunnyEarsTwist(3) == 7
+bunnyEarsTwist(4) == 10
+bunnyEarsTwist(5) == 12
+bunnyEarsTwist(10) == 25
+bunnyEarsTwist(12) == 30
+
+🧠 BRAINSTORM
+What approaches could work?
+Algorithm 1: Recursion
+Time: O(N)
+Space: O(N) recursive stack
+
+📆 PLAN
+Outline of algorithm #: 1
+
+Base Case
+If bunnies is 0 return 0
+
+Recursive cases
+- If number is odd add 2 else number is even add 3
+- Recursively call bunnyEarsTwist(bunnies -1)
+
 */
