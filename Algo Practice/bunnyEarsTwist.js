25dccc678b3431c952821898ccf3f6b67682881a
Timothy Shores <timothymshores@gmail.com>
2024-06-05 01:30:31 -0600

    Add prompt for bunny ears twist

---
+++ Algo Practice/bunnyEarsTwist.js
@@ -0,0 +1,11 @@
+/*
+
+❓ PROMPT
+We have bunnies standing in a line, numbered 1, 2, ... The odd bunnies (1, 3, ..) have the usual 2 ears. The even bunnies (2, 4, ..) we'll say have 3 ears because they each have a raised foot. Recursively return the number of "ears" in the bunny line 1, 2, ... n (without loops or multiplication).
+
+Example(s)
+bunnyEarsTwist(2) == 5
+bunnyEarsTwist(3) == 7
+bunnyEarsTwist(5) == 12
+
+*/
