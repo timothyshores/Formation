c276dab344e3206c3ec2a4218bbf26289d268f4a
Timothy Shores <timothymshores@gmail.com>
2024-06-05 01:41:42 -0600

    Add prompt for count odd elements in a LL

---
+++ Algo Practice/countOddElementsInLL.js
@@ -0,0 +1,15 @@
+/*
+
+❓ PROMPT
+Given a linked list of positive integers, count the elements with odd values from the list using a recursive approach.
+
+Note that 0 is an even number.
+
+Example(s)
+head = 1 -> 1 -> 1 -> 1
+countOdd(head) == 4
+
+head = 1 -> 2 -> 3 -> 4
+countOdd(head) == 2
+
+*/
