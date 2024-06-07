2c5acf63bb6cec99091a264bdd4aceadc5290f87
Timothy Shores <timothymshores@gmail.com>
2024-06-06 21:42:01 -0600

    Add engineering method notes for index of first occurance in linked list

--- Algo Practice/indexFirstOccurrenceLL.js
+++ Algo Practice/indexFirstOccurrenceLL.js
@@ -11,4 +11,52 @@ list1 = 1 -> 2 -> 3 -> 4 -> 5
 firstIndexInLL(list1, 9) == -1
 firstIndexInLL(list1, 3) == 2
  
+
+🔎 EXPLORE
+List your assumptions & discoveries:
+- Return first index of target value contained in a node
+  - Indicies are 0 indexed
+- If the value is NOT contained within the LL return -1
+
+Insightful & revealing test cases:
+ 
+
+🧠 BRAINSTORM
+What approaches could work?
+Algorithm 1: Iterative
+Time: O(N)
+Space: O(1)
+ 
+🧠 BRAINSTORM
+What approaches could work?
+Algorithm 2: Recursion
+Time: O(N)
+Space: O(1)
+
+📆 PLAN
+Outline of algorithm #: 1
+
+Initialize an index variable and set it to 0
+
+While node is not null
+  TODO: Guard clause
+  Check the value of the current node
+    If the current node's value is the target return the index
+    Else set the current node to the current node.next
+
+If target has not been found in the LL then return -1
+
+Outline of algorithm #: 2
+
+Create an inner helper funtion with an index parameter
+
+Base Case
+- If node is null then return -1
+
+Recursive Case
+- If the current node's value is equal to target return index
+- Call the helper function on node.next and increment index
+
+Call the helper function starting at index 0
+
 */
