ae17f8587e9950663b0573dab910156e48363e2e
Timothy Shores <timothymshores@gmail.com>
2024-06-06 21:41:39 -0600

    Add prompt for index of first occurance in linked list

---
+++ Algo Practice/indexFirstOccurrenceLL.js
@@ -0,0 +1,14 @@
+/*
+
+❓ PROMPT
+Given a linked list and a target value, return the index of the first occurrence of that value in the linked list. The index should be zero-indexed.
+
+You'll need to write your own node class for this. In an interview it might be provided or it might not.
+
+Example(s)
+list1 = 1 -> 2 -> 3 -> 4 -> 5
+
+firstIndexInLL(list1, 9) == -1
+firstIndexInLL(list1, 3) == 2
+ 
+*/
